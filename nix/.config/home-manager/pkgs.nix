048d086262a3115c02ffe8d862577d52cea96430
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-30 19:11:15 +0300

    [o] experiment

--- nix/.config/home-manager/pkgs.nix
+++ nix/.config/home-manager/pkgs.nix
@@ -11,6 +11,7 @@
       imagemagick # for convert
       libfido2 # ssh sk keys support (maybe not needed)
       libsForQt5.qt5ct libsForQt5.qtstyleplugin-kvantum qt6Packages.qtstyleplugin-kvantum # kvantum theme support
+      o # experiment
       pass-secret-service # gnome-keyring alternative via paste
       pinentry # for gpg/gnupg password entry GUI. why does it not install this itself? ah, found out... https://github.com/NixOS/nixpkgs/commit/3d832dee59ed0338db4afb83b4c481a062163771
       pwgen # generate passwords
