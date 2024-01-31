6725c8c0fbe33bbc5a9af23cc3d94bef5c787705
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-01 02:54:12 +0300

    [home] cleanup

--- nix/.config/home-manager/apps/neovim.nix
+++ nix/.config/home-manager/apps/neovim.nix
@@ -1,13 +1,16 @@
-{ lib, config, pkgs, xdg, ... }: {
+{ config, pkgs, xdg, ... }: with rec {
+    l = config.lib.file.mkOutOfStoreSymlink; 
+    dots = /home/neg/.dotfiles;
+}; {
   home.packages = with pkgs; [
       manix
       neovim-remote # nvr for neovim
       nodejs_21 # dependency for some lsp stuff
   ];
-  # xdg.configFile = {
-  #     "nvim" = {
-  #         source = lib.mk "/home/neg/.dotfiles/dev/.config/nvim";
-  #         recursive = true;
-  #     };
-  # };
+  xdg.configFile = {
+      "nvim" = {
+          source = l "${dots}/dev/.config/nvim";
+          recursive = true;
+      };
+  };
 }
