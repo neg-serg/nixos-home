1b76a87a834f5759cdc88eb0d77105e51c8ad661
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-03 00:32:12 +0300

    [sops] comment secrets for now

--- nix/.config/home-manager/sops.nix
+++ nix/.config/home-manager/sops.nix
@@ -2,6 +2,6 @@
     sops = {
         age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt"; # must have no password!
         defaultSopsFile = ./secrets/all.yaml;
-        secrets.mpdas = {};
+        # secrets.mpdas = {};
     };
  }
