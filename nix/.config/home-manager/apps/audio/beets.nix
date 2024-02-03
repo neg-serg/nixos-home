4d82fb6b1f8d647d5c8d58742f5c2c7c00e4920a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-03 22:10:26 +0300

    [beets] fix and cleanup

--- nix/.config/home-manager/apps/audio/beets.nix
+++ nix/.config/home-manager/apps/audio/beets.nix
@@ -8,18 +8,13 @@
                    "lastgenre" "lastimport" "lyrics" "mbcollection" "mbsubmit"
                    "mbsync" "mpdstats" "web"];
                    # "discogs"
-
         directory = "~/music";            # The default library root directory.
         library = "~/music/library.db";   # The default library database file to use.
-
         threaded = "yes";
         color = "yes";
-
         ui = { color = "yes"; };
-
         per_disc_numbering = "no";
         original_date = "yes";
-
         import = {
             copy = "no";
             incremental_skip_later = "yes";
@@ -27,45 +22,19 @@
             none_rec_action = "skip";
             duplicate_action = "remove";
         };
-
         chroma = { auto = "yes"; };
-
-        lastgenre = {
-            auto = true;
-            canonical = true;
-            count = "5";
-        };
-
-        match = {
-          ignored = ["missing_tracks" "unmatched_tracks"];
-        };
-
+        lastgenre = { auto = true; canonical = true; count = 5; };
+        match = { ignored = ["missing_tracks" "unmatched_tracks"]; };
         fetchart = {
           auto = "yes";
           cover_names = ["front" "back"];
           sources = ["filesystem" "coverart" "itunes" "amazon" "albumart" "wikipedia" "google"];
         };
-
-        embedart = {
-          auto = "yes";
-          remove_art_file = "no";
-        };
-
-        lastfm = {
-          user = "e7z0x1";
-        };
-
-        lyrics = {
-            auto = "yes";
-        };
-
-        bbq = {
-          fields = ["artist" "title" "album"];
-        };
-
-        include = [
-            "${config.sops.secrets."musicbrainz.yaml".path}"
-        ];
+        embedart = { auto = "yes"; remove_art_file = "no"; };
+        lastfm = { user = "e7z0x1"; };
+        lyrics = { auto = "yes"; };
+        bbq = { fields = ["artist" "title" "album"]; };
+        include = [ "${config.sops.secrets."musicbrainz.yaml".path}" ];
       };
   };
 }
