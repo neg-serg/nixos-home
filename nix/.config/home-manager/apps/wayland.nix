ef44c0595655a3a227258c9cc9fbf7346b59b6c3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-11 18:47:07 +0300

    [wayland] stash some stuff for future

--- nix/.config/home-manager/apps/wayland.nix
+++ nix/.config/home-manager/apps/wayland.nix
@@ -12,3 +12,32 @@
         ydotool # xdotool systemwide
     ];
 }
+
+# systemd.user.services.wpaperd = {
+#     Unit = {
+#         Description = "Wallpaper daemon";
+#         After = ["graphical-session-pre.target"];
+#         PartOf = ["graphical-session.target"];
+#     };
+#     Service = {
+#         ExecStart = "${pkgs.wpaperd}/bin/wpaperd --no-daemon";
+#         Environment = "XDG_CONFIG_HOME=${wpaperd-config-dir}";
+#     };
+#     Install.WantedBy = ["graphical-session.target"];
+# };
+
+# programs = {
+#     gamescope = {
+#         enable = true;
+#         capSysNice = true;
+#         args = [ "--steam"
+#                 "--expose-wayland"
+#                 "--rt"
+#                 "-W 1920"
+#                 "-H 1080"
+#                 "--force-grab-cursor"
+#                 "--grab"
+#                 "--fullscreen"
+#         ];
+#     };
+# }
