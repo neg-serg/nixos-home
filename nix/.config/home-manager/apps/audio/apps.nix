605698a01a3e9b13e47b886d799d0585ecf4d2c2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-16 17:22:44 +0300

    [audio/apps] CS

--- nix/.config/home-manager/apps/audio/apps.nix
+++ nix/.config/home-manager/apps/audio/apps.nix
@@ -33,88 +33,88 @@
       termplay # play video in terminal
   ];
 
-    systemd.user.services.playerctld = {
-        Unit = {
-            Description = "Keep track of media player activity";
-            After = ["network.target" "sound.target"];
-        };
+  systemd.user.services.playerctld = {
+      Unit = {
+          Description = "Keep track of media player activity";
+          After = ["network.target" "sound.target"];
+      };
 
-        Service = {
-            Type = "forking";
-            ExecStart = "${pkgs.playerctl}/bin/playerctld daemon";
-            RestartSec = "3";
-            StartLimitBurst = "0";
-        };
-        Install = { WantedBy = ["default.target"]; };
-    };
+      Service = {
+          Type = "forking";
+          ExecStart = "${pkgs.playerctl}/bin/playerctld daemon";
+          RestartSec = "3";
+          StartLimitBurst = "0";
+      };
+      Install = { WantedBy = ["default.target"]; };
+  };
 
-    systemd.user.services.mpdas = {
-        Unit = {
-            Description = "mpdas last.fm scrobbler";
-            After = ["network.target" "sound.target" "mpd.service"];
-            Requires = "mpd.service";
-        };
-        Service = {
-            ExecStart = "${pkgs.mpdas}/bin/mpdas -c ${config.sops.secrets.mpdas_negrc.path}";
-            Restart = "on-failure";
-            RestartSec = "10";
-        };
-        Install = { WantedBy = ["default.target"]; };
-    };
+  systemd.user.services.mpdas = {
+      Unit = {
+          Description = "mpdas last.fm scrobbler";
+          After = ["network.target" "sound.target" "mpd.service"];
+          Requires = "mpd.service";
+      };
+      Service = {
+          ExecStart = "${pkgs.mpdas}/bin/mpdas -c ${config.sops.secrets.mpdas_negrc.path}";
+          Restart = "on-failure";
+          RestartSec = "10";
+      };
+      Install = { WantedBy = ["default.target"]; };
+  };
 
-    systemd.user.services.cover-notify = {
-        Unit = {
-            Description = "Music track notification with cover";
-            After = ["network.target" "sound.target" "playerctld.service" "mpd.service" "mpDris.service"];
-            BindsTo = "mpDris.service";
-            StartLimitIntervalSec = "0";
-        };
-        Service = {
-            ExecStart = "${pkgs.cached-nix-shell}/bin/cached-nix-shell -p 'python3.withPackages (p: [p.pygobject3 p.systemd])' -p gobject-introspection -p playerctl --run %h/bin/track-notification-daemon";
-            Restart = "always";
-            RestartSec = "3";
-        };
-    };
+  systemd.user.services.cover-notify = {
+      Unit = {
+          Description = "Music track notification with cover";
+          After = ["network.target" "sound.target" "playerctld.service" "mpd.service" "mpDris.service"];
+          BindsTo = "mpDris.service";
+          StartLimitIntervalSec = "0";
+      };
+      Service = {
+          ExecStart = "${pkgs.cached-nix-shell}/bin/cached-nix-shell -p 'python3.withPackages (p: [p.pygobject3 p.systemd])' -p gobject-introspection -p playerctl --run %h/bin/track-notification-daemon";
+          Restart = "always";
+          RestartSec = "3";
+      };
+  };
 
-    systemd.user.services.mpDris = {
-        Unit = {
-            Description = "mpDris2 - Music Player Daemon D-Bus bridge";
-            After = ["playerctld.service" "network.target" "sound.target" "mpd.service"];
-            PartOf = ["mpd.socket" "mpd.service"];
-        };
-        Service = {
-            Type = "simple";
-            Restart = "on-failure";
-            ExecStart = "${pkgs.writeShellScriptBin "delay-mpdris2" "${pkgs.coreutils}/bin/sleep 1 && ${pkgs.mpdris2}/bin/mpDris2"}/bin/delay-mpdris2";
-        };
-        Install = { WantedBy = ["default.target"]; };
-    };
+  systemd.user.services.mpDris = {
+      Unit = {
+          Description = "mpDris2 - Music Player Daemon D-Bus bridge";
+          After = ["playerctld.service" "network.target" "sound.target" "mpd.service"];
+          PartOf = ["mpd.socket" "mpd.service"];
+      };
+      Service = {
+          Type = "simple";
+          Restart = "on-failure";
+          ExecStart = "${pkgs.writeShellScriptBin "delay-mpdris2" "${pkgs.coreutils}/bin/sleep 1 && ${pkgs.mpdris2}/bin/mpDris2"}/bin/delay-mpdris2";
+      };
+      Install = { WantedBy = ["default.target"]; };
+  };
 
-    systemd.user.services.mpd = {
-        Unit = {
-            Description = "Music Player Daemon";
-            Documentation = "man:mpd(1) man:mpd.conf(5)";
-            After = ["network.target" "sound.target"];
-            ConditionPathExists = "${pkgs.mpd}/bin/mpd";
-        };
-        Service = {
-            Type = "notify";
-            ExecStart = "${pkgs.mpd}/bin/mpd --no-daemon";
-            WatchdogSec = 120;
-            LimitRTPRIO = "40"; # allow MPD to use real-time priority 40
-            LimitRTTIME = "infinity";
-            LimitMEMLOCK = "64M"; # for io_uring
-            ProtectSystem = "yes"; # disallow writing to /usr, /bin, /sbin, ...
-            NoNewPrivileges = "yes";
-            ProtectKernelTunables = "yes";
-            ProtectControlGroups = "yes";
-            # AF_NETLINK is required by libsmbclient, or it will exit() .. *sigh*
-            RestrictAddressFamilies = ["AF_INET" "AF_INET6" "AF_UNIX" "AF_NETLINK"];
-            RestrictNamespaces = "yes";
-            Restart = "on-failure";
-            RestartSec = "5";
-        };
-        Install = { WantedBy = ["default.target"]; };
-    };
+  systemd.user.services.mpd = {
+      Unit = {
+          Description = "Music Player Daemon";
+          Documentation = "man:mpd(1) man:mpd.conf(5)";
+          After = ["network.target" "sound.target"];
+          ConditionPathExists = "${pkgs.mpd}/bin/mpd";
+      };
+      Service = {
+          Type = "notify";
+          ExecStart = "${pkgs.mpd}/bin/mpd --no-daemon";
+          WatchdogSec = 120;
+          LimitRTPRIO = "40"; # allow MPD to use real-time priority 40
+          LimitRTTIME = "infinity";
+          LimitMEMLOCK = "64M"; # for io_uring
+          ProtectSystem = "yes"; # disallow writing to /usr, /bin, /sbin, ...
+          NoNewPrivileges = "yes";
+          ProtectKernelTunables = "yes";
+          ProtectControlGroups = "yes";
+          # AF_NETLINK is required by libsmbclient, or it will exit() .. *sigh*
+          RestrictAddressFamilies = ["AF_INET" "AF_INET6" "AF_UNIX" "AF_NETLINK"];
+          RestrictNamespaces = "yes";
+          Restart = "on-failure";
+          RestartSec = "5";
+      };
+      Install = { WantedBy = ["default.target"]; };
+  };
 
 }
