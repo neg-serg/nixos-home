5514221e2504b3baebae75d2634807d09c46304a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-16 13:46:17 +0300

    [nixpkgs] align

--- nix/.config/home-manager/dotfiles.nix
+++ nix/.config/home-manager/dotfiles.nix
@@ -92,9 +92,11 @@
             fi
         '';
         "${config.xdg.configHome}/nixpkgs/config.nix".text = ''
-                { packageOverrides = pkgs: {
-                    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { inherit pkgs; }; };
-                  permittedInsecurePackages = [ "electron-25.9.0" ];
+                { 
+                    packageOverrides = pkgs: {
+                        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { inherit pkgs; }; 
+                    };
+                    permittedInsecurePackages = [ "electron-25.9.0" ];
                 }
             '';
     };
