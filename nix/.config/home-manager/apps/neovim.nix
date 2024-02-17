c02a3e8bf0c9691a6bc3245cbb5e7e0379f993fe
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-17 21:05:36 +0300

    [nvim] refactoring

--- nix/.config/home-manager/apps/neovim.nix
+++ nix/.config/home-manager/apps/neovim.nix
@@ -3,17 +3,15 @@
     dots = "${config.home.homeDirectory}/.dotfiles";
 };{
     home.packages = with pkgs; [
-        cargo
-        manix
         neovim-remote # nvr for neovim
-        nodePackages.bash-language-server
-        nodePackages.pyright
-        rust-analyzer
+        nodePackages.bash-language-server # bash lsp
+        nodePackages.pyright # python lsp
+        rust-analyzer # rust lsp
         stable.nil # nixos language server
     ];
     programs.neovim.plugins = with pkgs.vimPlugins; [
-        clangd_extensions-nvim
-        nvim-treesitter.withAllGrammars
+        clangd_extensions-nvim # llvm-based engine
+        nvim-treesitter.withAllGrammars # ts support
     ];
     xdg.configFile = {
         # █▓▒░ nvim ─────────────────────────────────────────────────────────────────────────
