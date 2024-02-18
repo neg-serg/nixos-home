24b9b0c7aa98ad5dab4cd1d826e5f0dad92d4224
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-18 18:11:20 +0300

    [x11] refactoring

--- nix/.config/home-manager/apps/x11/apps.nix
+++ nix/.config/home-manager/apps/x11/apps.nix
@@ -1,14 +1,18 @@
 { pkgs, negwmPkg, ... }: {
-  home.packages = with pkgs; [
-      dunst # notification daemon
-      flameshot # interactive screenshot tool
-      herbe # notification without daemon and dbus
-      hsetroot # set x11 root image
-      i3 # my favorite wm
-      i3lock-fancy-rapid
-      maim # screenshot tool for x11
-      picom # x11 compositing
-      polybar # my favorite panel so far
-      negwmPkg.negwm # my own i3 helper
-    ];
+    home.packages = with pkgs; [
+        dunst # notification daemon
+        flameshot # interactive screenshot tool
+        herbe # notification without daemon and dbus
+        hsetroot # set x11 root image
+        i3lock-fancy-rapid
+        i3 # my favorite wm
+        maim # screenshot tool for x11
+        negwmPkg.negwm # my own i3 helper
+        picom # x11 compositing
+        polybar # my favorite panel so far
+  ];
+  services.sxhkd = {
+      enable = false;
+      keybindings = {};
+  };
 }
