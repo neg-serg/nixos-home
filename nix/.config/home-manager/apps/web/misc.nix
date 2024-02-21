2fa85096aa75968b93af3ac0ac74a7eefe021ae8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-21 20:02:19 +0300

    [mega] use stable

--- nix/.config/home-manager/apps/web/misc.nix
+++ nix/.config/home-manager/apps/web/misc.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, stable, ... }: {
   home.packages = with pkgs; [
       gallery-dl # download image galleries/collections
       magic-wormhole # tool for local file sharing
-      megacmd # cli for MEGA
       monolith # download all webpage stuff as one file
       pipe-viewer # lightweight youtube client
       plowshare # download/upload from popular sites
       prettyping # fancy ping
+      stable.megacmd # cli for MEGA
       whois # get domain info
       xidel # download webpage parts
       yt-dlp # download from youtube and another sources
