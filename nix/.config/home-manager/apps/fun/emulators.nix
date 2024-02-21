3d4448170fe5ff5e7601afd8546db55bf103c60d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-21 20:33:15 +0300

    [pkgs] add retroarch

--- nix/.config/home-manager/apps/fun/emulators.nix
+++ nix/.config/home-manager/apps/fun/emulators.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
       pcem # emulator for ibm pc and clones
+      retroarchFull # multiplatform emulator frontend
       yuzu-mainline # experimental Nintendo Switch emulator
-      # retroarchFull # multiplatform emulator frontend
   ];
 }
