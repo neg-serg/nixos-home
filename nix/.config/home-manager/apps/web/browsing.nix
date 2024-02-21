c185b51e632c35b24e2bfc67ee89a306e6f71418
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-21 16:02:26 +0300

    [web] proxychains not working

--- nix/.config/home-manager/apps/web/browsing.nix
+++ nix/.config/home-manager/apps/web/browsing.nix
@@ -2,7 +2,6 @@
   imports = [ ./floorp.nix ];
   home.packages = with pkgs; [
       passff-host # host app for the WebExtension PassFF
-      proxychains-ng # autoproxy
       tor torsocks # tor stuff
       tridactyl-native # native package for nix
   ];
