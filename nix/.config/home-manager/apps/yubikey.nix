f7b029a33a1724579617cdab2eeb97e092790e25
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-21 23:20:08 +0300

    [yubikey] some apps

--- nix/.config/home-manager/apps/yubikey.nix
+++ nix/.config/home-manager/apps/yubikey.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
+        yubikey-agent # ssh agent for yk
         yubikey-manager # yubikey manager cli
+        yubikey-personalization # ykinfo
     ];
 }
