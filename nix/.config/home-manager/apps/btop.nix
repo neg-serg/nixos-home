1e3cbbc5a17dd66d436822c97c7aa7869efcb8b0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-23 23:06:05 +0300

    [btop] use stylix theme

--- nix/.config/home-manager/apps/btop.nix
+++ nix/.config/home-manager/apps/btop.nix
@@ -2,8 +2,6 @@
     programs.btop = {
         enable = true;
         settings = {
-            color_theme = "flat-remix.theme";
-            theme_background = true; # set to False if you want terminal background transparency.
             truecolor = true;
             presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
             vim_keys = true;
