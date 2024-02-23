e886e79d4e6044c9e5667817684b04394d4ebe0e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-24 01:51:07 +0300

    [nvim] obsidian: cleanup config

--- nvim/.config/nvim/lua/plugins/text/obsidian-nvim.lua
+++ nvim/.config/nvim/lua/plugins/text/obsidian-nvim.lua
@@ -8,44 +8,45 @@ return {'epwalsh/obsidian.nvim', -- obsidian helpers for neovim
         'nvim-telescope/telescope.nvim'},
     config=function()
         require'obsidian'.setup({
-              dir="~/1st_level", -- Required, the path to your vault directory.
+              workspaces = {
+                  { name = "personal", path = "~/1st_level/", },
+              },
               new_notes_location="current_dir",
               completion={
-                nvim_cmp=true, -- If using nvim-cmp, otherwise set to false
-                min_chars=2, -- Trigger completion at 2 chars
+                  nvim_cmp=true, -- If using nvim-cmp, otherwise set to false
+                  min_chars=2, -- Trigger completion at 2 chars
               },
               mappings={
-                ["gf"]={ -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+                  ["gf"]={ -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
                   action=function()
-                    return require'obsidian'.util.gf_passthrough()
+                      return require'obsidian'.util.gf_passthrough()
                   end,
                   opts={ noremap=false, expr=true, buffer=true },
-                },
               },
+          },
               note_id_func=function(title)
-                local suffix=""
-                if title ~= nil then
-                  suffix=title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-                else
-                  for _=1, 4 do
-                    suffix=suffix .. string.char(math.random(65, 90))
+                  local suffix=""
+                  if title ~= nil then
+                      suffix=title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+                  else
+                      for _=1, 4 do
+                          suffix=suffix .. string.char(math.random(65, 90))
+                      end
                   end
-                end
-                return tostring(os.time()) .. "-" .. suffix
+                  return tostring(os.time()) .. "-" .. suffix
               end,
               disable_frontmatter=true,
               note_frontmatter_func=function(note)
-                local out={ id=note.id, aliases=note.aliases, tags=note.tags }
-                if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-                  for k, v in pairs(note.metadata) do
-                    out[k]=v
+                  local out={ id=note.id, aliases=note.aliases, tags=note.tags }
+                  if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+                      for k, v in pairs(note.metadata) do
+                          out[k]=v
+                      end
                   end
-                end
-                return out
+                  return out
               end,
-              backlinks={height=10, wrap=true},
               follow_url_func=function(url)
-                vim.fn.jobstart({"xdg-open", url})  -- linux
+                  vim.fn.jobstart({"xdg-open", url})  -- linux
               end,
               use_advanced_uri=true, -- https://github.com/Vinzent03/obsidian-advanced-uri
               open_app_foreground=false,
