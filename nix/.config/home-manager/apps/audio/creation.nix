c50134f961610621d93ae1d870b12a1d83af34a5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-24 04:11:46 +0300

    [audio] creation: new stuff

--- nix/.config/home-manager/apps/audio/creation.nix
+++ nix/.config/home-manager/apps/audio/creation.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
       bitwig-studio # great DAW
+      noisetorch # virtual microphone to suppress the noise
       reaper # А вот в рипере!
       renoise # modern tracker
       rnnoise # neural network noise reduction
       tenacity # audio editor
       yabridge # vst for linux
       yabridgectl # vst control for linux
+      zrythm # free audio workstation
   ];
 }
