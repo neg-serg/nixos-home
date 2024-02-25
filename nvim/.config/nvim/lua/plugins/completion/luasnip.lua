7c7a298a7c2a7222e6e1784414261c743080ae0e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-25 20:18:47 +0300

    [nvim] more snippets

--- nvim/.config/nvim/lua/plugins/completion/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/completion/luasnip.lua
@@ -7,6 +7,12 @@ return {'L3MON4D3/LuaSnip', -- snippets engine
         local status, vscode_snips = pcall(require, 'luasnip.loaders.from_vscode')
         if (not status) then return end
         vscode_snips.lazy_load()    
+        local status, snipmate_snips = pcall(require'luasnip.loaders.from_snipmate')
+        if (not status) then return end
+        snipmate_snips.lazy_load()
+        local status, luasnip_snips = pcall(require'luasnip.loaders.from_luasnip')
+        if (not status) then return end
+        luasnip_snips.lazy_load()
     end,
     dependencies={
         'rafamadriz/friendly-snippets', -- additional snippets'
