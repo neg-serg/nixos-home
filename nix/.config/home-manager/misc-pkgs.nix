ed97f33f10eb892fc07698b34bf3e9e9c1bb2155
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-25 20:20:24 +0300

    [nvim] python: more default modules

--- nix/.config/home-manager/misc-pkgs.nix
+++ nix/.config/home-manager/misc-pkgs.nix
@@ -22,7 +22,7 @@
     libsForQt5.qt5ct libsForQt5.qtstyleplugin-kvantum qt6Packages.qtstyleplugin-kvantum # kvantum theme support
     neomutt # email client
     pwgen # generate passwords
-    (python3-lto.withPackages (ps: with ps; [ docopt i3ipc psutil colored pynvim ]))
+    (python3-lto.withPackages (ps: with ps; [ docopt i3ipc psutil colored pynvim requests ]))
     # swayfx # i3 for wayland
     sway-unwrapped_git # i3 for wayland
   ];
