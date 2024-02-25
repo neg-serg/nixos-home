34368f41c25524504727e9f658a38738c2469858
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-25 20:19:50 +0300

    [nvim] lazy: better settings

--- nvim/.config/nvim/lua/01-plugins.lua
+++ nvim/.config/nvim/lua/01-plugins.lua
@@ -6,15 +6,20 @@ if not vim.loop.fs_stat(lazypath) then
         '--branch=stable', lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
-require'lazy'.setup({spec={
-    {import='plugins/performance'}, -- █▓▒░ Performance / Fixes
-    {import='plugins/generic'},     -- █▓▒░ Generic
-    {import='plugins/completion'},  -- █▓▒░ Completion
-    {import='plugins/dev'},         -- █▓▒░ Dev
-    {import='plugins/debug'},       -- █▓▒░ Debug
-    {import='plugins/text'},        -- █▓▒░ Text
-    {import='plugins/edit'},        -- █▓▒░ Edit
-    {import='plugins/appearance'},  -- █▓▒░ Appearance
-    {import='plugins/filetypes'},   -- █▓▒░ Filetypes
-    {import='plugins/dcvs'},        -- █▓▒░ DCVS
-}})
+require'lazy'.setup({
+    spec={
+        {import='plugins/performance'}, -- █▓▒░ Performance / Fixes
+        {import='plugins/generic'},     -- █▓▒░ Generic
+        {import='plugins/completion'},  -- █▓▒░ Completion
+        {import='plugins/dev'},         -- █▓▒░ Dev
+        {import='plugins/debug'},       -- █▓▒░ Debug
+        {import='plugins/text'},        -- █▓▒░ Text
+        {import='plugins/edit'},        -- █▓▒░ Edit
+        {import='plugins/appearance'},  -- █▓▒░ Appearance
+        {import='plugins/filetypes'},   -- █▓▒░ Filetypes
+        {import='plugins/dcvs'},        -- █▓▒░ DCVS
+    },
+    defaults={lazy=false},
+    install={colorscheme={"neg"}},
+    ui={icons={ft="", lazy="󰂠 ", loaded="", not_loaded=""},},
+})
