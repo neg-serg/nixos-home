f0dc627c514eeb40e3c5083594b44b076a9be36e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-26 03:06:47 +0300

    [nvim] disable md-bullets

--- nvim/.config/nvim/lua/plugins/text/md-bullets.lua
+++ nvim/.config/nvim/lua/plugins/text/md-bullets.lua
@@ -2,6 +2,7 @@
 -- │ █▓▒░ cstsunfu/md-bullets.nvim                                                     │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {'cstsunfu/md-bullets.nvim', -- markdown org-like bullets(better highlighting)
+        enabled=false,
         config=function() 
             local ok, bullets = pcall(require, 'md-bullets')
             if (not ok) then return end
