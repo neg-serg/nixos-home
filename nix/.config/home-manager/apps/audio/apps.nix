07b0f0189b1c5934c2061fa8533797c0f49fd9bb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-27 21:31:11 +0300

    [audio] sort

--- nix/.config/home-manager/apps/audio/apps.nix
+++ nix/.config/home-manager/apps/audio/apps.nix
@@ -1,25 +1,25 @@
 { pkgs, config, stable, ... }: {
   home.packages = with pkgs; [
       ape # monkey audio codec
-      stable.audiowaveform # shows soundwaveform
       cdparanoia # cdrip / cdrecord
       cider # apple music player
+      dr14_tmeter # compute the DR14 of a given audio file according to the procedure from Pleasurize Music Foundation
       easytag # use this for tags
       id3v2 # id3v2 edit
       jamesdsp # pipewire dsp
       mpc-cli # mpd client
-      opensoundmeter # sound measurement application for tuning audio systems in real-time
       ncmpcpp # curses mpd client
       ncpamixer # cli-pavucontrol
       nicotine-plus # download music via soulseek
+      opensoundmeter # sound measurement application for tuning audio systems in real-time
       picard # autotags
+      screenkey # screencast tool to display your keys inspired by Screenflick
       sonic-visualiser # audio analyzer
+      sox # audio processing
+      stable.audiowaveform # shows soundwaveform
+      streamlink  # CLI for extracting streams from websites
       tauon # fancy standalone music player
       unflac # split2flac alternative
-      dr14_tmeter # compute the DR14 of a given audio file according to the procedure from Pleasurize Music Foundation
-      screenkey # screencast tool to display your keys inspired by Screenflick
-      sox # audio processing
-      streamlink  # CLI for extracting streams from websites
   ];
 
   systemd.user.services = {
