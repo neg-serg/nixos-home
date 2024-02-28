9ab57618fb31a98532aa5d1f317682d8146f5c3d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-28 16:07:03 +0300

    [fhs] refactoring

--- nix/.config/home-manager/fhs/flake.nix
+++ nix/.config/home-manager/fhs/flake.nix
@@ -1,7 +1,10 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
-      (buildFHSEnv { name = "foo"; targetPkgs = pkgs: with pkgs; [hello]; }).env;
-  };
+    inputs.nixpkgs.url = "github:nixos/nixpkgs";
+    outputs = { self, nixpkgs }: {
+        devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
+        (buildFHSEnv {
+            name = "fhs-env";
+            targetPkgs = pkgs: with pkgs; [zsh];
+         }).env;
+    };
 }
