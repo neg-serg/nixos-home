e25d23e951e2d854d477fc68d24f4e51c134c9c8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-04 01:29:39 +0300

    [kitty] font fix

--- nix/.config/home-manager/apps/terminal/default.nix
+++ nix/.config/home-manager/apps/terminal/default.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, stable, ... }: {
   home.packages = with pkgs; [
       asciinema # record terminal
       chafa # terminal graphics
-      kitty # fastest terminal emulator so far
+      stable.kitty # fastest terminal emulator so far
       kitty-img # print images inline in kitty
       termplay # play video in terminal
   ];
