45c6bdaa5c9dd0d1223127a6d12d974ba4eb784f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-08 16:21:18 +0300

    [yuzu-mainline] remove

--- nix/.config/home-manager/apps/fun/emulators.nix
+++ nix/.config/home-manager/apps/fun/emulators.nix
@@ -2,6 +2,5 @@
   home.packages = with pkgs; [
       pcem # emulator for ibm pc and clones
       retroarchFull # multiplatform emulator frontend
-      yuzu-mainline # experimental Nintendo Switch emulator
   ];
 }
