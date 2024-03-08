6a0926a24ccf5d5a0eabba49b2ba6f2a500cb972
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-09 00:24:21 +0300

    [wayland] use default options with amd

--- nix/.config/home-manager/apps/wayland.nix
+++ nix/.config/home-manager/apps/wayland.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }: {
-    home.sessionVariables = {
-        WLR_BACKEND = "vulkan"; # nvidia compatibility
-        WLR_DRM_NO_ATOMIC = "1";
-        WLR_NO_HARDWARE_CURSORS = "1"; # nvidia compatibility
-        WLR_RENDERER = "vulkan"; # nvidia compatibility
-    };
+    home.sessionVariables = {};
     home.packages = with pkgs; [
         # fnott # wayland notifications
         # fuzzel # wayland launcher
@@ -13,7 +8,7 @@
         ydotool # xdotool systemwide
     ];
     wayland.windowManager.sway = {
-        extraOptions = [ "--unsupported-gpu" ];
+        extraOptions = [];
     };
 }
 
