0775b5c7bf3c0e73b75f97ab78b4d8c179bea8cd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-12 02:30:53 +0300

    [reaper] try fix

--- nix/.config/home-manager/apps/audio/creation.nix
+++ nix/.config/home-manager/apps/audio/creation.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, stable, ... }: {
+  nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
       bitwig-studio # great DAW
       noisetorch # virtual microphone to suppress the noise
-      reaper # А вот в рипере!
+      stable.reaper # А вот в рипере!
       renoise # modern tracker
       rnnoise # neural network noise reduction
       tenacity # audio editor
