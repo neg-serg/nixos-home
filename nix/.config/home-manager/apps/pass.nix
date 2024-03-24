a2459dbfeab42f56d2ab8ab66f17c448e7ce220d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-24 13:28:28 +0300

    [pass] fix install

--- nix/.config/home-manager/apps/pass.nix
+++ nix/.config/home-manager/apps/pass.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, stable, ... }: {
   home.packages = with pkgs; [
       # password manager via gpg
-      (pass.withExtensions (ext: with ext; [
+      (stable.pass.withExtensions (ext: with ext; [
           pass-audit # extension for auditing your password repository
           pass-otp # one time passwords integration
           pass-tomb # encrypt all password tree inside a tomb
