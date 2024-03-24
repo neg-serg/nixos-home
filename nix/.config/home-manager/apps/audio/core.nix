98fe80385046bcbf878eb82bea9585dada502701
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-24 13:28:41 +0300

    [pwnucontrol] fix install

--- nix/.config/home-manager/apps/audio/core.nix
+++ nix/.config/home-manager/apps/audio/core.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, stable, ... }: {
   home.packages = with pkgs; [
       alsa-utils # aplay -l and friends
       easyeffects # pipewire-based dsp via cpu
       helvum # pipewire router
       pw-volume # pipewire volume
-      pwvucontrol # pavucontrol for pipewire
+      stable.pwvucontrol # pavucontrol for pipewire
   ];
 }
