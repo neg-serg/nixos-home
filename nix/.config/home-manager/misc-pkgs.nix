ddc9a9a2161afc39eda48aea1afe5dcc27f564b5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-24 16:30:05 +0300

    [pkgs] CS

--- nix/.config/home-manager/misc-pkgs.nix
+++ nix/.config/home-manager/misc-pkgs.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
-       blesh # bluetooth shell
-       gnome.gpaste # clipboard manager
-       gnupg # encryption
-       imwheel # for mouse wheel scrolling
-       neomutt # email client
-       pwgen # generate passwords
+        blesh # bluetooth shell
+        gnome.gpaste # clipboard manager
+        gnupg # encryption
+        imwheel # for mouse wheel scrolling
+        neomutt # email client
+        pwgen # generate passwords
   ];
 }
