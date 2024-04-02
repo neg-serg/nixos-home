39cddce65e69cbbfd44083bee93296d0839d2e59
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-03 02:44:53 +0300

    [pkgs] remove easyeffects

--- nix/.config/home-manager/apps/audio/core.nix
+++ nix/.config/home-manager/apps/audio/core.nix
@@ -1,7 +1,6 @@
 { pkgs, stable, ... }: {
   home.packages = with pkgs; [
       alsa-utils # aplay -l and friends
-      easyeffects # pipewire-based dsp via cpu
       helvum # pipewire router
       pw-volume # pipewire volume
       stable.pwvucontrol # pavucontrol for pipewire
