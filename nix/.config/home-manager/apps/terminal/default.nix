82f2a71e07d61cd4f323dcd7b8a25943a2c226d4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-15 01:56:01 +0300

    [pkgs] try more master

--- nix/.config/home-manager/apps/terminal/default.nix
+++ nix/.config/home-manager/apps/terminal/default.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, master, ... }: {
   home.packages = with pkgs; [
       asciinema # record terminal
       chafa # terminal graphics
-      kitty # fastest terminal emulator so far
-      kitty-img # print images inline in kitty
+      master.kitty # fastest terminal emulator so far
+      master.kitty-img # print images inline in kitty
       termplay # play video in terminal
   ];
 }
