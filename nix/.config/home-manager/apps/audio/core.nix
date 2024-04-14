93157ab6e0729038cae9f00025529bc7a46f47e7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-15 01:33:16 +0300

    [audio] add patchbay patcher

--- nix/.config/home-manager/apps/audio/core.nix
+++ nix/.config/home-manager/apps/audio/core.nix
@@ -2,6 +2,7 @@
   home.packages = with pkgs; [
       alsa-utils # aplay -l and friends
       helvum # pipewire router
+      open-music-kontrollers.patchmatrix # alternative patcher
       pw-volume # pipewire volume
       stable.pwvucontrol # pavucontrol for pipewire
   ];
