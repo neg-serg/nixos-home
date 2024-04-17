a89b9b5106d63b56219360c8796916870d8b5d78
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-18 02:37:25 +0300

    [zsh] cleanup completion

--- shell/.config/zsh/03-completion.zsh
+++ shell/.config/zsh/03-completion.zsh
@@ -95,16 +95,4 @@ zstyle ':zle:*kill*' word-chars '*?_-.[]~=&;!#$%^(){}<>'
 zshcache_time="$(date +%s%N)"
 autoload -Uz add-zsh-hook
 
-rehash_precmd() {
-    if [[ -a /var/cache/zsh/pacman ]]; then
-        local paccache_time="$(date -r /var/cache/zsh/pacman +%s%N)"
-        if (( zshcache_time < paccache_time )); then
-            rehash
-            zshcache_time="$paccache_time"
-        fi
-    fi
-}
-
-add-zsh-hook -Uz precmd rehash_precmd
-
 # vim: ft=zsh:nowrap
