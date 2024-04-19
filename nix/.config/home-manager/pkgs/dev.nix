07543b12c2cdfe9337583ab2a62cf9a7f409744f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-20 02:59:34 +0300

    [pkgs] cleanup

--- nix/.config/home-manager/pkgs/dev.nix
+++ nix/.config/home-manager/pkgs/dev.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-      # chez # Chez Scheme (useful for idris)
-      # cosmocc # Cosmopolitan (Actually Portable Executable) C/C++ toolchain; use via CC=cosmocc, CXX=cosmoc++
-      # idris2 # Idris2 functional statically-typed programming language that looks cool and compiles to C
-      # micromamba # for python env
       cloc # count lines of code
       deheader # remove unneeded includes for c, cpp
       flawfinder # examine c, cpp code for security flaws
