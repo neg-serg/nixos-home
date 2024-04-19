b4bb8ecb8e99f437f6c12f11dca36aa9b9cf47fd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-20 02:23:16 +0300

    [nix] remove nixfmt

--- nix/.config/home-manager/pkgs/dev.nix
+++ nix/.config/home-manager/pkgs/dev.nix
@@ -8,7 +8,6 @@
       deheader # remove unneeded includes for c, cpp
       flawfinder # examine c, cpp code for security flaws
       mypy # optional static-typing for python
-      nixfmt # nix formatter
       shfmt # shell formatting
       stylua # lua styler
       tokei # count your code, quickly
