9ffe19a8177a3517d47d3113dcd478eb656917db
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 19:39:11 +0300

    [sops] CS

--- nix/.config/home-manager/sops.nix
+++ nix/.config/home-manager/sops.nix
@@ -1,18 +1,18 @@
-{ config, ... }: {
-    sops = {
-        age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
-        defaultSopsFile = ./secrets/all.yaml;
-        secrets = {
-            "mpdas_negrc" = {
-                format = "binary";
-                sopsFile = ./secrets/mpdas/neg.rc;
-                path = "/run/user/1000/secrets/mpdas_negrc";
-            };
-            "musicbrainz.yaml" = {
-                format = "binary";
-                sopsFile = ./secrets/musicbrainz;
-                path = "/run/user/1000/secrets/musicbrainz.yaml";
-            };
-        };
+{config, ...}: {
+  sops = {
+    age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+    defaultSopsFile = ./secrets/all.yaml;
+    secrets = {
+      "mpdas_negrc" = {
+        format = "binary";
+        sopsFile = ./secrets/mpdas/neg.rc;
+        path = "/run/user/1000/secrets/mpdas_negrc";
+      };
+      "musicbrainz.yaml" = {
+        format = "binary";
+        sopsFile = ./secrets/musicbrainz;
+        path = "/run/user/1000/secrets/musicbrainz.yaml";
+      };
     };
- }
+  };
+}
