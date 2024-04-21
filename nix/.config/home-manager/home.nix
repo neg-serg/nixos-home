8e87dac7fae5500270a289b98c973fb1581adda2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 19:39:03 +0300

    [home] CS

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -1,16 +1,16 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   nix.package = pkgs.nix;
   imports = [
-      ./dotfiles.nix
-      ./pkgs
-      ./sops.nix
-      ./systemd
-      ./theme
-      ./xdg.nix
+    ./dotfiles.nix
+    ./pkgs
+    ./sops.nix
+    ./systemd
+    ./theme
+    ./xdg.nix
   ];
   home = {
-      homeDirectory = "/home/neg";
-      stateVersion = "23.11"; # Please read the comment before changing.
-      username = "neg";
+    homeDirectory = "/home/neg";
+    stateVersion = "23.11"; # Please read the comment before changing.
+    username = "neg";
   };
 }
