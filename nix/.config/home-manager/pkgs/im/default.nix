1cdbccf54486ef97377e513aa39c0534930d7311
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 20:13:19 +0300

    [pkgs] use basic telegram

--- nix/.config/home-manager/pkgs/im/default.nix
+++ nix/.config/home-manager/pkgs/im/default.nix
@@ -3,7 +3,7 @@
     betterdiscord-installer
     betterdiscordctl # better discord
     discord # audio / video calls for gaming
-    telegram-desktop_git # famous cloud-based im
+    telegram-desktop # famous cloud-based im
     zoom-us # weird shit
   ];
 }
