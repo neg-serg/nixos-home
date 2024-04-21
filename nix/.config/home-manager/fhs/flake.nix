56b5785ca39b716544fd03548613234f5ebe58a2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 19:38:55 +0300

    [fhs] CS

--- nix/.config/home-manager/fhs/flake.nix
+++ nix/.config/home-manager/fhs/flake.nix
@@ -1,10 +1,14 @@
 {
-    inputs.nixpkgs.url = "github:nixos/nixpkgs";
-    outputs = { self, nixpkgs }: {
-        devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
-        (buildFHSEnv {
-            name = "fhs-env";
-            targetPkgs = pkgs: with pkgs; [zsh];
-         }).env;
-    };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
+      (buildFHSEnv {
+        name = "fhs-env";
+        targetPkgs = pkgs: with pkgs; [zsh];
+      })
+      .env;
+  };
 }
