1d9a56406bdc5c4ad5bdff552aef02c5d55487d4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-22 23:23:44 +0300

    [pkgs] cleanup

--- nix/.config/home-manager/pkgs/x11/stuff.nix
+++ nix/.config/home-manager/pkgs/x11/stuff.nix
@@ -9,7 +9,6 @@
     xsel # cli for X session clipboard manipulation
     xdo # X11 automation
     xdotool # another X11 automation
-    xiccd
     xorg.xev # X11 window to debug events
     xorg.xkill # kill X11 window via cursor or id
     xorg.xset # X11 settings rule
