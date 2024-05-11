2cb67684e582cef1201f34c9b3c5a6b17d994b30
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-05-11 17:48:01 +0300

    [audio] add coppwr

--- nix/.config/home-manager/pkgs/audio/core.nix
+++ nix/.config/home-manager/pkgs/audio/core.nix
@@ -5,6 +5,7 @@
 }: {
   home.packages = with pkgs; [
     alsa-utils # aplay -l and friends
+    coppwr # low level control for pipewire
     helvum # pipewire router
     open-music-kontrollers.patchmatrix # alternative patcher
     pw-volume # pipewire volume
