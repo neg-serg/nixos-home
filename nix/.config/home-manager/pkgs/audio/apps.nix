f3524359cf42923a32ba09787a00358a4151173f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-05-26 14:43:20 +0300

    [audio/apps] add REW

--- nix/.config/home-manager/pkgs/audio/apps.nix
+++ nix/.config/home-manager/pkgs/audio/apps.nix
@@ -5,21 +5,22 @@
   ...
 }: {
   home.packages = with pkgs; [
-    master.ape # monkey audio codec
     cdparanoia # cdrip / cdrecord
     cider # apple music player
     dr14_tmeter # compute the DR14 of a given audio file according to the procedure from Pleasurize Music Foundation
     id3v2 # id3v2 edit
     jamesdsp # pipewire dsp
+    master.ape # monkey audio codec
     master.mpc-cli # mpd client
     master.ncmpcpp # curses mpd client
     master.ncpamixer # cli-pavucontrol
+    master.picard # autotags
+    master.sox # audio processing
     nicotine-plus # download music via soulseek
     opensoundmeter # sound measurement application for tuning audio systems in real-time
-    master.picard # autotags
+    roomeqwizard # room acoustics software
     screenkey # screencast tool to display your keys inspired by Screenflick
     sonic-visualiser # audio analyzer
-    master.sox # audio processing
     stable.audiowaveform # shows soundwaveform
     stable.streamlink # CLI for extracting streams from websites
     tauon # fancy standalone music player
