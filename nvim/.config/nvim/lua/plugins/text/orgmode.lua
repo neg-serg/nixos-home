40a627f895cf3d19a246b343fa5e3e1e8da80551
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-06 03:20:33 +0300

    [nvim] org-mode: align

--- nvim/.config/nvim/lua/plugins/text/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/text/orgmode.lua
@@ -2,11 +2,11 @@
 -- │ █▓▒░ nvim-orgmode/orgmode                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {'nvim-orgmode/orgmode', config=function() -- orgmode support
-        require'orgmode'.setup_ts_grammar() -- Load treesitter grammar for org
-        -- Setup orgmode
-        require'orgmode'.setup({
-            org_agenda_files='~/orgfiles/**/*',
-            org_default_notes_file='~/orgfiles/refile.org',
-        })
-        end,
-        event={'VeryLazy'}}
+    require'orgmode'.setup_ts_grammar() -- Load treesitter grammar for org
+    -- Setup orgmode
+    require'orgmode'.setup({
+        org_agenda_files='~/orgfiles/**/*',
+        org_default_notes_file='~/orgfiles/refile.org',
+    })
+    end,
+    event={'VeryLazy'}}
