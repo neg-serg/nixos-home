964cafff04acd79990b42c04a6636c998fcf877e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-08 00:16:11 +0300

    [nvim] org-mode cleanup

--- nvim/.config/nvim/lua/plugins/text/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/text/orgmode.lua
@@ -2,8 +2,6 @@
 -- │ █▓▒░ nvim-orgmode/orgmode                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {'nvim-orgmode/orgmode', config=function() -- orgmode support
-    require'orgmode'.setup_ts_grammar() -- Load treesitter grammar for org
-    -- Setup orgmode
     require'orgmode'.setup({
         org_agenda_files='~/orgfiles/**/*',
         org_default_notes_file='~/orgfiles/refile.org',
