81460cd907940287af4335c6c14fb777c8e2ab47
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-09 02:52:31 +0300

    [clipboard] try new tools

--- nix/.config/home-manager/pkgs/graphical-session/apps.nix
+++ nix/.config/home-manager/pkgs/graphical-session/apps.nix
@@ -2,8 +2,14 @@
   pkgs,
   ...
 }:
-{
+with {
+  clipboard-sync = pkgs.callPackage ../../packages/clipboard-sync {};
+}; {
   home.packages = with pkgs; [
     activitywatch # track your activity on pc
+    autocutsel
+    clipcat
+    clipboard-jh
+    clipboard-sync
   ];
 }
