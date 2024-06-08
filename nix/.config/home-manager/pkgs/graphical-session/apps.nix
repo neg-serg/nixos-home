0ea76716877ec651a7282f3c5bc7dae25265ad8a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-09 02:54:13 +0300

    [graphical-session] remove clipboard-sync

--- nix/.config/home-manager/pkgs/graphical-session/apps.nix
+++ nix/.config/home-manager/pkgs/graphical-session/apps.nix
@@ -2,14 +2,11 @@
   pkgs,
   ...
 }:
-with {
-  clipboard-sync = pkgs.callPackage ../../packages/clipboard-sync {};
-}; {
+{
   home.packages = with pkgs; [
     activitywatch # track your activity on pc
     autocutsel
     clipcat
     clipboard-jh
-    clipboard-sync
   ];
 }
