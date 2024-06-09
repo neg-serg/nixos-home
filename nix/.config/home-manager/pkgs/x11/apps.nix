17cc87a6ccaab2c2bb7a867a3d4602fac7e7a8b1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-10 00:18:23 +0300

    [x11] new apps to try

--- nix/.config/home-manager/pkgs/x11/apps.nix
+++ nix/.config/home-manager/pkgs/x11/apps.nix
@@ -10,22 +10,24 @@ with {
   i3-get-window-criteria = pkgs.callPackage ../../packages/i3-get-window-criteria {};
   i3-lock-fancy-rapid = pkgs.callPackage ../../packages/i3lock-fancy-rapid {};
   i3-balance-workspace = pkgs.callPackage ../../packages/i3-balance-workspace {};
+  rofi-games = pkgs.callPackage ../../packages/rofi-games {};
   # dup-img-finder = pkgs.callPackage ../../packages/dup-img-finder {};
 }; {
   home.packages = with pkgs; [
     dunst # notification daemon
+    # dup-img-finder # TRY: img duplicates finder
     flameshot # interactive screenshot tool
     herbe # notification without daemon and dbus
     hsetroot # set x11 root image
-    # dup-img-finder # TRY: img duplicates finder
+    i3-balance-workspace # TRY: balance workspaces
     i3-get-window-criteria # xwindowinfo (test)
     i3lock-fancy-rapid # TRY: better lock
-    i3-balance-workspace # TRY: balance workspaces
     i3 # my favorite wm
     maim # screenshot tool for x11
     negwmPkg.negwm # my own i3 helper
     picom # x11 compositing
     polybar # my favorite panel so far
+    rofi-games # TRY: rofi launcher for games
   ];
   services.sxhkd = {
     enable = false;
