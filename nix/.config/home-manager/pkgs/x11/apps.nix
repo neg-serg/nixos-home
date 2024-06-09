5a63ecfed9a0ac10c863a135dc6602c15c4b2780
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-10 01:00:13 +0300

    [x11] apps: remove crap

--- nix/.config/home-manager/pkgs/x11/apps.nix
+++ nix/.config/home-manager/pkgs/x11/apps.nix
@@ -10,12 +10,9 @@ with {
   i3-get-window-criteria = pkgs.callPackage ../../packages/i3-get-window-criteria {};
   i3-lock-fancy-rapid = pkgs.callPackage ../../packages/i3lock-fancy-rapid {};
   i3-balance-workspace = pkgs.callPackage ../../packages/i3-balance-workspace {};
-  rofi-games = pkgs.callPackage ../../packages/rofi-games {};
-  # dup-img-finder = pkgs.callPackage ../../packages/dup-img-finder {};
 }; {
   home.packages = with pkgs; [
     dunst # notification daemon
-    # dup-img-finder # TRY: img duplicates finder
     flameshot # interactive screenshot tool
     herbe # notification without daemon and dbus
     hsetroot # set x11 root image
@@ -27,7 +24,6 @@ with {
     negwmPkg.negwm # my own i3 helper
     picom # x11 compositing
     polybar # my favorite panel so far
-    rofi-games # TRY: rofi launcher for games
   ];
   services.sxhkd = {
     enable = false;
