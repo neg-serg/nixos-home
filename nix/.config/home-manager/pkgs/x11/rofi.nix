563b569477b0afa94c8ad37f4ccf161122a71e9a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-10 01:09:38 +0300

    [rofi] try install rofi-games

--- nix/.config/home-manager/pkgs/x11/rofi.nix
+++ nix/.config/home-manager/pkgs/x11/rofi.nix
@@ -1,6 +1,11 @@
-{pkgs, ...}: {
+{pkgs, ...}: with {
+  rofi-games = pkgs.callPackage ../../packages/rofi-games {};
+}; {
   home.packages = with pkgs; [
-    (rofi.override {plugins = [rofi-file-browser];}) # modern dmenu alternative
+    (rofi.override {plugins = [
+        rofi-file-browser
+        rofi-games
+    ];}) # modern dmenu alternative
     rofi-pass # pass integration for pass
   ];
 }
