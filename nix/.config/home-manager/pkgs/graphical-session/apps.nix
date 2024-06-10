02467464d67d9f3d24957920f79da3821e5dccaf
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-10 15:25:06 +0300

    [activitywatch] disable for now for test

--- nix/.config/home-manager/pkgs/graphical-session/apps.nix
+++ nix/.config/home-manager/pkgs/graphical-session/apps.nix
@@ -4,7 +4,6 @@
 }:
 {
   home.packages = with pkgs; [
-    activitywatch # track your activity on pc
     autocutsel # tool to sync x11 buffers
     clipboard-jh # platform independent clipboard manager, test it more later
     clipcat # replacement for gpaste
