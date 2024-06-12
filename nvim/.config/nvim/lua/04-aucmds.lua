6310afab800280ee00ec514b02fc828fc3009351
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-12 21:41:19 +0300

    [nvim] aucmds: set some filetypes

--- nvim/.config/nvim/lua/04-aucmds.lua
+++ nvim/.config/nvim/lua/04-aucmds.lua
@@ -63,3 +63,19 @@ au({'BufNewFile','BufWritePre'}, {pattern={'*'},
     command=[[if @% !~# '\(://\)' | call mkdir(expand('<afile>:p:h'), 'p') | endif]],
     group=utils
 })
+
+vim.g.markdown_fenced_languages={'shell=bash'}
+local file_syntax_map={
+    {pattern='*.rasi',     syntax='scss'},
+    {pattern='flake.lock', syntax='json'},
+    {pattern='*.ignore',   syntax='gitignore'}, -- also ignore for fd/ripgrep
+    {pattern='*.ojs',      syntax='javascript'},
+    {pattern='*.astro',    syntax='astro'},
+    {pattern='*.mdx',      syntax='mdx'}
+}
+for _, elem in ipairs(file_syntax_map) do
+    au({'BufNewFile', 'BufRead'}, {
+        pattern=elem.pattern,
+        command='set syntax=' .. elem.syntax,
+   })
+end
