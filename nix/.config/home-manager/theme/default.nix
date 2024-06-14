3ad0a2ac4fc4162bbfdc4e33b1666c224cead634
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-14 03:10:57 +0300

    [stylix] now needs to be enabled explicitly

--- nix/.config/home-manager/theme/default.nix
+++ nix/.config/home-manager/theme/default.nix
@@ -72,6 +72,7 @@ with {
   };
 
   stylix = {
+    enable = true;
     image = pkgs.fetchurl {
       url = "https://i.imgur.com/t3bTk2b.jpg";
       sha256 = "sha256-WVDIxyy9ww39JNFkMOJA2N6KxLMh9cKjmeQwLY7kCjk=";
