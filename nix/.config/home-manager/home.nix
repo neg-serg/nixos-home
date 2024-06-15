fc167488f098fe64bdb2dd1ea1d00e251d003752
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-15 13:51:58 +0300

    [schizofox] code style

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -1,4 +1,7 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  ...
+}: {
   nix.package = pkgs.nix;
   imports = [
     ./dotfiles.nix
--- nix/.config/home-manager/pkgs/web/schizofox.nix
+++ nix/.config/home-manager/pkgs/web/schizofox.nix
@@ -1,4 +1,5 @@
-{...}: {
+{inputs, ...}: {
+  imports = [inputs.schizofox.homeManagerModule];
   programs.schizofox = {
     enable = true;
     theme = {
@@ -50,32 +51,17 @@
       simplefox.enable = true;
       darkreader.enable = true;
       extraExtensions = {
-        "uBlock0@raymondhill.net".install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        "flagfox".install_url = "https://addons.mozilla.org/firefox/downloads/latest/flagfox/latest.xpi";
+        # "uBlock0@raymondhill.net".install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        # "flagfox".install_url = "https://addons.mozilla.org/firefox/downloads/latest/flagfox/latest.xpi";
       };
       # extraExtensions = let
       #   mkUrl = name: "https://addons.mozilla.org/firefox/downloads/latest/${name}/latest.xpi";
       #   extensions = [
-      #     {
-      #       id = "1018e4d6-728f-4b20-ad56-37578a4de76";
-      #       name = "flagfox";
-      #     }
-      #     {
-      #       id = "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}";
-      #       name = "auto-tab-discard";
-      #     }
-      #     {
-      #       id = "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}";
-      #       name = "refined-github-";
-      #     }
-      #     {
-      #       id = "sponsorBlocker@ajay.app";
-      #       name = "sponsorblock";
-      #     }
-      #     {
-      #       id = "{446900e4-71c2-419f-a6a7-df9c091e268b}";
-      #       name = "bitwarden-password-manager";
-      #     }
+      #     { id = "1018e4d6-728f-4b20-ad56-37578a4de76"; name = "flagfox"; }
+      #     { id = "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}"; name = "auto-tab-discard"; }
+      #     { id = "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}"; name = "refined-github-"; }
+      #     { id = "sponsorBlocker@ajay.app"; name = "sponsorblock"; }
+      #     { id = "{446900e4-71c2-419f-a6a7-df9c091e268b}"; name = "bitwarden-password-manager"; }
       #   ];
       #   extraExtensions = builtins.foldl' (acc: ext: acc // {ext.id = {install_url = mkUrl ext.name;};}) {} extensions ++ [
       #       "uBlock0@raymondhill.net".install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
