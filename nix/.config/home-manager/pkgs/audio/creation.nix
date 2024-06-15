7214cfb0d81167af815e967b4a37ed38d1a168e2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-15 12:55:07 +0300

    [creation] disable renoise for now

--- nix/.config/home-manager/pkgs/audio/creation.nix
+++ nix/.config/home-manager/pkgs/audio/creation.nix
@@ -3,7 +3,7 @@
     bitwig-studio # great DAW
     noisetorch # virtual microphone to suppress the noise
     reaper # А вот в рипере!
-    renoise # modern tracker
+    # renoise # modern tracker
     rnnoise # neural network noise reduction
     tenacity # audio editor
     zrythm # free audio workstation
