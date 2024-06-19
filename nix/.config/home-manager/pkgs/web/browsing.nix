5835ad8a148cf2ceef81b885a1a9f504ebd49b5e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-19 23:44:05 +0300

    [tor] use stable tor

--- nix/.config/home-manager/pkgs/web/browsing.nix
+++ nix/.config/home-manager/pkgs/web/browsing.nix
@@ -1,11 +1,11 @@
-{pkgs, ...}: {
+{pkgs, stable, ...}: {
   imports = [
       ./floorp.nix
   ];
   home.packages = with pkgs; [
     passff-host # host app for the WebExtension PassFF
-    tor-browser # browse web via tor
-    tor # anti-censorship network
+    stable.tor-browser # browse web via tor
+    stable.tor # anti-censorship network
     torsocks # tor stuff
     tridactyl-native # native package for nix
   ];
