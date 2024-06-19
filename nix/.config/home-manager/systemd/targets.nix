3aa882be9a8566ec057bd00a21568a254e8c996b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-19 23:37:27 +0300

    [systemd] sway cleanup for now

--- nix/.config/home-manager/systemd/targets.nix
+++ nix/.config/home-manager/systemd/targets.nix
@@ -17,14 +17,4 @@
       ];
     };
   };
-
-  # systemd.user.targets.sway-session = {
-  #       Unit = {
-  #           Description = "sway compositor session";
-  #           Documentation = "man:systemd.special(7)";
-  #           BindsTo = ["graphical-session.target"];
-  #           Wants = ["graphical-session-pre.target"];
-  #           After = ["graphical-session-pre.target"];
-  #       };
-  # };
 }
