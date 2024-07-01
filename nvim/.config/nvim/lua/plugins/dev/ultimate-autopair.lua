063a56b60241168a3b08029cf99456eb0cfae49d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-01 14:55:21 +0300

    [nvim] fix ultimate-autopair

--- nvim/.config/nvim/lua/plugins/dev/ultimate-autopair.lua
+++ nvim/.config/nvim/lua/plugins/dev/ultimate-autopair.lua
@@ -2,7 +2,7 @@
 -- │ █▓▒░ altermo/ultimate-autopair.nvim                                               │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {'altermo/ultimate-autopair.nvim', -- better autopair
-        config={cmap=false}, -- remove cmdline autoclose
-        event={'InsertEnter','CmdlineEnter'},
-        branch='v0.6', --recomended as each new version will have breaking changes
-        opts={}}
+    event={'InsertEnter','CmdlineEnter'},
+    branch='v0.6', --recomended as each new version will have breaking changes
+    opts={cmap=false}  -- remove cmdline autoclose
+}
