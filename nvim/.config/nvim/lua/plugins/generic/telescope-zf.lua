154b484c5b4409e95a159a2a1130233debb3ad48
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-01 20:59:52 +0300

    [nvim] hide telescope-zf for now

--- nvim/.config/nvim/lua/plugins/generic/telescope-zf.lua
+++ nvim/.config/nvim/lua/plugins/generic/telescope-zf.lua
@@ -1,7 +1,7 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ natecraddock/telescope-zf-native.nvim                                        │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'natecraddock/telescope-zf-native.nvim', -- telescope-sorter via zf integration
-    config=function() require("telescope").load_extension("zf-native") end}
-    opts={},
-}
+-- return {'natecraddock/telescope-zf-native.nvim', -- telescope-sorter via zf integration
+--     config=function() require("telescope").load_extension("zf-native") end}
+--     opts={},
+-- }
