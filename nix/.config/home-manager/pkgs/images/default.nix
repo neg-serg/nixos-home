e96d1986f6ed67c9bf582cdf9d085a7dfc82f011
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-03 01:47:35 +0300

    [images] add lutgen

--- nix/.config/home-manager/pkgs/images/default.nix
+++ nix/.config/home-manager/pkgs/images/default.nix
@@ -4,7 +4,6 @@ with {
 }; {
   home.packages = with pkgs; [
     advancecomp # AdvanceCOMP PNG Compression Utility
-    stable.darktable # photo editing
     exiftool # extract media metadata
     exiv2 # metadata manipulation
     gcolor3 # color selector
@@ -12,12 +11,14 @@ with {
     graphviz # graphics
     jpegoptim # jpeg optimization
     krita # digital painting
+    lutgen # fast lut generator
     mediainfo # another tool to extract media info
     nsxiv-neg # my favorite image viewer
     optipng # optimize png
     pngquant # convert png from RGBA to 8 bit with alpha-channel
     qrencode # qr encoding
     scour # svg optimizer
+    stable.darktable # photo editing
     zbar # bar code reader
   ];
 }
