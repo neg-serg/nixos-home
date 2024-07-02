c4dee8a401414a2083fa475d1a15e0581b86c7c1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-02 19:36:33 +0300

    [nvim] glance: better defaults

--- nvim/.config/nvim/lua/plugins/dev/glance.lua
+++ nvim/.config/nvim/lua/plugins/dev/glance.lua
@@ -1,34 +1,23 @@
 return {'DNLHC/glance.nvim', -- glance neovim for nice navigation
 config=function()
-    -- Lua configuration
-    local glance=require('glance')
+    local glance=require'glance'
     local actions=glance.actions
-
     glance.setup({
         height=18, -- Height of the window
         zindex=45,
-
         -- By default glance will open preview "embedded" within your active window
         -- when `detached` is enabled, glance will render above all existing windows
         -- and won't be restiricted by the width of your active window
         detached=true,
-
         -- Or use a function to enable `detached` only when the active window is too small
         -- (default behavior)
         detached=function(winid)
             return vim.api.nvim_win_get_width(winid) < 100
         end,
-
-        preview_win_opts={ -- Configure preview window options
-            cursorline=true,
-            number=true,
-            wrap=true,
-        },
-        border={
-            enable=false, -- Show window borders. Only horizontal borders allowed
-            top_char='―',
-            bottom_char='―',
-        },
+        -- Configure preview window options
+        preview_win_opts={cursorline=true, number=true, wrap=true},
+        -- Show window borders. Only horizontal borders allowed
+        border={enable=false, top_char='―', bottom_char='―',},
         list={
             position='right', -- Position of the list window 'left'|'right'
             width=0.33, -- 33% width relative to the active window, min 0.1, max 0.5
@@ -68,20 +57,32 @@ config=function()
                 ['<leader>l']=actions.enter_win('list'), -- Focus list window
             },
         },
-        hooks={},
+        hooks={
+            before_open=function(results, open, jump, method)
+                local uri=vim.uri_from_bufnr(0)
+                if #results == 1 then
+                    local target_uri=results[1].uri or results[1].targetUri
+
+                    if target_uri == uri then
+                        jump(results[1])
+                    else
+                        open(results)
+                    end
+                else
+                    open(results)
+                end
+            end,
+        },
         folds={
             fold_closed='',
             fold_open='',
             folded=true, -- Automatically fold list on startup
         },
-        indent_lines={
-            enable=true,
-            icon='│',
-        },
+        indent_lines={enable=true, icon='│'},
         winbar={
             enable=true, -- Available strating from nvim-0.8+
         },
-        use_trouble_qf=false -- Quickfix action will open trouble.nvim instead of built-in quickfix list window
+        use_trouble_qf=true -- Quickfix action will open trouble.nvim instead of built-in quickfix list window
     })
     vim.keymap.set('n', 'gD', '<CMD>Glance definitions<CR>')
     vim.keymap.set('n', 'gR', '<CMD>Glance references<CR>')
