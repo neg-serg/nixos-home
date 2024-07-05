64398d7de43419f593fafeb62ef4928f6d2b34e6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-06 01:57:00 +0300

    [web] cleanup

--- nix/.config/home-manager/pkgs/web/browsing.nix
+++ nix/.config/home-manager/pkgs/web/browsing.nix
@@ -1,11 +1,10 @@
-{pkgs, stable, ...}: {
+{pkgs, ...}: {
   imports = [
       ./floorp.nix
   ];
   home.packages = with pkgs; [
     passff-host # host app for the WebExtension PassFF
-    stable.tor-browser # browse web via tor
-    stable.tor # anti-censorship network
+    tor-browser # browse web via tor
     torsocks # tor stuff
     tridactyl-native # native package for nix
   ];
