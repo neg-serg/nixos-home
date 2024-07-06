af4cadad52d026160955c6f3e9ac7c64544f7883
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-06 17:17:57 +0300

    [modules] refactoring-7

--- nix/.config/home-manager/modules/default.nix
+++ nix/.config/home-manager/modules/default.nix
@@ -3,14 +3,11 @@
     ./cli
     ./dev
     ./distros
-    ./dotfiles
-    ./fonts
     ./hardware
     ./main
     ./media
     ./misc
     ./secrets
-    ./systemd
     ./text
     ./user
   ];
--- nix/.config/home-manager/modules/user/default.nix
+++ nix/.config/home-manager/modules/user/default.nix
@@ -1,11 +1,14 @@
 {
   imports = [
+    ./dotfiles
     ./envs
+    ./fonts
     ./fun
     ./games
     ./gui
     ./im
     ./mail
+    ./systemd
     ./terminal
     ./theme
     ./torrent
