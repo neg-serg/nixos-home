3abfa789e0d1fcbecdfb353f35e035e59b2d800d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-07 16:34:36 +0300

    [zsh] carbonyl add fun stuff

--- shell/.config/zsh/02-cmds.zsh
+++ shell/.config/zsh/02-cmds.zsh
@@ -222,6 +222,7 @@ if [[ -e /etc/NIXOS ]]; then
 fi
 
 _exists docker && {
+    carbonyl(){docker run --rm -ti fathyb/carbonyl https://youtube.com}
     ipmi_one(){ docker run -p 127.0.0.1:5900:5900 -p 127.0.0.1:8080:8080 gari123/ipmi-kvm-docker; echo xdg-open http://127.0.0.1:8080|xsel }
     ipmi_two(){ docker run -p 8080:8080 solarkennedy/ipmi-kvm-docker; echo xdg-open localhost:8080|xsel }
 }
