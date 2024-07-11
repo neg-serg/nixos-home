4f276a0150f60652d6550595c5497e22775ba8f1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-12 00:11:03 +0300

    [plowshare] broken

--- nix/.config/home-manager/modules/user/web/misc.nix
+++ nix/.config/home-manager/modules/user/web/misc.nix
@@ -7,7 +7,7 @@
     gallery-dl # download image galleries/collections
     monolith # download all webpage stuff as one file
     pipe-viewer # lightweight youtube client
-    plowshare # download/upload from popular sites
+    # plowshare # download/upload from popular sites
     prettyping # fancy ping
     stable.megacmd # cli for MEGA
     whois # get domain info
