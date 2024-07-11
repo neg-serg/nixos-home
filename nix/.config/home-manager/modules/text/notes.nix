0945240531c19632d57b6f520f2593002bcec7a1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-11 17:25:43 +0300

    [home] stash

--- nix/.config/home-manager/modules/text/notes.nix
+++ nix/.config/home-manager/modules/text/notes.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = ["electron-25.9.0"];
+  nixpkgs.config.permittedInsecurePackages = [
+      "electron-25.9.0"
+      "python3.12-youtube-dl-2021.12.17"
+  ];
   home.packages = with pkgs; [
     # obsidian # notes
     zk # notes database
