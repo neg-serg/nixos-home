8ef87430b5f8104ec3f591b368217c81319b58bc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-12 00:11:16 +0300

    [floorp] broken now

--- nix/.config/home-manager/modules/user/web/floorp.nix
+++ nix/.config/home-manager/modules/user/web/floorp.nix
@@ -1,10 +1,10 @@
 {master, ...}: {
-  # home.packages = [master.floorp]; # Module installing  as default browser
-  # home.sessionVariables = {
-  #   DEFAULT_BROWSER = "${master.floorp}/bin/floorp";
-  #   MOZ_DBUS_REMOTE = "1";
-  #   MOZ_ENABLE_WAYLAND = "1";
-  # };
+  home.packages = [master.firefox]; # Module installing  as default browser
+  home.sessionVariables = {
+    DEFAULT_BROWSER = "${master.firefox}/bin/firefox";
+    MOZ_DBUS_REMOTE = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
   xdg.mimeApps.defaultApplications = {
     "text/html" = "floorp.desktop";
     "x-scheme-handler/http" = "floorp.desktop";
