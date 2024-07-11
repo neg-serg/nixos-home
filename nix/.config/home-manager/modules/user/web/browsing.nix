ce7982a290049eaf0b1704dd07873f1493e9ce0d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-12 00:11:31 +0300

    [browsing] cleanup

--- nix/.config/home-manager/modules/user/web/browsing.nix
+++ nix/.config/home-manager/modules/user/web/browsing.nix
@@ -1,15 +1,13 @@
-{pkgs, master, stable, ...}: {
+{pkgs, ...}: {
   imports = [
-    # ./floorp.nix
+    ./floorp.nix
   ];
   home.packages = with pkgs; [
-    # master.chromium
-    # master.firefox
-    # master.chromium
-    # master.qutebrowser
-    # passff-host # host app for the WebExtension PassFF
-    # tor-browser # browse web via tor
-    # torsocks # tor stuff
-    # tridactyl-native # native package for nix
+    chromium # google crap
+    firefox # normal browser
+    passff-host # host app for the WebExtension PassFF
+    tor-browser # browse web via tor
+    torsocks # tor stuff
+    tridactyl-native # native package for nix
   ];
 }
