2b9ea2ae84daf232eaf151afa014b11bc0477d7c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-22 18:32:46 +0300

    Revert "[flatpak] set variables"
    This reverts commit f0eab3c2342708fb1ad45b9f5de1440b7810a2a1.

--- nix/.config/home-manager/modules/flatpak/default.nix
+++ nix/.config/home-manager/modules/flatpak/default.nix
@@ -2,7 +2,4 @@
   home.packages = with pkgs; [
     flatpak # sandboxing and distribution framework
   ];
-  home.sessionVariables = {
-    XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"; # lets flatpak work
-  };
 }
