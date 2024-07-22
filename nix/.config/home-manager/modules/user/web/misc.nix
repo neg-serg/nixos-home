2b70bd73eba8b14f613b88723ed2b6bfc79df66b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-22 20:56:08 +0300

    [plowshare] install again

--- nix/.config/home-manager/modules/user/web/misc.nix
+++ nix/.config/home-manager/modules/user/web/misc.nix
@@ -9,7 +9,7 @@
     master.yt-dlp # download from youtube and another sources
     monolith # download all webpage stuff as one file
     pipe-viewer # lightweight youtube client
-    # plowshare # download/upload from popular sites
+    plowshare # download/upload from popular sites
     prettyping # fancy ping
     stable.megacmd # cli for MEGA
     whois # get domain info
