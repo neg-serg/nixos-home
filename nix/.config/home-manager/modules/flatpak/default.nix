3db7568d7e8959cfe2b1a65fd02bc73357f1d183
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-22 18:39:31 +0300

    [flatpak] cleanup

--- nix/.config/home-manager/modules/flatpak/default.nix
+++ nix/.config/home-manager/modules/flatpak/default.nix
@@ -1,7 +1,4 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    flatpak # sandboxing and distribution framework
-  ];
+{...}: {
   home.sessionVariables = {
     XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"; # lets flatpak work
   };
