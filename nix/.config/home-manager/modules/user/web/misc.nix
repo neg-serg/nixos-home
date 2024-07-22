4ad23642372323ef6c09bd5dce01f43f1f076f5b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-22 20:11:02 +0300

    [misc] add mov-cli

--- nix/.config/home-manager/modules/user/web/misc.nix
+++ nix/.config/home-manager/modules/user/web/misc.nix
@@ -6,13 +6,14 @@
 }: {
   home.packages = with pkgs; [
     gallery-dl # download image galleries/collections
+    master.yt-dlp # download from youtube and another sources
     monolith # download all webpage stuff as one file
+    mov-cli # to watch everything from terminal
     pipe-viewer # lightweight youtube client
     # plowshare # download/upload from popular sites
     prettyping # fancy ping
     stable.megacmd # cli for MEGA
     whois # get domain info
     xidel # download webpage parts
-    master.yt-dlp # download from youtube and another sources
   ];
 }
