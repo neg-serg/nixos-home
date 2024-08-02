d5eba7b167b45481fad764e829f18774adf67b39
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-02 04:40:48 +0300

    [fun] add neo

--- nix/.config/home-manager/modules/user/fun/misc.nix
+++ nix/.config/home-manager/modules/user/fun/misc.nix
@@ -12,6 +12,7 @@
     fortune # fortune cookie
     free42 # A software clone of HP-42S Calculator
     neo-cowsay # say something
+    neo # yet another digital rain
     nms # No More Secrets, a recreation of the live decryption effect from the famous hacker movie "Sneakers"
     solfege # ear training program
     taoup # The Tao of Unix Programming
