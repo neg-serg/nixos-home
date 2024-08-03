5f179880a2c99045b1ead7854ab289880cc224ca
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-03 03:32:44 +0300

    [audio] install picard

--- nix/.config/home-manager/modules/media/audio/apps.nix
+++ nix/.config/home-manager/modules/media/audio/apps.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     ape # monkey audio codec
+    audiowaveform # shows soundwaveform
     cdparanoia # cdrip / cdrecord
     cider # apple music player
     dr14_tmeter # compute the DR14 of a given audio file according to the procedure from Pleasurize Music Foundation
@@ -8,12 +9,11 @@
     ncpamixer # cli-pavucontrol
     nicotine-plus # download music via soulseek
     opensoundmeter # sound measurement application for tuning audio systems in real-time
-    # picard # autotags
+    picard # autotags
     roomeqwizard # room acoustics software
     screenkey # screencast tool to display your keys inspired by Screenflick
     sonic-visualiser # audio analyzer
     sox # audio processing
-    audiowaveform # shows soundwaveform
     streamlink # CLI for extracting streams from websites
     tauon # fancy standalone music player
     unflac # split2flac alternative
