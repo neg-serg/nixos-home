b38e2d61fab77379c0bdeddf557b7140b3f2be6e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-04 14:56:33 +0300

    [web] browsers cleanup

--- nix/.config/home-manager/modules/user/web/browsing.nix
+++ nix/.config/home-manager/modules/user/web/browsing.nix
@@ -3,10 +3,8 @@
     ./firefox.nix
   ];
   home.packages = with pkgs; [
-    chromium # google crap
     passff-host # host app for the WebExtension PassFF
     tor-browser # browse web via tor
-    torsocks # tor stuff
     tridactyl-native # native package for nix
   ];
 }
