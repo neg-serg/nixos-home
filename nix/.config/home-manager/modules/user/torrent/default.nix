5c354fad644499cb09cff59a38f7d383c41f3064
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-21 18:00:58 +0300

    [transmission] update to v4

--- nix/.config/home-manager/modules/user/torrent/default.nix
+++ nix/.config/home-manager/modules/user/torrent/default.nix
@@ -5,22 +5,22 @@
 }:
 with {
   bt-migrate = pkgs.callPackage ../../packages/bt-migrate {};
+  transmission = pkgs.transmission_4;
 }; {
   home.packages = with pkgs; [
     bt-migrate # torrent migrator
     oldstable.stig # transmission client
-    transmission # bittorrent daemon
   ];
 
   systemd.user.services.transmission-daemon = {
     Unit = {
       Description = "transmission service";
       After = ["network.target"];
-      ConditionPathExists = "${pkgs.transmission}/bin/transmission-daemon";
+      ConditionPathExists = "${transmission}/bin/transmission-daemon";
     };
     Service = {
       Type = "notify";
-      ExecStart = "${pkgs.transmission}/bin/transmission-daemon -g %E/transmission-daemon -f --log-error";
+      ExecStart = "${transmission}/bin/transmission-daemon -g %E/transmission-daemon -f --log-error";
       Restart = "on-failure";
       RestartSec = "30";
       StartLimitBurst = "8";
