dcb4692da7049a0d706f439ff78b5ff4dd563637
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-21 16:54:08 +0300

    [bat] extract

---
+++ nix/.config/home-manager/modules/cli/bat.nix
@@ -0,0 +1,9 @@
+{lib, ...}: {
+    programs.bat.enable = true;
+    programs.bat.config = {
+      theme = lib.mkForce "ansi";
+      italic-text = "always";
+      paging = "never";
+      decorations = "never";
+    };
+}
