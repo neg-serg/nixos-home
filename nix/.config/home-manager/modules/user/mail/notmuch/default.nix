6031816a376ab6f20cc29116c3aa31dd878720cb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-21 21:30:00 +0300

    [notmuch] extract.

---
+++ nix/.config/home-manager/modules/user/mail/notmuch/default.nix
@@ -0,0 +1,21 @@
+{...}: {
+  programs.notmuch = {
+    enable = true;
+    new = {
+      tags = ["unread" "inbox"];
+      ignore = [];
+    };
+    search = {
+        excludeTags = ["deleted" "spam"];
+    };
+    maildir = {
+      synchronizeFlags = true;
+    };
+    extraConfig = {
+      database = {
+        path = "/home/neg/.local/mail";
+      };
+    };
+  };
+  accounts.email.accounts."gmail".notmuch.enable = true;
+}
