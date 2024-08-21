8143c534b0a1572389ce499b2181c403b0c79b54
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-21 21:29:33 +0300

    [email] fix account

--- nix/.config/home-manager/modules/user/mail/accounts/default.nix
+++ nix/.config/home-manager/modules/user/mail/accounts/default.nix
@@ -3,6 +3,8 @@
   accounts.email.accounts."gmail" = {
     passwordCommand = "pass show google|wc -1";
     userName = "serg.zorg@gmail.com";
+    realName = "Sergey Miroshnichenko";
+    address = "serg.zorg@gmail.com";
     primary = true;
     imap = {
       host = "imap.gmail.com";
