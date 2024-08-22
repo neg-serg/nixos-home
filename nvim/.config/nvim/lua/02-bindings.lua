277bce18caa10837f377ece71d203a95b972eaea
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-22 20:42:54 +0300

    [nvim] future reuse

--- nvim/.config/nvim/lua/02-bindings.lua
+++ nvim/.config/nvim/lua/02-bindings.lua
@@ -68,3 +68,5 @@ Map('o', '<M-e>', '<End>', {noremap=true})
 Map('i', '<C-e>', "<C-o>$", {noremap=true})
 
 map('i', '<C-v>', 'paste#paste_cmd["i"]', {expr=true})
+-- Map('n', "<M-;>", function() vim.diagnostic.open_float({ border = "rounded", }) end, "display diagnostics")
+-- Map('i', "<M-;>", function() vim.diagnostic.open_float({ border = "rounded", }) end, "display diagnostics")
