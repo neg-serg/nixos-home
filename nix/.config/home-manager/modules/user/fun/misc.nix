47f2af90cd86de71920a4724c1bdf2a83c3d2f0f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-26 18:20:04 +0300

    [fun] cleanup misc

--- nix/.config/home-manager/modules/user/fun/misc.nix
+++ nix/.config/home-manager/modules/user/fun/misc.nix
@@ -5,9 +5,7 @@ with {
   home.packages = with pkgs; [
     almonds # TUI fractal viewer
     bucklespring # for keyboard sounds
-    clolcat # rainbow color version of cat
     cool-retro-term # a retro terminal emulator
-    cxxmatrix # nice matrix in terminal
     dotacat # yet another color version of cat
     figlet # ascii art
     fortune # fortune cookie
@@ -17,10 +15,7 @@ with {
     nms # No More Secrets, a recreation of the live decryption effect from the famous hacker movie "Sneakers"
     solfege # ear training program
     taoup # The Tao of Unix Programming
-    tmatrix # matrix-like screensaver for terminal
     toilet # text banners
-    toipe # typing tester
-    typioca # cozy typing tester
     xlife # cellular automata
   ];
 }
