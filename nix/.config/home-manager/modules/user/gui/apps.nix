c4e2b98f6b686b33f8f9180672bda2092a03d875
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-09-17 02:02:45 +0300

    [apps] use unstable clipcat

--- nix/.config/home-manager/modules/user/gui/apps.nix
+++ nix/.config/home-manager/modules/user/gui/apps.nix
@@ -1,11 +1,10 @@
 {
   pkgs,
-  stable,
   ...
 }: {
   home.packages = with pkgs; [
     autocutsel # tool to sync x11 buffers
     clipboard-jh # platform independent clipboard manager, test it more later
-    stable.clipcat # replacement for gpaste
+    clipcat # replacement for gpaste
   ];
 }
