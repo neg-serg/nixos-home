d2dea29dde02261a23a470521378b7aba95b5659
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-09-17 21:46:03 +0300

    [espanso] add

--- nix/.config/home-manager/modules/user/gui/apps.nix
+++ nix/.config/home-manager/modules/user/gui/apps.nix
@@ -6,5 +6,6 @@
     autocutsel # tool to sync x11 buffers
     clipboard-jh # platform independent clipboard manager, test it more later
     clipcat # replacement for gpaste
+    espanso # systemwide expander for keyboard
   ];
 }
