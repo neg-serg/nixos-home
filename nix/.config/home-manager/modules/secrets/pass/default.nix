c0a7d796ddc2a5c0fe5250587ff4f0694db4c7ff
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-13 18:57:32 +0300

    [pass] use unstable

--- nix/.config/home-manager/modules/secrets/pass/default.nix
+++ nix/.config/home-manager/modules/secrets/pass/default.nix
@@ -1,11 +1,10 @@
 {
   pkgs,
-  stable,
   ...
 }: {
   home.packages = with pkgs; [
     # password manager via gpg
-    (stable.pass.withExtensions (ext:
+    (pass.withExtensions (ext:
       with ext; [
         pass-audit # extension for auditing your password repository
         pass-otp # one time passwords integration
