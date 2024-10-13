85087823bf686527d6960984059927c478500600
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-14 00:12:18 +0300

    [zsh] add impure nix shell

--- shell/.config/zsh/02-cmds.zsh
+++ shell/.config/zsh/02-cmds.zsh
@@ -210,6 +210,7 @@ if [[ -e /etc/NIXOS ]]; then
     alias nixify='nix-shell -p nur.repos.kampka.nixify'
     alias S="nix shell"
     q(){ nix shell 'nixpkgs#'$1 }
+    qi(){ NIXPKGS_ALLOW_UNFREE=1 nix shell --impure 'nixpkgs#'$1 }
     flakify() {
         # thx to Mic92:
         if [ ! -e flake.nix ]; then
