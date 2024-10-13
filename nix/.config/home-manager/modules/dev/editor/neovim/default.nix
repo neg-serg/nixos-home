b1f8a334641fda11c8bfe8ded2a1e112dea08979
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-13 19:22:42 +0300

    [nvim] use latest nil

--- nix/.config/home-manager/modules/dev/editor/neovim/default.nix
+++ nix/.config/home-manager/modules/dev/editor/neovim/default.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   config,
-  stable,
   ...
 }:
 with {
@@ -9,15 +8,15 @@ with {
   dots = "${config.home.homeDirectory}/.dotfiles";
 }; {
   home.packages = with pkgs; [
+    bash-language-server # bash lsp
     neovim # neovim from master
     neovim-remote # nvr for neovim
-    bash-language-server # bash lsp
-    pyright # python lsp
+    nil # nixos language server
     pylyzer # python type checker
+    pyright # python lsp
     ruff-lsp # python lsp
     ruff # python linter
     rust-analyzer # rust lsp
-    stable.nil # nixos language server
   ];
   programs.neovim.plugins = with pkgs.vimPlugins; [
     clangd_extensions-nvim # llvm-based engine
