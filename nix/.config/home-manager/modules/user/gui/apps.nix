aca41f8c057df63263a09edcc51fa3fe671cfc47
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-13 17:42:07 +0300

    [gui] clipcat: move all to gui

--- nix/.config/home-manager/modules/user/gui/apps.nix
+++ nix/.config/home-manager/modules/user/gui/apps.nix
@@ -1,11 +1,24 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, stable, ...}: {
   home.packages = with pkgs; [
     autocutsel # tool to sync x11 buffers
     clipboard-jh # platform independent clipboard manager, test it more later
-    clipcat # replacement for gpaste
     espanso # systemwide expander for keyboard
   ];
+  systemd.user.services = {
+    clipcat = {
+      # replacement for gpaste
+      Unit = {
+        Description = "Clipcat daemon";
+        PartOf = ["graphical-session.target"];
+      };
+
+      Service = {
+        ExecStartPre = "${pkgs.coreutils}/bin/rm -f %t/clipcat/grpc.sock";
+        ExecStart = "${stable.clipcat}/bin/clipcatd --no-daemon --replace";
+        Restart = "on-failure";
+        Type = "simple";
+      };
+      Install = {WantedBy = ["graphical-session.target"];};
+    };
+  };
 }
