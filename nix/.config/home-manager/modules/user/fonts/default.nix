c4f0830d050b255a4799d50b58abc663ed6960e4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-13 19:31:50 +0300

    [ueberzugpp] remove

--- nix/.config/home-manager/modules/user/fonts/default.nix
+++ nix/.config/home-manager/modules/user/fonts/default.nix
@@ -1,11 +1,9 @@
 {
   pkgs,
-  stable,
   ...
 }: {
   home.packages = with pkgs; [
     fontforge # font processing
     pango # for pango-list
-    stable.ueberzugpp # better w3mimgdisplay
   ];
 }
