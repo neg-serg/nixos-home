7675c65db63f4bb74d383ed379c35ca238d7b350
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-31 18:25:41 +0300

    [yubikey] use stable

--- nix/.config/home-manager/modules/secrets/yubikey/default.nix
+++ nix/.config/home-manager/modules/secrets/yubikey/default.nix
@@ -1,5 +1,5 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
+{stable, ...}: {
+  home.packages = with stable; [
     yubikey-agent # ssh agent for yk
     yubikey-manager # yubikey manager cli
     yubikey-personalization # ykinfo
