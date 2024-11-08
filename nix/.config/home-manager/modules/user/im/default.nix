63f23aa7a8b7ef32dcca770427b6a5a1e7201f0a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-08 16:25:59 +0300

    [im] remove element

--- nix/.config/home-manager/modules/user/im/default.nix
+++ nix/.config/home-manager/modules/user/im/default.nix
@@ -1,6 +1,5 @@
-{pkgs, master, ...}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
-    master.element-desktop # matrix client to test #3
     skypeforlinux # skype for linux client
     telegram-desktop # famous cloud-based im
     vesktop # alternative discord client
