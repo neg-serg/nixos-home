6282f34e551c5d261de5294289119f3a687358e2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-09 02:36:23 +0300

    [text] use flatpak for obsidian

--- nix/.config/home-manager/modules/text/notes.nix
+++ nix/.config/home-manager/modules/text/notes.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-25.9.0"
-    "python3.12-youtube-dl-2021.12.17"
-  ];
+  # nixpkgs.config.allowUnfree = true;
+  # nixpkgs.config.permittedInsecurePackages = [
+  #   "electron-25.9.0"
+  #   "python3.12-youtube-dl-2021.12.17"
+  # ];
   home.packages = with pkgs; [
-    obsidian # notes
+    # obsidian # notes (use flatpak for this)
     zk # notes database
   ];
 }
