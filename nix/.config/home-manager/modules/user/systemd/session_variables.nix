e48a9eabe943bff48b5615e051b0a298b59bcf1e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-12 19:33:15 +0300

    [systemd] update session variables

--- nix/.config/home-manager/modules/user/systemd/session_variables.nix
+++ nix/.config/home-manager/modules/user/systemd/session_variables.nix
@@ -1,8 +1,8 @@
 {
   systemd.user.sessionVariables = {
     GDK_BACKEND = "x11";
-    XDG_CURRENT_DESKTOP = "i3";
-    XDG_SESSION_DESKTOP = "i3";
+    XDG_CURRENT_DESKTOP = "X-Generic";
+    XDG_SESSION_DESKTOP = "X-Generic";
     XDG_SESSION_TYPE = "x11";
   };
 }
