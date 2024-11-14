a6f9a023cad9891fda4ac092e83342ffc28eb8f2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-14 07:06:37 +0300

    [emulators] retroarch update

--- nix/.config/home-manager/modules/user/fun/emulators.nix
+++ nix/.config/home-manager/modules/user/fun/emulators.nix
@@ -1,10 +1,9 @@
 {
   pkgs,
-  stable,
   ...
 }: {
   home.packages = with pkgs; [
     pcem # emulator for ibm pc and clones
-    stable.retroarchFull # multiplatform emulator frontend
+    retroarchFull # multiplatform emulator frontend
   ];
 }
