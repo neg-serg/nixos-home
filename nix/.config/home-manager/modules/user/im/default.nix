bdde24796681b4f588140c9b87fc86a8b355e4f4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-03 12:06:18 +0300

    [im] remove skype

--- nix/.config/home-manager/modules/user/im/default.nix
+++ nix/.config/home-manager/modules/user/im/default.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    skypeforlinux # skype for linux client
     telegram-desktop # famous cloud-based im
     vesktop # alternative discord client
   ];
