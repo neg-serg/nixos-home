d5a1f2ecc5ae73087d2207915ebcf89d9aae15ca
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-19 04:05:57 +0300

    [greenclip] make a try

--- nix/.config/home-manager/modules/user/gui/apps.nix
+++ nix/.config/home-manager/modules/user/gui/apps.nix
@@ -3,5 +3,6 @@
     autocutsel # tool to sync x11 buffers
     clipboard-jh # platform independent clipboard manager, test it more later
     espanso # systemwide expander for keyboard
+    haskellPackages.greenclip # yet another clipboard manager
   ];
 }
