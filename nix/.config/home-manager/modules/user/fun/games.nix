1fb7692de1217aac93eeb2a4adab71db62a1e581
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-31 05:03:40 +0300

    [games] openmw and brogue from stable nixpkgs

--- nix/.config/home-manager/modules/user/fun/games.nix
+++ nix/.config/home-manager/modules/user/fun/games.nix
@@ -1,9 +1,9 @@
-{pkgs, ...}: {
+{pkgs, stable, ...}: {
   home.packages = with pkgs; [
     abuse # classic side-scrolling shooter customizable with LISP
     airshipper # for veloren voxel game
     angband # roguelike
-    brogue # roguelike
+    stable.brogue # roguelike
     crawl # roguelike
     crawlTiles # roguelike
     endless-sky # space exploration game
@@ -12,7 +12,7 @@
     gzdoom # open-source doom
     jazz2 # open source reimplementation of classic Jazz Jackrabbit 2 game
     nethack # roguelike
-    openmw # Unofficial open source engine reimplementation of the game Morrowind
+    stable.openmw # Unofficial open source engine reimplementation of the game Morrowind
     shattered-pixel-dungeon # roguelike
     # unnethack # roguelike
     xaos # smooth fractal explorer
