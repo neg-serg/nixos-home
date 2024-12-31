56a67d9721a97d6cefd52bca1b7fcd9e17d34b50
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-31 12:37:44 +0300

    [firefox] try instead of floorp

--- nix/.config/home-manager/modules/user/web/browsing.nix
+++ nix/.config/home-manager/modules/user/web/browsing.nix
@@ -1,11 +1,10 @@
 {pkgs, stable, ...}: {
   imports = [
-    # ./firefox.nix
-    ./floorp.nix
+    ./firefox.nix
+    # ./floorp.nix
   ];
   home.packages = with pkgs; [
     passff-host # host app for the WebExtension PassFF
     stable.tor-browser # browse web via tor
-    tridactyl-native # native package for nix
   ];
 }
