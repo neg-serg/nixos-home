883b4943a433a8a303edc33a80903530128df6e1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-01-09 10:56:39 +0300

    [nvim] plugins: better settings

--- nvim/.config/nvim/lua/01-plugins.lua
+++ nvim/.config/nvim/lua/01-plugins.lua
@@ -22,4 +22,9 @@ require'lazy'.setup({
     defaults={lazy=false},
     install={colorscheme={"neg"}},
     ui={icons={ft="", lazy="󰂠 ", loaded="", not_loaded=""},},
+	performance={
+	  cache={enabled=true,},
+	  reset_packpath=true,
+	  rtp={disabled_plugins={"gzip","matchparen","netrwPlugin","tarPlugin","tohtml","tutor","zipPlugin",},},
+	},
 })
