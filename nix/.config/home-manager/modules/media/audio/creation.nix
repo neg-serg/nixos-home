d12a890eb72d19d1393152359d6ecb8cf4bc408d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-01-25 18:47:12 +0300

    [audio] cleanup creation stuff

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -1,11 +1,8 @@
-{pkgs, stable, ...}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
-    bitwig-studio # great DAW
     noisetorch # virtual microphone to suppress the noise
     reaper # А вот в рипере!
-    # renoise # modern tracker
     rnnoise # neural network noise reduction
     tenacity # audio editor
-    stable.zrythm # free audio workstation
   ];
 }
