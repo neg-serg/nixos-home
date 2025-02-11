55ca21ab33c5095ccc6cc0ef5567f013cc698368
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-02-11 20:10:06 +0300

    [creation] add sunvox

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -3,6 +3,7 @@
     noisetorch # virtual microphone to suppress the noise
     reaper # А вот в рипере!
     rnnoise # neural network noise reduction
+    sunvox # fast and powerful modular synthesizer with pattern-based sequencer
     tenacity # audio editor
   ];
 }
