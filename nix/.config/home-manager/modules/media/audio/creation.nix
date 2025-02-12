0c013d9db88bafb083157a87007e2992b5be860b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-02-12 23:58:39 +0300

    [creation] add orca

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     noisetorch # virtual microphone to suppress the noise
+    orca-c # esoteric programming language designed to quickly create procedural sequencers
     reaper # А вот в рипере!
     rnnoise # neural network noise reduction
     sunvox # fast and powerful modular synthesizer with pattern-based sequencer
