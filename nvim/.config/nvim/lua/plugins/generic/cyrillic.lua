a2aa43e322571459ef0d26f81a375ff21b8c74c3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-02-20 21:28:27 +0300

    [nvim] add cyrillic

---
+++ nvim/.config/nvim/lua/plugins/generic/cyrillic.lua
@@ -0,0 +1,12 @@
+-- ┌───────────────────────────────────────────────────────────────────────────────────┐
+-- │ █▓▒░ nativerv/cyrillic.nvim                                                       │
+-- └───────────────────────────────────────────────────────────────────────────────────┘
+return {
+	"nativerv/cyrillic.nvim",
+	event = { "VeryLazy" },
+	config = function()
+		require("cyrillic").setup({
+			no_cyrillic_abbrev = false, -- default
+		})
+	end,
+}
