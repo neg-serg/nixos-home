b26bf515ff6da6ef642df7e02d051cd1ed1f6609
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-02-27 09:08:48 +0300

    [nvim] try lsp-lines

---
+++ nvim/.config/nvim/lua/plugins/dev/lsp-lines.lua
@@ -0,0 +1,9 @@
+-- ┌───────────────────────────────────────────────────────────────────────────────────┐
+-- │ █▓▒░ https://git.sr.ht/~whynothugo/lsp_lines.nvim                                 │
+-- └───────────────────────────────────────────────────────────────────────────────────┘
+return {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+        require("lsp_lines").setup()
+    end,
+}
