da3c7e0f81dfa418f026e178e6a2970113a1d011
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-02 01:21:09 +0300

    [nvim] add otter

---
+++ nvim/.config/nvim/lua/plugins/dev/otter.lua
@@ -0,0 +1,8 @@
+-- ┌───────────────────────────────────────────────────────────────────────────────────┐
+-- │ █▓▒░ jmbuhr/otter.nvim                                                            │
+-- └───────────────────────────────────────────────────────────────────────────────────┘
+return {
+  'jmbuhr/otter.nvim', -- provides lsp features and a code completion source for code embedded in other documents
+  dependencies = {'nvim-treesitter/nvim-treesitter'},
+  opts = {},
+}
