62284b878a3581722dd8c19d8f366712e5867885
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-01 23:13:09 +0300

    [zsh] first commit for se

--- shell/.config/zsh/02-cmds.zsh
+++ shell/.config/zsh/02-cmds.zsh
@@ -242,6 +242,28 @@ _exists flatpak && {
     alias zoom='flatpak run us.zoom.Zoom'
 }
 
+se() {
+    if [[ -f $1 ]]; then
+        case $1 in
+            *.tar.bz2) tar xvjf "$1" ;;
+            *.tar.gz) tar xvzf "$1" ;;
+            *.bz2) bunzip2 "$1" ;;
+            *.rar) unrar x "$1" ;;
+            *.gz) gunzip "$1" ;;
+            *.tar) tar xvf "$1" ;;
+            *.tbz2) tar xvjf "$1" ;;
+            *.tgz) tar xvzf "$1" ;;
+            *.zip) unzip "$1" ;;
+            *.Z) uncompress "$1" ;;
+            *.7z) 7z x "$1" ;;
+            *.xz) unxz "$1" ;;
+            *) echo "'$1' unknown compression" ;;
+        esac
+    else
+        echo "'$1' not a valid file"
+    fi
+}
+
 autoload zc
 unfunction _exists
 # vim: ft=zsh:nowrap
