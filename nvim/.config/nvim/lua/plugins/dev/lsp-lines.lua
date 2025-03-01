cc01902e38edd338473023c8992c34a459dc7c61
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-02 00:22:38 +0300

    [nvim] lsp-lines: CS

--- nvim/.config/nvim/lua/plugins/dev/lsp-lines.lua
+++ nvim/.config/nvim/lua/plugins/dev/lsp-lines.lua
@@ -2,14 +2,11 @@
 -- │ █▓▒░ https://git.sr.ht/~whynothugo/lsp_lines.nvim                                 │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
     config = function()
         -- vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
         vim.diagnostic.config({ virtual_lines = false })
-        vim.keymap.set("", "<leader>w",
-            require("lsp_lines").toggle,
-            { desc = "Toggle lsp_lines" }
-        )
-        require("lsp_lines").setup()
+        vim.keymap.set('', '<leader>w', require'lsp_lines'.toggle, {desc='Toggle lsp_lines'})
+        require'lsp_lines'.setup()
     end,
 }
