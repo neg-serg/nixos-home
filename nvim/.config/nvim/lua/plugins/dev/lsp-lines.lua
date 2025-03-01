58c9eade8ddc8558ee6108cb077497a6e0d0c0b0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-02 00:21:57 +0300

    [nvim] lsp-lines better setup

--- nvim/.config/nvim/lua/plugins/dev/lsp-lines.lua
+++ nvim/.config/nvim/lua/plugins/dev/lsp-lines.lua
@@ -4,6 +4,12 @@
 return {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     config = function()
+        -- vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+        vim.diagnostic.config({ virtual_lines = false })
+        vim.keymap.set("", "<leader>w",
+            require("lsp_lines").toggle,
+            { desc = "Toggle lsp_lines" }
+        )
         require("lsp_lines").setup()
     end,
 }
