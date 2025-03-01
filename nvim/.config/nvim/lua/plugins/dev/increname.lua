5767512b5f5600feeaf4ee8de387811083b524dd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-01 07:43:00 +0300

    [nvim] add increname

---
+++ nvim/.config/nvim/lua/plugins/dev/increname.lua
@@ -0,0 +1,13 @@
+-- ┌───────────────────────────────────────────────────────────────────────────────────┐
+-- │ █▓▒░ smjonas/inc-rename.nvim                                                      │
+-- └───────────────────────────────────────────────────────────────────────────────────┘
+return {
+  "smjonas/inc-rename.nvim",
+	lazy = true,
+	cmd = {
+		"IncRename",
+	},
+  config = function()
+    require("inc_rename").setup()
+  end,
+}
