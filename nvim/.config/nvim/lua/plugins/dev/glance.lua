78d2aa979ee925e0f72358d8b8e1016c855b0db4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-01 23:26:34 +0300

    [nvim] glance: better settings

--- nvim/.config/nvim/lua/plugins/dev/glance.lua
+++ nvim/.config/nvim/lua/plugins/dev/glance.lua
@@ -5,26 +5,19 @@ return {'DNLHC/glance.nvim', -- glance neovim for nice navigation
         glance.setup({
             height=18, -- Height of the window
             zindex=45,
-            -- By default glance will open preview "embedded" within your active window
-            -- when `detached` is enabled, glance will render above all existing windows
-            -- and won't be restiricted by the width of your active window
             detached=true,
-            -- Or use a function to enable `detached` only when the active window is too small
-            -- (default behavior)
             detached=function(winid)
                 return vim.api.nvim_win_get_width(winid) < 100
             end,
-            -- Configure preview window options
-            preview_win_opts={cursorline=true, number=true, wrap=true},
-            -- Show window borders. Only horizontal borders allowed
-            border={enable=false, top_char='―', bottom_char='―',},
+            preview_win_opts={cursorline=true, number=true, wrap=true}, -- Configure preview window options
+            border={enable=true, top_char='―', bottom_char='―',}, -- Show window borders. Only horizontal borders allowed
             list={
                 position='right', -- Position of the list window 'left'|'right'
-                width=0.33, -- 33% width relative to the active window, min 0.1, max 0.5
+                width=0.4, -- 33% width relative to the active window, min 0.1, max 0.5
             },
-            theme={ -- This feature might not work properly in nvim-0.7.2
+            theme={
                 enable=true, -- Will generate colors for the plugin based on your current colorscheme
-                mode='auto', -- 'brighten'|'darken'|'auto', 'auto' will set mode based on the brightness of your colorscheme
+                mode='darken', -- 'brighten'|'darken'|'auto', 'auto' will set mode based on the brightness of your colorscheme
             },
             mappings={
                 list={
