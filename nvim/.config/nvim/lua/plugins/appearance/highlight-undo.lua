0335ed9f526597ccc04676290d95ee29e38cc4b2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-03 19:57:14 +0300

    [nvim] fix highlight-undo

--- nvim/.config/nvim/lua/plugins/appearance/highlight-undo.lua
+++ nvim/.config/nvim/lua/plugins/appearance/highlight-undo.lua
@@ -2,11 +2,11 @@
 -- │ █▓▒░ tzachar/highlight-undo.nvim                                                  │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {
-    'tzachar/highlight-undo.nvim', -- highlight changed text after any action not in insert
-    opts = {
-        hlgroup = "HighlightUndo",
-        duration = 300,
-        pattern = {"*"},
-        ignored_filetypes = {"neo-tree", "fugitive", "TelescopePrompt", "mason", "lazy"},
-    },
+  'tzachar/highlight-undo.nvim', -- highlight changed text after any action not in insert
+  opts = {
+    hlgroup = "HighlightUndo",
+    duration = 300,
+    pattern = {"*"},
+    ignored_filetypes = {"neo-tree", "fugitive", "TelescopePrompt", "mason", "lazy"},
   },
+}
