cef90fe546458d6f1f5448e8aa79d425c4df2085
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-04 02:03:42 +0300

    [nvim] otter: CS

--- nvim/.config/nvim/lua/plugins/dev/otter.lua
+++ nvim/.config/nvim/lua/plugins/dev/otter.lua
@@ -1,8 +1,7 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ jmbuhr/otter.nvim                                                            │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-return {
-  'jmbuhr/otter.nvim', -- provides lsp features and a code completion source for code embedded in other documents
+return {'jmbuhr/otter.nvim', -- provides lsp features and a code completion source for code embedded in other documents
   dependencies = {'nvim-treesitter/nvim-treesitter'},
   opts = {},
 }
