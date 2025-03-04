06515f5920005383af4e9fd344aa8b32b12d2a8a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-04 03:11:16 +0300

    [nvim] nvim-session-manager: cleanup

--- nvim/.config/nvim/lua/plugins/generic/nvim-session-manager.lua
+++ nvim/.config/nvim/lua/plugins/generic/nvim-session-manager.lua
@@ -22,17 +22,5 @@ return {'Shatur/neovim-session-manager', -- copy current location
           max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
           load_include_current = false,  -- The currently loaded session appears in the load_session UI.
       })
-
-      vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-          callback = function ()
-              for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-                  -- Don't save while there's any 'nofile' buffer open.
-                  if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
-                      return
-                  end
-              end
-              session_manager.save_current_session()
-          end
-      })
   end
 }
