b44bc7f46b7799b29cb49308b45f31e3d38b8962
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-05 09:12:37 +0300

    [creation] get rid of sonic-pi because of it pollute home dir

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -7,7 +7,6 @@
     orca-c # esoteric programming language designed to quickly create procedural sequencers
     reaper # А вот в рипере!
     rnnoise # neural network noise reduction
-    sonic-pi # free live coding synth for everyone originally designed to support computing and music lessons within schools
     stochas # nice free sequencer
     sunvox # fast and powerful modular synthesizer with pattern-based sequencer
     surge-XT # great wavetable synth 
