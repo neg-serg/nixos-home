f71e79496343778bc04abfc66f6e64aab776d42d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-23 15:08:26 +0300

    [games] add flare

--- nix/.config/home-manager/modules/user/fun/games.nix
+++ nix/.config/home-manager/modules/user/fun/games.nix
@@ -3,17 +3,18 @@
     abuse # classic side-scrolling shooter customizable with LISP
     airshipper # for veloren voxel game
     angband # roguelike
-    stable.brogue # roguelike
     crawl # roguelike
     crawlTiles # roguelike
     endless-sky # space exploration game
     fheroes2 # free heroes 2 implementation
+    flare # fantasy action RPG using the FLARE engine
     gnuchess # GNU chess engine
     gzdoom # open-source doom
     jazz2 # open source reimplementation of classic Jazz Jackrabbit 2 game
     nethack # roguelike
-    stable.openmw # Unofficial open source engine reimplementation of the game Morrowind
     shattered-pixel-dungeon # roguelike
+    stable.brogue # roguelike
+    stable.openmw # Unofficial open source engine reimplementation of the game Morrowind
     stable.unnethack # roguelike
     xaos # smooth fractal explorer
   ];
