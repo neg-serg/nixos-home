24f52e7e7221eb81f0d8fa8dd74d60f24c4a44f3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-04-01 00:53:54 +0300

    [hack] add katana

--- nix/.config/home-manager/modules/dev/hack/default.nix
+++ nix/.config/home-manager/modules/dev/hack/default.nix
@@ -2,5 +2,6 @@
   home.packages = with pkgs; [
     # binwalk # search binary image for embedded files
     capstone # disassembly framework
+    katana # next-generation crawling and spidering framework
   ];
 }
