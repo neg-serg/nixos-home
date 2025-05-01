cc75d782bc5fc48e8d0d223f40f4004ed6bf88f0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-02 02:41:31 +0300

    [nvim] remove git-conflict

--- nvim/.config/nvim/lua/plugins/dcvs/git-conflict.lua
+++
@@ -1,9 +0,0 @@
--- ┌───────────────────────────────────────────────────────────────────────────────────┐
--- │ █▓▒░ akinsho/git-conflict.nvim                                                    │
--- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'akinsho/git-conflict.nvim', -- visualize and resolve git conflicts
-    config=function()
-        local status, git_conflict = pcall(require, 'git-conflict')
-        if (not status) then return end
-        git_conflict.setup()
-    end}
