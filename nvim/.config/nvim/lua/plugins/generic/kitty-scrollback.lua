1bf4d3e1410ca75fc7388b1fd6c7623b47a82266
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-01 22:00:13 +0300

    [nvim] kitty-scrollback: modern config

--- nvim/.config/nvim/lua/plugins/generic/kitty-scrollback.lua
+++ nvim/.config/nvim/lua/plugins/generic/kitty-scrollback.lua
@@ -6,4 +6,14 @@ return {'mikesmithgh/kitty-scrollback.nvim', -- kitty modern neovim scrollback s
     cmd={'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth'},
     event={'User KittyScrollbackLaunch'},
     opts={kitty_get_text={extent='all', ansi=false}},
-    config=function() require'kitty-scrollback'.setup() end}
+    config=function()
+        require'kitty-scrollback'.setup({
+          kitty_get_text = {
+            ansi = true, -- boolean If true, the text will include the ANSI formatting escape codes for colors, bold, italic, etc.
+            -- string What text to get. The default of screen means all text currently on the screen. all means all the screen+scrollback and selection means the currently selected text. first_cmd_output_on_screen means the output of the first command that was run in the window on screen. last_cmd_output means the output of the last command that was run in the window. last_visited_cmd_output means the first command output below the last scrolled position via scroll_to_prompt. last_non_empty_output is the output from the last command run in the window that had some non empty output. The last four require shell_integration to be enabled. Choices: screen, all, first_cmd_output_on_screen, last_cmd_output, last_non_empty_output, last_visited_cmd_output, selection
+            extent = 'all',
+            clear_selection = true, -- boolean If true, clear the selection in the matched window, if any.
+          },
+      })
+    end
+}
