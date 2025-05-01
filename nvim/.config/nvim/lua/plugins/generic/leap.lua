232844707fd1bf5c04314cbd1f7d529678389eda
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-01 21:01:31 +0300

    [nvim] visual: remove binding

--- nvim/.config/nvim/lua/plugins/generic/leap.lua
+++ nvim/.config/nvim/lua/plugins/generic/leap.lua
@@ -34,7 +34,6 @@ return {'ggandor/leap.nvim',
           },
       }
       vim.keymap.set('n', 's', '<Plug>(leap-anywhere)')
-      vim.keymap.set('x', 's', '<Plug>(leap)')
       vim.keymap.set('o', 's', '<Plug>(leap-forward)')
       vim.keymap.set('o', 'S', '<Plug>(leap-backward)')
       -- If using the default mappings (`gs` for multi-window mode), you can
