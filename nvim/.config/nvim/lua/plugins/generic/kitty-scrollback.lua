84a50853f481942bcdb4a69c17f0b20813846d4b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-01 21:29:49 +0300

    [nvim] kitty-scrollback: use latest version

--- nvim/.config/nvim/lua/plugins/generic/kitty-scrollback.lua
+++ nvim/.config/nvim/lua/plugins/generic/kitty-scrollback.lua
@@ -6,5 +6,4 @@ return {'mikesmithgh/kitty-scrollback.nvim', -- kitty modern neovim scrollback s
     cmd={'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth'},
     event={'User KittyScrollbackLaunch'},
     opts={kitty_get_text={extent='all', ansi=false}},
-    version='3.2.1', -- latest stable version, may have breaking changes if major version changed
     config=function() require'kitty-scrollback'.setup() end}
