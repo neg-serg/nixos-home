bcc37c59db6dab1c1e4849467d40daa8d05647f5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-05 03:07:48 +0300

    [nvim] cleanup bindings

--- nvim/.config/nvim/lua/plugins/generic/yazi.lua
+++ nvim/.config/nvim/lua/plugins/generic/yazi.lua
@@ -8,9 +8,6 @@ return {"mikavilpas/yazi.nvim",
   },
   keys = {
         { "<leader>-", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file", },
-        { "<leader>cw", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory", }, -- Open in the current working directory
-        { "<c-up>", "<cmd>Yazi toggle<cr>", desc = "Resume the last yazi session",
-    },
   },
   opts = {
     open_for_directories = true, -- if you want to open yazi instead of netrw, see below for more info
