c7220968e87625dbfe8a8c02cc615780b5db63a2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-10 12:21:40 +0300

    [sway] cleanup

--- nix/.config/home-manager/modules/user/gui/sway.nix
+++ nix/.config/home-manager/modules/user/gui/sway.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs; [
     fnott # wayland notifications
     fuzzel # wayland launcher
-    sway # i3 for wayland
+    swayfx # i3 for wayland
     swww # wallpaper daemon for wayland
     wdisplays # gui for configuring displays in Wayland compositors
     wofi # rofi for wayland
@@ -13,61 +13,4 @@
   ];
   services.hyprpaper.enable = true;
   programs.hyprlock.enable = true;
-  wayland.windowManager.sway = {
-    enable = true;
-    systemd.enable = true;
-    config = {
-      terminal = "kitty";
-      modifier = "Mod4";
-      output = {
-        "*" = {
-          mode = "3440x1440@174.962Hz";
-          adaptive_sync = "on";
-          scale = "1.0";
-          #bg = lib.mkForce "~/pic/wl/wallhaven-49pqxk.jpg fill";
-        };
-      };
-      input = {
-        "*" = {
-          xkb_layout = "us,ru";
-          xkb_options = "grp:alt_shift_toggle";
-        };
-        "type:keyboard" = {
-          repeat_delay = "250";
-          repeat_rate = "60";
-        };
-      };
-    };
-  };
 }
-# bindsym Mod4+Return exec kitty
-# bindsym Mod1+grave exec ~/bin/rofi-run
-#
-# systemd.user.services.wpaperd = {
-#     Unit = {
-#         Description = "Wallpaper daemon";
-#         After = ["graphical-session-pre.target"];
-#         PartOf = ["graphical-session.target"];
-#     };
-#     Service = {
-#         ExecStart = "${pkgs.wpaperd}/bin/wpaperd --no-daemon";
-#         Environment = "XDG_CONFIG_HOME=${wpaperd-config-dir}";
-#     };
-#     Install.WantedBy = ["graphical-session.target"];
-# };
-# programs = {
-#     gamescope = {
-#         enable = true;
-#         capSysNice = true;
-#         args = [ "--steam"
-#                 "--expose-wayland"
-#                 "--rt"
-#                 "-W 1920"
-#                 "-H 1080"
-#                 "--force-grab-cursor"
-#                 "--grab"
-#                 "--fullscreen"
-#         ];
-#     };
-# }
-
