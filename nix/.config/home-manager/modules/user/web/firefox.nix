afd759a5e4f2840b8be9bafe118925a95093a70e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-12 05:23:22 +0300

    [firefox] try enable profile

--- nix/.config/home-manager/modules/user/web/firefox.nix
+++ nix/.config/home-manager/modules/user/web/firefox.nix
@@ -10,6 +10,7 @@
     MOZ_DBUS_REMOTE = "1";
     MOZ_ENABLE_WAYLAND = "1";
   };
+  config.stylix.targets.firefox.profileNames = ["default"];
   xdg.mimeApps.defaultApplications = {
     "text/html" = "firefox.desktop";
     "x-scheme-handler/http" = "firefox.desktop";
