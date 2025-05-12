ca636216f4ebdf4c4ed14a2db8ba97debbcf2fc1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-12 05:23:59 +0300

    Revert "[firefox] try enable profile"
    This reverts commit 466648ac454f0b0d18038afa9c1b6c77dfcdc5af.

--- nix/.config/home-manager/modules/user/web/firefox.nix
+++ nix/.config/home-manager/modules/user/web/firefox.nix
@@ -10,7 +10,6 @@
     MOZ_DBUS_REMOTE = "1";
     MOZ_ENABLE_WAYLAND = "1";
   };
-  config.stylix.targets.firefox.profileNames = ["default"];
   xdg.mimeApps.defaultApplications = {
     "text/html" = "firefox.desktop";
     "x-scheme-handler/http" = "firefox.desktop";
