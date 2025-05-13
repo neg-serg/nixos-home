4f4d9b27341b48da1082a539595d5e75b8fabbb9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 09:10:34 +0300

    [systemd] session_variables: cleanup

--- nix/.config/home-manager/modules/user/systemd/session_variables.nix
+++ nix/.config/home-manager/modules/user/systemd/session_variables.nix
@@ -1,8 +1,2 @@
 {
-  systemd.user.sessionVariables = {
-    GDK_BACKEND = "x11";
-    XDG_CURRENT_DESKTOP = "X-Generic";
-    XDG_SESSION_DESKTOP = "X-Generic";
-    XDG_SESSION_TYPE = "x11";
-  };
 }
