fd58c829dd744dbaa6f0a82cab7970de16798da0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 15:53:50 +0300

    [web] tor-browser: use latest

--- nix/.config/home-manager/modules/user/web/browsing.nix
+++ nix/.config/home-manager/modules/user/web/browsing.nix
@@ -1,4 +1,4 @@
-{pkgs, stable, yandex-browser, ...}: {
+{pkgs, yandex-browser, ...}: {
   imports = [
     ./firefox.nix
     # ./floorp.nix
@@ -6,6 +6,6 @@
   home.packages = with pkgs; [
     yandex-browser.packages.x86_64-linux.yandex-browser-stable # google chrome-based yandex fork
     passff-host # host app for the WebExtension PassFF
-    stable.tor-browser # browse web via tor
+    tor-browser # browse web via tor
   ];
 }
