65459d4e87125a46ed6de147fe0fd240adaa1728
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 23:17:10 +0300

    [wayland] new stuff

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -3,7 +3,9 @@
   home.packages = with pkgs; [
     fnott # wayland notifications
     fuzzel # wayland launcher
+    grim # to take screenshots
     swww # wallpaper daemon for wayland
+    tofi # an extremely fast and simple dmenu / rofi replacement for wlroots-based Wayland compositors
     wdisplays # gui for configuring displays in Wayland compositors
     wev # xev for wayland
     wtype # xdotool for wayland
