06c71798d8df3e959308d9951d4bec899687dabd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 11:03:25 +0300

    [stuff] cleanup

--- nix/.config/home-manager/modules/user/x11/stuff.nix
+++ nix/.config/home-manager/modules/user/x11/stuff.nix
@@ -2,17 +2,6 @@
   home.packages = with pkgs; [
     inputplug # xinput event monitor
     slop # rectangle selection
-    unclutter-xfixes # x11 hide cursor via xfixes
-    warpd
-    wmctrl
-    xclip
-    xsel # cli for X session clipboard manipulation
-    xdo # X11 automation
-    xdotool # another X11 automation
-    xorg.xev # X11 window to debug events
-    xorg.xkill # kill X11 window via cursor or id
-    xorg.xset # X11 settings rule
-    xsettingsd # manage xdg settings without gnome
-    xss-lock
+    xss-lock # x locker
   ];
 }
