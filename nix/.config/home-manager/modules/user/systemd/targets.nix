3ddf6555c951e27711830a9874023a2a39432abd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 09:10:08 +0300

    [systemd] targets: cleanup

--- nix/.config/home-manager/modules/user/systemd/targets.nix
+++ nix/.config/home-manager/modules/user/systemd/targets.nix
@@ -1,20 +1,2 @@
 {
-  systemd.user.targets.tray = {
-    Unit = {
-      Description = "Home Manager System Tray";
-      Requires = ["graphical-session-pre.target"];
-    };
-  };
-
-  systemd.user.targets.i3-session = {
-    Unit = {
-      Description = "X session";
-      BindsTo = ["i3.service"];
-      Wants = [
-        "executor.service"
-        "negwm-autostart.service"
-        "negwm.service"
-      ];
-    };
-  };
 }
