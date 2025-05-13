91bc8703486f5bc45fdca4d56ef8f28efab1260e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 15:59:33 +0300

    [emulators] pcem: use latest

--- nix/.config/home-manager/modules/user/fun/emulators.nix
+++ nix/.config/home-manager/modules/user/fun/emulators.nix
@@ -1,10 +1,9 @@
 {
   pkgs,
-  stable,
   ...
 }: {
   home.packages = with pkgs; [
-    stable.pcem # emulator for ibm pc and clones
+    pcem # emulator for ibm pc and clones
     retroarchFull # multiplatform emulator frontend
   ];
 }
