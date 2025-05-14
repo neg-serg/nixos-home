bfa95f0f124f366ed268885147c6444cc183a468
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-14 12:02:02 +0300

    [wayland] move wayland stuff to home

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -4,6 +4,17 @@
     fnott # wayland notifications
     fuzzel # wayland launcher
     grim # to take screenshots
+    hyprcursor # is a new cursor theme format that has many advantages over the widely used xcursor.
+    hypridle # idle daemon
+    hyprland-qt-support # qt support
+    hyprland-qtutils # utility apps for hyprland
+    hyprpaper # setup wallpaper
+    hyprpicker # color picker
+    hyprpolkitagent # better polkit agent
+    hyprsysteminfo # show system info
+    hyprutils # small library for hyprland
+    libsForQt5.qt5ct kdePackages.qt6ct # qt integration stuff
+    pyprland # hyperland plugin system
     slurp # select region in wayland compositor
     swww # wallpaper daemon for wayland
     tofi # an extremely fast and simple dmenu / rofi replacement for wlroots-based Wayland compositors
