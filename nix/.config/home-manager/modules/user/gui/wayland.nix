32230558344e87ca2dadd729a3e13216d6c0af32
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-14 08:45:36 +0300

    [wayland] new wayland tools

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -4,10 +4,12 @@
     fnott # wayland notifications
     fuzzel # wayland launcher
     grim # to take screenshots
+    slurp # select region in wayland compositor
     swww # wallpaper daemon for wayland
     tofi # an extremely fast and simple dmenu / rofi replacement for wlroots-based Wayland compositors
     wdisplays # gui for configuring displays in Wayland compositors
     wev # xev for wayland
+    wf-recorder # tool to make screencasts
     wlr-randr # xrandr for wayland
     wtype # xdotool for wayland
     ydotool # xdotool systemwide
