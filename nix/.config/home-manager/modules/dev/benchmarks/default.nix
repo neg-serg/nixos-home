dc43e4842fa9c5bd445c8b454be43d16abb76485
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-15 07:10:08 +0300

    [benchmarks] cleanup

--- nix/.config/home-manager/modules/dev/benchmarks/default.nix
+++ nix/.config/home-manager/modules/dev/benchmarks/default.nix
@@ -1,10 +1,7 @@
 {pkgs, ...}: {
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
-    # interbench # interactive benchmark
     memtester # memory test
-    phoronix-test-suite # massive benchmark toolset
-    stress
     stress-ng # stress testing
     vrrtest # freesync/gsync working test
   ];
