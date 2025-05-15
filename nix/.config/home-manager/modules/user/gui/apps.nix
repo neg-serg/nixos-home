2f4dea0aa445bb97cb1000362977456cf157c8e4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-15 04:16:28 +0300

    [gui] install bzmenu

--- nix/.config/home-manager/modules/user/gui/apps.nix
+++ nix/.config/home-manager/modules/user/gui/apps.nix
@@ -1,9 +1,10 @@
-{pkgs, ...}: {
+{pkgs, inputs, ...}: {
   home.packages = with pkgs; [
     autocutsel # tool to sync x11 buffers
     clipboard-jh # platform independent clipboard manager, test it more later
     cliphist # wayland stuff for clipboard
     espanso # systemwide expander for keyboard
     haskellPackages.greenclip # yet another clipboard manager
+    inputs.bzmenu.packages.${pkgs.system}.default # bluetooth support menu
   ];
 }
