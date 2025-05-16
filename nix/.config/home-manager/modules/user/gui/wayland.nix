d4b9db2f37fe32ffe7a850bbacde210c8f53fd33
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-16 05:21:31 +0300

    [wayland] add waybar

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -18,6 +18,7 @@
     slurp # select region in wayland compositor
     swww # wallpaper daemon for wayland
     tofi # an extremely fast and simple dmenu / rofi replacement for wlroots-based Wayland compositors
+    waybar # simple titlebar for wayland
     wdisplays # gui for configuring displays in Wayland compositors
     wev # xev for wayland
     wf-recorder # tool to make screencasts
