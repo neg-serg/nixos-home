50d6698fefbaf32fda58f8b15849e02d08396f94
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-25 19:03:16 +0300

    [nvim] blink: use super-tab bindings

--- nvim/.config/nvim/lua/plugins/dev/blink.lua
+++ nvim/.config/nvim/lua/plugins/dev/blink.lua
@@ -6,7 +6,7 @@ return {'saghen/blink.cmp',
   version = '1.*', -- use a release tag to download pre-built binaries
   -- build = 'nix run .#build-plugin',
   opts = {
-    keymap = { preset = 'default' }, -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'super-tab' }, -- See :h blink-cmp-config-keymap for defining your own keymap
     appearance = { nerd_font_variant = 'mono'},
     completion = { documentation = { auto_show = false } }, -- (Default) Only show the documentation popup when manually triggered
     sources = {default = { 'lsp', 'path', 'snippets', 'buffer' },},
