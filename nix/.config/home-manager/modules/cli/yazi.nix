5f141dd799c0b2d346279739ea8dbe503081bc87
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-26 13:44:47 +0300

    [yazi] try better bindings

--- nix/.config/home-manager/modules/cli/yazi.nix
+++ nix/.config/home-manager/modules/cli/yazi.nix
@@ -3,34 +3,17 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      manager = {
-        show_hidden = true;
-      };
-      opener.edit = [
-        {
-          run = "nvim \"$@\"";
-          block = true;
-        }
-      ];
+      manager = { show_hidden = true; };
+      opener.edit = [{ run = "nvim \"$@\""; block = true; }];
     };
     keymap = {
       manager.prepend_keymap = [
-        {
-          run = "close";
-          on = ["<C-q>"];
-        }
-        {
-          run = "yank --cut";
-          on = ["d"];
-        }
-        {
-          run = "remove --force";
-          on = ["D"];
-        }
-        {
-          run = "remove --permanently";
-          on = ["X"];
-        }
+        { run = "close"; on = ["<Esc>"]; }
+        { run = "close"; on = ["<C-q>"]; }
+        { run = "yank --cut"; on = ["d"]; }
+        { run = "remove --force"; on = ["D"]; }
+        { run = "remove --permanently"; on = ["X"]; }
+        { on = ["f"]; run = "shell \"$SHELL\" --block"; desc = "Open $SHELL here"; }
       ];
     };
   };
