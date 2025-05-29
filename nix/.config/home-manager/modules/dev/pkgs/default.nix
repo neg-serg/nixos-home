2d49d7fd53f69020ac3afe8c9c53df602b34470e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-29 12:12:11 +0300

    [dev] add dprint

--- nix/.config/home-manager/modules/dev/pkgs/default.nix
+++ nix/.config/home-manager/modules/dev/pkgs/default.nix
@@ -2,6 +2,7 @@
   home.packages = with pkgs; [
     cloc # count lines of code
     deheader # remove unneeded includes for c, cpp
+    dprint # code formatting platform
     flawfinder # examine c, cpp code for security flaws
     mypy # optional static-typing for python
     scc # parallel cloc
