7c55962ffaf627fe8b56604851c5375b39f99727
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-07 06:10:48 +0300

    [wayland] cleanup

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -16,14 +16,12 @@
     grim # to take screenshots
     inputs.iwmenu.packages.${pkgs.system}.default # wifi menu
     inputs.matugen.packages.${pkgs.system}.default # modern theme generator
-    nwg-look # tool to test and set gtk themes
     satty # screenshot helper tool
     slurp # select region in wayland compositor
     swayimg # wayland-native image viewer
     swaynotificationcenter # try another notification center
     swww # wallpaper daemon for wayland
     tofi # an extremely fast and simple dmenu / rofi replacement for wlroots-based Wayland compositors
-    waybar # temporary use this panel
     wev # xev for wayland
     wf-recorder # tool to make screencasts
     wl-clipboard # copy-paste for wayland
