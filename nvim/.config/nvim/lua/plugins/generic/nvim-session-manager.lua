50bd95bb034fd0e72d9c5ebb1581d1e188202429
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-10 01:07:11 +0300

    [nvim] session-manager: do not ignore not normal

--- nvim/.config/nvim/lua/plugins/generic/nvim-session-manager.lua
+++ nvim/.config/nvim/lua/plugins/generic/nvim-session-manager.lua
@@ -11,11 +11,11 @@ return {'Shatur/neovim-session-manager', -- copy current location
           dir_to_session_filename = dir_to_session_filename, -- Function that replaces separators and colons into special symbols to transform session directory into a filename. Should use `vim.uv.cwd()` if the passed `dir` is `nil`.
           autoload_mode = config.AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments. See "Autoload mode" section below.
           autosave_last_session = true, -- Automatically save last session on exit and on session switch.
-          autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+          autosave_ignore_not_normal = false, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
           autosave_ignore_dirs = {}, -- A list of directories where the session will not be autosaved.
           autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
-              'gitcommit',
-              'gitrebase',
+            'gitcommit',
+            'gitrebase',
           },
           autosave_ignore_buftypes = {}, -- All buffers of these bufer types will be closed before the session is saved.
           autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
