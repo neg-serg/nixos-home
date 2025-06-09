2b949d642995baca9108d36f21ffa95eca344e8b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-10 01:30:12 +0300

    [nvim] yazi.nvim: cleanup

--- nvim/.config/nvim/lua/plugins/generic/yazi.lua
+++ nvim/.config/nvim/lua/plugins/generic/yazi.lua
@@ -1,32 +1,31 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ mikavilpas/yazi.nvim                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-return {"mikavilpas/yazi.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "folke/snacks.nvim" -- check the installation instructions at https://github.com/folke/snacks.nvim
+return {'mikavilpas/yazi.nvim',
+  dependencies={
+    'folke/snacks.nvim' -- check the installation instructions at https://github.com/folke/snacks.nvim
   },
-  keys = {
-        { "<leader>-", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file", },
+  keys={
+    {'<leader>-', mode={'n','v'}, "<cmd>Yazi<cr>", desc="Open yazi at the current file",},
   },
-  opts = {
-    open_for_directories = true, -- if you want to open yazi instead of netrw, see below for more info
-    keymaps = {
-        show_help = "<f1>",
-        open_file_in_vertical_split = "<c-v>",
-        open_file_in_horizontal_split = "<c-x>",
-        open_file_in_tab = "<c-t>",
-        grep_in_directory = "<c-f>",
-        replace_in_directory = "<c-g>",
-        cycle_open_buffers = "<NOP>",
-        copy_relative_path_to_selected_files = "<c-y>",
-        send_to_quickfix_list = "<c-q>",
-        change_working_directory = "<tab>",
+  opts={
+    open_for_directories=true, -- if you want to open yazi instead of netrw, see below for more info
+    keymaps={
+        show_help="<f1>",
+        open_file_in_vertical_split="<c-v>",
+        open_file_in_horizontal_split="<c-x>",
+        open_file_in_tab="<c-t>",
+        grep_in_directory="<c-f>",
+        replace_in_directory="<c-g>",
+        cycle_open_buffers="<NOP>",
+        copy_relative_path_to_selected_files="<c-y>",
+        send_to_quickfix_list="<c-q>",
+        change_working_directory="<tab>",
     },
-    yazi_floating_window_border = "double",
+    yazi_floating_window_border="double",
   },
-  init = function()
+  init=function()
+    vim.g.loaded_netrwPlugin=1
     require'yazi'.setup()
-    vim.g.loaded_netrwPlugin = 1
   end,
 }
