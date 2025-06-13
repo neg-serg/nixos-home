1e18fb2087f35b1138cf71ec8be0a9f879bcbca5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-14 02:09:28 +0300

    [atuin] initial commit

--- nix/.config/home-manager/modules/cli/default.nix
+++ nix/.config/home-manager/modules/cli/default.nix
@@ -14,6 +14,21 @@
     ./starship.nix # universal command prompt
     ./yazi.nix
   ];
+  programs.atuin = {
+    enable = true;
+    flags = [ "--disable-up-arrow" ];
+    settings = {
+      update_check = false;
+      style = "compact";
+      invert = true;
+      inline_height = 15;
+      show_help = false;
+      show_tabs = false;
+      enter_accept = true;
+      prefers_reduced_motion = true;
+      keys.scroll_exits = false;
+    };
+  };
   home.packages = with pkgs; [
     babashka # native clojure for scripts
     choose # yet another cut/awk alternative
