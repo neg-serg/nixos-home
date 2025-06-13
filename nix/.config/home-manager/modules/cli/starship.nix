607c19a250fdd04e9587171d4f39a6a517dc12ee
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-13 14:54:48 +0300

    [starship] initial commit

---
+++ nix/.config/home-manager/modules/cli/starship.nix
@@ -0,0 +1,11 @@
+{...}: {
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      command_timeout = 1300;
+      scan_timeout = 50;
+      format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
+    };
+  };
+}
