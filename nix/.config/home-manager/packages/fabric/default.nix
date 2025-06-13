951dc096d6dd03ce94fa939369a3fc2edbaf380e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-14 02:08:26 +0300

    [fabric] add package

---
+++ nix/.config/home-manager/packages/fabric/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  python312Packages,
+  gtk3,
+  gtk-layer-shell,
+  cairo,
+  gobject-introspection,
+  libdbusmenu-gtk3,
+  gdk-pixbuf,
+  cinnamon-desktop,
+  gnome-bluetooth,
+}:
+python312Packages.buildPythonPackage {
+  pname = "python-fabric";
+  version = "0.0.2";
+  pyproject = true;
+
+  src = ./.;
+
+  buildInputs = [
+    gtk3
+    gtk-layer-shell
+    cairo
+    gobject-introspection
+    libdbusmenu-gtk3
+    gdk-pixbuf
+    cinnamon-desktop
+    gnome-bluetooth
+  ];
+
+  dependencies = with python312Packages; [
+    setuptools
+    click
+    pycairo
+    pygobject3
+    pygobject-stubs
+    loguru
+    psutil
+  ];
+
+  meta = {
+    changelog = "";
+    description = ''
+      next-gen framework for building desktop widgets using Python (check the rewrite branch for progress)
+    '';
+    homepage = "https://github.com/Fabric-Development/fabric";
+    license = lib.licenses.agpl3Plus;
+    maintainers = with lib.maintainers; [];
+  };
+}
