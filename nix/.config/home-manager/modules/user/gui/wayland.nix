9fbdcb93bac120082d7254cb4f54502671886c29
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-15 00:09:22 +0300

    [ags] new modules

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -5,14 +5,16 @@
     enable = true;
     configDir = null;
     extraPackages = [
+      inputs.ags.packages.${pkgs.system}.apps
       inputs.ags.packages.${pkgs.system}.battery
+      inputs.ags.packages.${pkgs.system}.bluetooth
       inputs.ags.packages.${pkgs.system}.hyprland
       inputs.ags.packages.${pkgs.system}.io
       inputs.ags.packages.${pkgs.system}.mpris
-      inputs.ags.packages.${pkgs.system}.notifd
-      inputs.ags.packages.${pkgs.system}.wireplumber
       inputs.ags.packages.${pkgs.system}.network
+      inputs.ags.packages.${pkgs.system}.notifd
       inputs.ags.packages.${pkgs.system}.tray
+      inputs.ags.packages.${pkgs.system}.wireplumber
     ];
   };
   home.packages = with pkgs; [
