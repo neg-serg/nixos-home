3ac45132fbeb0b7b184a64b8bf58f9c24e501ede
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-14 15:43:47 +0300

    [android] add tui file manager

--- nix/.config/home-manager/modules/dev/android/default.nix
+++ nix/.config/home-manager/modules/dev/android/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
+    adbtuifm # TUI-based file manager for the Android Debug Bridge,
     android-tools # android debug stuff
     jmtpfs # mount mtp devices
     scrcpy # rule android device via pc
