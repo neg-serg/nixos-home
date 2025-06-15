d61789e857f5bbe55e6565b43e848e8b5aaa3dde
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-15 20:08:44 +0300

    [hack] add gitleaks scanner

--- nix/.config/home-manager/modules/dev/hack/default.nix
+++ nix/.config/home-manager/modules/dev/hack/default.nix
@@ -2,6 +2,7 @@
   home.packages = with pkgs; [
     binwalk # search binary image for embedded files
     capstone # disassembly framework
+    gitleaks # scan repo for secrets
     katana # next-generation crawling and spidering framework
   ];
 }
