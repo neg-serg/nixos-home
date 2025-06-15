8dd6c79e0a5d1436045cb3d375e3246b7caf6890
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-15 20:18:52 +0300

    [terminal] add asciinema-agg converter to gif

--- nix/.config/home-manager/modules/user/terminal/default.nix
+++ nix/.config/home-manager/modules/user/terminal/default.nix
@@ -4,6 +4,7 @@
 }: {
   home.packages = with pkgs; [
     asciinema # record terminal
+    asciinema-agg # asciinema files to gif
     chafa # terminal graphics
     kitty # fastest terminal emulator so far
     kitty-img # print images inline in kitty
