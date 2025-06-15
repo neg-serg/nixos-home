c5b65bae216f46160bc2494efdf25352f26551c9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-15 22:45:26 +0300

    [rofi] add rofi-pass for wayland

--- nix/.config/home-manager/modules/user/gui/rofi.nix
+++ nix/.config/home-manager/modules/user/gui/rofi.nix
@@ -3,6 +3,7 @@ with {
   rofi-games = pkgs.callPackage ../../../packages/rofi-games {};
 }; {
   home.packages = with pkgs; [
+    rofi-pass-wayland # pass interface for rofi-wayland
     (rofi-wayland.override {
       plugins = [
         rofi-file-browser
