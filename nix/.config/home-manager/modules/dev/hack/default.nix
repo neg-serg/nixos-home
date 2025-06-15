82ee961beff4dbce094ec97913e86d737e9bec2b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-15 20:09:29 +0300

    [hack] add git-secrets

--- nix/.config/home-manager/modules/dev/hack/default.nix
+++ nix/.config/home-manager/modules/dev/hack/default.nix
@@ -3,6 +3,7 @@
     binwalk # search binary image for embedded files
     capstone # disassembly framework
     gitleaks # scan repo for secrets
+    git-secrets # prevents you from committing secrets and credentials into git repositories
     katana # next-generation crawling and spidering framework
   ];
 }
