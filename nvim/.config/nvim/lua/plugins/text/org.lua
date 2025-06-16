b006ae42df964ea7eb74392294b6e5c9ff85086b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-16 07:28:06 +0300

    [nvim] org: better settings

--- nvim/.config/nvim/lua/plugins/text/org.lua
+++ nvim/.config/nvim/lua/plugins/text/org.lua
@@ -4,8 +4,8 @@ return {
   -- └───────────────────────────────────────────────────────────────────────────────────┘
   {"nvim-orgmode/orgmode",lazy=false,ft="org",config=function()
     require("orgmode").setup({
-      org_agenda_files={"~/org/**/*"},
-      org_default_notes_file="~/org/notes.org",
+      org_agenda_files={"~/1st_level/**/*"},
+      org_default_notes_file="~/1st_level/**/*.org",
     })
   end},
   -- ┌───────────────────────────────────────────────────────────────────────────────────┐
@@ -17,7 +17,7 @@ return {
       load={
         ["core.defaults"]={},
         ["core.concealer"]={},
-        ["core.dirman"]={config={workspaces={notes="~/norg"}}},
+        ["core.dirman"]={config={workspaces={notes="~/1st_level/**/*.norg"}}},
       },
     })
   end},
