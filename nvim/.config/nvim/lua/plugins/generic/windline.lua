b5d07483cfda2eaf48ac30c5183cc68cf93b0a61
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-16 07:35:03 +0300

    [windline] cleanup code

--- nvim/.config/nvim/lua/plugins/generic/windline.lua
+++ nvim/.config/nvim/lua/plugins/generic/windline.lua
@@ -1,249 +1,124 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ windwp/windline.nvim                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'windwp/windline.nvim', -- most modern statusline
-    config=function()
-        local windline=require'windline'
-        local helper=require'windline.helpers'
-        local b_components=require'windline.components.basic'
-        local cache_utils=require'windline.cache_utils'
-        local lsp_comps=require'windline.components.lsp'
-        local git_comps=require'windline.components.git'
+return {{'windwp/windline.nvim', config = function()
+      local wl = require'windline'
+      local comp = require'windline.components.basic'
+      local cache = require'windline.cache_utils'
+      local lsp = require'windline.components.lsp'
+      local git = require'windline.components.git'
 
-        local hl_list={
-            Black={'white', 'black'},
-            White={'black', 'white'},
-            Inactive={'InactiveFg', 'InactiveBg'},
-            Active={'ActiveFg', 'ActiveBg'},
+      local colors = {
+        black = 'NONE', white = '#54667a', red = '#970d4f', green = '#007a51',
+        blue = '#005faf', yellow = '#c678dd', cyan = '#6587b3', base = '#234758',
+        NormalBg = '#282828', InactiveFg = '#c6c6c6', InactiveBg = '#3c3836'
+      }
+
+      local hl = {
+        Black = {'white', 'black'},
+        White = {'black', 'white'},
+        Inactive = {'InactiveFg', 'InactiveBg'}
+      }
+
+      local bp_width = 90
+      local sep = require'windline.helpers'.separators.slant_right
+
+      local function file_components(wide)
+        return wide and {
+          {cache.cache_on_buffer('BufEnter', 'dir_symbol', function()
+            return vim.fn.empty(vim.fn.expand('%:t')) ~= 1 and '' or ''
+          end), 'blue'},
+          {' ', ''},
+          {cache.cache_on_buffer({'BufEnter', 'DirChanged'}, 'WL_filename', function()
+            return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+          end), 'default'},
+          {cache.cache_on_buffer('BufEnter', 'delimiter', function()
+            return vim.fn.empty(vim.fn.expand('%:t')) ~= 1 and ' ¦' or ''
+          end), 'blue'},
+          {' ', ''},
+          {comp.file_name(''), 'cyan'},
+          {comp.file_modified(' '), 'blue'}
+        } or {
+          {cache.cache_on_buffer({'BufEnter', 'DirChanged'}, 'WL_filename', function()
+            return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+          end), 'cyan'},
+          {comp.cache_file_size(), 'default'},
+          {' ', ''},
+          {comp.file_modified(' '), 'blue'}
         }
-        local basic={}
+      end
 
-        local breakpoint_width=90
-        basic.divider={b_components.divider, ''}
-        basic.bg={' ', 'StatusLine'}
+      local components = {
+        divider = {comp.divider, ''},
+        file = {
+          hl_colors = {default = hl.Black, white = {'white', 'black'}, cyan = {'cyan', 'black'}, blue = {'blue', 'black'}},
+          text = function(_, _, w) return file_components(w > bp_width) end
+        },
+        lsp_diagnos = {
+          hl_colors = {red = {'red', 'black'}, yellow = {'yellow', 'black'}, blue = {'blue', 'black'}},
+          width = bp_width,
+          text = function(b) return lsp.check_lsp(b) and {
+            {lsp.lsp_error({format = '  %s', show_zero = false}), 'red'},
+            {lsp.lsp_warning({format = '  %s', show_zero = false}), 'yellow'},
+            {lsp.lsp_hint({format = '  %s', show_zero = false}), 'blue'}
+          } or '' end
+        },
+        git = {
+          hl_colors = {green = {'green', 'black'}, red = {'red', 'black'}, white = {'white', 'black'}},
+          width = bp_width,
+          text = function(b) return git.is_git(b) and {
+            {git.diff_added({format = '  %s', show_zero = false}), 'green'},
+            {git.diff_changed({format = '  %s', show_zero = false}), 'white'},
+            {git.diff_removed({format = '  %s', show_zero = false}), 'red'}
+          } or '' end
+        }
+      }
 
-        basic.lsp_diagnos={
-            name='diagnostic',
-            hl_colors={
-                red={'red','black'},
-                yellow={'yellow','black'},
-                blue={'blue','black'},
+      wl.setup({
+        colors_name = function() return colors end,
+        statuslines = {
+          {
+            filetypes = {'default'},
+            active = {
+              components.file,
+              components.lsp_diagnos,
+              components.divider,
+              {comp.cache_file_size(), {'white', 'black'}},
+              {' ', hl.Black},
+              {git.git_branch({icon = '  '}), {'blue', 'black'}, bp_width},
+              {' ', hl.Black}
             },
-            width=breakpoint_width,
-            text=function(bufnr)
-                if lsp_comps.check_lsp(bufnr) then
-                    return {
-                        {lsp_comps.lsp_error({format='  %s', show_zero=false}), 'red'},
-                        {lsp_comps.lsp_warning({format='  %s', show_zero=false}), 'yellow'},
-                        {lsp_comps.lsp_hint({format='  %s', show_zero=false}), 'blue'},
-                    }
-                end
-                return ''
-            end,
-        }
-
-        local function dir_name(default, modify)
-            default = default or ''
-            modify = modify or 'name'
-            return function()
-                local dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
-                if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-                    return dir
-                else
-                    return default
-                end
-            end
-        end
-
-        local function delimiter()
-            return function()
-                if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-                    return ' ¦'
-                else
-                    return ''
-                end
-            end
-        end
-
-        local function dir_symbol()
-            return function()
-                if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-                    return ''
-                else
-                    return ''
-                end
-            end
-        end
-
-        local cache_dir_name = function(default, modify)
-            return cache_utils.cache_on_buffer({'BufEnter', 'DirChanged'}, 'WL_filename', dir_name(default, modify))
-        end
-        local cache_delimiter = function()
-            return cache_utils.cache_on_buffer('BufEnter', 'delimiter', delimiter())
-        end
-        local cache_dir_symbol = function()
-            return cache_utils.cache_on_buffer('BufEnter', 'dir_symbol', dir_symbol())
-        end
-
-        basic.file = {
-            name = 'file',
-            hl_colors = {
-                default = hl_list.Black,
-                white = {'white', 'black'},
-                cyan = {'cyan', 'black'},
-                blue = {'blue', 'black'},
+            inactive = {
+              {comp.full_file_name, hl.Inactive},
+              components.divider,
+              {comp.progress, hl.Inactive}
+            }
+          },
+          {
+            filetypes = {'qf', 'Trouble'},
+            active = {
+              {'Quickfix ', {'white', 'black'}},
+              {function() return vim.fn.getqflist({title = 0}).title end, {'blue', 'base'}},
+              {' Total:%L ', {'base', 'black'}}
             },
-            text = function(_, _, width)
-                if width > breakpoint_width then
-                    return {
-                        {cache_dir_symbol(), 'blue'},
-                        {' ', ''},
-                        {cache_dir_name(), 'default'},
-                        {cache_delimiter(), 'blue'},
-                        {' ', ''},
-                        {b_components.file_name(''), 'cyan'},
-                        {b_components.file_modified(' '), 'blue'},
-                    }
-                else
-                    return {
-                        {cache_dir_name(), 'cyan'},
-                        {b_components.cache_file_size(), 'default'},
-                        {' ', ''},
-                        {b_components.file_modified(' '), 'blue'},
-                    }
-                end
-            end,
-        }
-
-        basic.file_right={
-            hl_colors={
-                default=hl_list.Black,
-                white={'white', 'black'},
-                cyan={'cyan', 'black'},
+            always_active = true
+          },
+          {
+            filetypes = {'fern', 'NvimTree', 'lir'},
+            active = {
+              {'  ', {'black', 'red'}},
+              {sep, {'red', 'NormalBg'}},
+              components.divider,
+              {comp.file_name(''), {'white', 'NormalBg'}}
             },
-            text=function(_, _, width)
-                if width < breakpoint_width then
-                    return {{b_components.progress_lua, ''}}
-                end
-            end,
+            always_active = true
+          },
+          {
+            filetypes = {'TelescopePrompt'},
+            active = {{'  ', {'white', 'black'}}}
+          }
         }
-
-        basic.git = {
-            name='git',
-            hl_colors={
-                green={'green', 'black'},
-                red={'red', 'black'},
-                white={'white', 'black'},
-                blue={'blue', 'black'},
-            },
-            width = breakpoint_width,
-            text = function(bufnr)
-                if git_comps.is_git(bufnr) then
-                    return {
-                        {git_comps.diff_added({format='  %s', show_zero=false}), 'green'},
-                        {git_comps.diff_changed({format='  %s', show_zero=false}), 'white'},
-                        {git_comps.diff_removed({format='  %s', show_zero=false}), 'red'},
-                    }
-                end
-                return ''
-            end,
-        }
-
-        local quickfix={
-            filetypes={'qf','Trouble'},
-            active={
-                {'Quickfix ',{'white','black'}},
-                {function() return vim.fn.getqflist({title=0}).title end,{'blue','base'}},
-                {' Total:%L ',{'base','black'}},
-                {' ',{'InactiveFg','black'}},
-                {'',{'black','black'}},
-            },
-            always_active=true,
-            show_last_status=true,
-        }
-
-        local explorer={
-            filetypes={'fern', 'NvimTree', 'lir'},
-            active={
-                {'  ', {'black', 'red'}},
-                {helper.separators.slant_right, {'red', 'NormalBg'}},
-                {b_components.divider, ''},
-                {b_components.file_name(''), {'white', 'NormalBg'}},
-            },
-            always_active=true,
-            show_last_status=true,
-        }
-
-        basic.lsp_name={
-            width=breakpoint_width,
-            name='lsp_name',
-            hl_colors={cyan={'cyan', 'black'}},
-            text=function(bufnr)
-                if lsp_comps.check_lsp(bufnr) then
-                    return {{lsp_comps.lsp_name(), 'cyan'}}
-                end
-                return {
-                    {b_components.cache_file_type({icon=true}), 'cyan'},
-                }
-            end,
-        }
-
-        local default={
-            filetypes={'default'},
-            active={
-                basic.file,
-                basic.lsp_diagnos,
-                basic.divider,
-                {b_components.cache_file_size(), {'white','black'}},
-                {' ', hl_list.Black},
-                basic.file_right,
-                basic.lsp_name,
-                basic.git,
-                {git_comps.git_branch({icon='  '}), {'blue', 'black'}, breakpoint_width},
-                {' ', hl_list.Black},
-            },
-            inactive={
-                {b_components.full_file_name, hl_list.Inactive},
-                basic.divider,
-                {b_components.progress, hl_list.Inactive},
-            },
-        }
-
-        local telescope={
-            filetypes={'TelescopePrompt'},
-            active={{'  ',{'white','black'}}},
-        }
-
-        windline.setup({
-            colors_name=function(colors)
-                colors={
-                    black='NONE',
-                    black_light='NONE',
-                    white='#54667a',
-                    red='#970d4f',
-                    green='#007a51',
-                    blue='#005faf',
-                    yellow='#c678dd',
-                    magenta='#c678dd',
-                    base='#234758',
-                    blue_light="#517f8d",
-                    cyan='#6587b3',
-
-                    NormalFg="#ff0000",
-                    NormalBg="#282828",
-                    InactiveFg='#c6c6c6',
-                    InactiveBg='#3c3836',
-                    ActiveFg='#54667a',
-                    ActiveBg='NONE',
-                    FileName=colors.cyan,
-                }
-                return colors
-            end,
-            statuslines={
-                default,
-                quickfix,
-                explorer,
-                telescope,
-            },
-        })
-    end}
+      })
+    end
+  }
+}
