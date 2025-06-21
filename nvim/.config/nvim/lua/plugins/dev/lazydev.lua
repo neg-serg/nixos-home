b78d5cba2f29e38185213e1916dc515884a029ae
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-21 04:05:41 +0300

    [nvim] update lazydev stuff

--- nvim/.config/nvim/lua/plugins/dev/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/dev/lazydev.lua
@@ -1,8 +1,14 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ folke/lazydev.nvim                                                           │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-return {{"folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {library = {},},
+return {
+  {"folke/lazydev.nvim", ft = "lua",
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
   },
 }
