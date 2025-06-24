5ff41fbe29959f6520da6b86165bc1b5ac3a7aa4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-24 09:25:40 +0300

    [editor] add lmstudio

--- nix/.config/home-manager/modules/dev/editor/default.nix
+++ nix/.config/home-manager/modules/dev/editor/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     code-cursor-fhs # AI-powered code editor built on vscode
+    lmstudio # LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models
     zed # to try new editor with the native llm support
   ];
   imports = [
