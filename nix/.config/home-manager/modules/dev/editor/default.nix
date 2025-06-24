744cf3a1f73a0460a3158a71de731b6ec9cfc194
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-24 09:21:51 +0300

    [dev] add code-cursor

--- nix/.config/home-manager/modules/dev/editor/default.nix
+++ nix/.config/home-manager/modules/dev/editor/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-      zed # to try new editor with the native llm support
+    code-cursor-fhs # AI-powered code editor built on vscode
+    zed # to try new editor with the native llm support
   ];
   imports = [
     ./neovim
