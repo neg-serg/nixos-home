48ffc531e9c160482ce34fd36b6d3e1af9f23972
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-24 05:27:17 +0300

    [creation] cleanup stuff

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -1,16 +1,12 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     bespokesynth # nice modular synth
-    guitarix # virtual guitar amplifier for Linux running with JACK
-    gxplugins-lv2 # set of extra lv2 plugins from the guitarix project
     noisetorch # virtual microphone to suppress the noise
     ocenaudio # good audio editor
-    orca-c # esoteric programming language designed to quickly create procedural sequencers
     reaper # А вот в рипере!
     rnnoise # neural network noise reduction
     stochas # nice free sequencer
     sunvox # fast and powerful modular synthesizer with pattern-based sequencer
-    surge-XT # great wavetable synth 
     vcv-rack # powerful soft modular synth
     vital # serum-like digital synth
   ];
