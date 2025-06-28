c2607254792b94282e52c291dbc931378b1d08fa
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-28 12:21:26 +0300

    [creation] new dx7-like synth

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     bespokesynth # nice modular synth
+    dexed # nice yamaha dx7-like fm synth
     noisetorch # virtual microphone to suppress the noise
     # ocenaudio # good audio editor
     reaper # А вот в рипере!
