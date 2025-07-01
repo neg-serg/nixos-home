7ac95edb54609c04ac5f03d0567e6b98c53e4c6c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-01 20:49:11 +0300

    [nvim] render-markdown: stash checkbox

--- nvim/.config/nvim/lua/plugins/text/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/text/render-markdown.lua
@@ -2,7 +2,7 @@
 -- │ █▓▒░ MeanderingProgrammer/render-markdown.nvim                                    │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {
-    enabled=false,
+    enabled=true,
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies={'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim'}, -- if you use the mini.nvim suite
     opts = {},
@@ -16,16 +16,44 @@ return {
             conceallevel = { default = vim.o.conceallevel, rendered = 3 },
             concealcursor = { default = vim.o.concealcursor, rendered = '' },
         },
-        anti_conceal={
-            enabled=false,
-            -- Which elements to always show, ignoring anti conceal behavior. Values can either be
-            -- booleans to fix the behavior or string lists representing modes where anti conceal
-            -- behavior will be ignored. Valid values are:
-            --   head_icon, head_background, head_border, code_language, code_background, code_border,
-            --   dash, bullet, check_icon, check_scope, quote, table_border, callout, link, sign
-            ignore={code_background = true, sign = true,},
-            above=0,
-            below=0,
+        checkbox = {
+            -- Checkboxes are a special instance of a 'list_item' that start with a 'shortcut_link'.
+            -- There are two special states for unchecked & checked defined in the markdown grammar.
+            -- Turn on / off checkbox state rendering.
+            enabled = true,
+            -- Additional modes to render checkboxes.
+            render_modes = true,
+            -- Render the bullet point before the checkbox.
+            bullet = false,
+            -- Padding to add to the right of checkboxes.
+            right_pad = 1,
+            unchecked = {
+                -- Replaces '[ ]' of 'task_list_marker_unchecked'.
+                icon = '󰄱 ',
+                -- Highlight for the unchecked icon.
+                highlight = 'RenderMarkdownUnchecked',
+                -- Highlight for item associated with unchecked checkbox.
+                scope_highlight = nil,
+            },
+            checked = {
+                -- Replaces '[x]' of 'task_list_marker_checked'.
+                icon = '󰱒 ',
+                -- Highlight for the checked icon.
+                highlight = 'RenderMarkdownChecked',
+                -- Highlight for item associated with checked checkbox.
+                scope_highlight = nil,
+            },
+            -- Define custom checkbox states, more involved, not part of the markdown grammar.
+            -- As a result this requires neovim >= 0.10.0 since it relies on 'inline' extmarks.
+            -- The key is for healthcheck and to allow users to change its values, value type below.
+            -- | raw             | matched against the raw text of a 'shortcut_link'           |
+            -- | rendered        | replaces the 'raw' value when rendering                     |
+            -- | highlight       | highlight for the 'rendered' icon                           |
+            -- | scope_highlight | optional highlight for item associated with custom checkbox |
+            -- stylua: ignore
+            custom = {
+                todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
+            },
         },
       })
     end
