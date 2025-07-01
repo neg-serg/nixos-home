18c96d0d283fec0274fd047c9651b00ad2ae681f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-01 08:22:14 +0300

    [nvim] render-markdown: cleanup

--- nvim/.config/nvim/lua/plugins/text/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/text/render-markdown.lua
@@ -5,13 +5,9 @@ return {
     enabled=true,
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
     opts = {},
     setup = function()
-      require('render-markdown').setup({
+      require'render-markdown'.setup({
         completions = { blink = { enabled = true } },
       })
     end
