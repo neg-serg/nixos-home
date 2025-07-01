dd995ede92aeb44d063ea21b25372c7337bc7036
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-01 20:43:58 +0300

    [nvim] render-markdown: try new stuff

--- nvim/.config/nvim/lua/plugins/text/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/text/render-markdown.lua
@@ -2,13 +2,31 @@
 -- │ █▓▒░ MeanderingProgrammer/render-markdown.nvim                                    │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {
-    enabled=true,
+    enabled=false,
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    dependencies={'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim'}, -- if you use the mini.nvim suite
     opts = {},
+    ft={'markdown', 'quarto'},
     setup = function()
       require'render-markdown'.setup({
-        completions = { blink = { enabled = true } },
+        enabled=true,
+        completions={blink={enabled=true}},
+        render_modes=true,
+        win_options = {
+            conceallevel = { default = vim.o.conceallevel, rendered = 3 },
+            concealcursor = { default = vim.o.concealcursor, rendered = '' },
+        },
+        anti_conceal={
+            enabled=false,
+            -- Which elements to always show, ignoring anti conceal behavior. Values can either be
+            -- booleans to fix the behavior or string lists representing modes where anti conceal
+            -- behavior will be ignored. Valid values are:
+            --   head_icon, head_background, head_border, code_language, code_background, code_border,
+            --   dash, bullet, check_icon, check_scope, quote, table_border, callout, link, sign
+            ignore={code_background = true, sign = true,},
+            above=0,
+            below=0,
+        },
       })
     end
 }
