091ffc27cf70943e72f7496976f0301acd7ece32
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-03 07:08:07 +0300

    [shell] install nix-your-shell

--- nix/.config/home-manager/modules/cli/shell.nix
+++ nix/.config/home-manager/modules/cli/shell.nix
@@ -3,6 +3,9 @@
   ...
 }: {
   programs.carapace.enable = true; # cross-shell completion
+  programs.nix-your-shell = {
+    enable = true;
+  };
   home.packages = with pkgs; [
     dash # faster sh
     nushell # alternative shell
