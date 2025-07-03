75c6fd77dab744d701f40d10bbf1b3fa7e34ea9d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-03 03:51:05 +0300

    [gui] cleanup

--- nix/.config/home-manager/modules/user/gui/apps.nix
+++ nix/.config/home-manager/modules/user/gui/apps.nix
@@ -1,11 +1,8 @@
 {pkgs, inputs, ...}: {
   programs.walllust.enable = true;
   home.packages = with pkgs; [
-    autocutsel # tool to sync x11 buffers
-    clipboard-jh # platform independent clipboard manager, test it more later
     cliphist # wayland stuff for clipboard
     espanso # systemwide expander for keyboard
-    haskellPackages.greenclip # yet another clipboard manager
     inputs.bzmenu.packages.${pkgs.system}.default # bluetooth support menu
     matugen # modern theme generator like pywal
   ];
