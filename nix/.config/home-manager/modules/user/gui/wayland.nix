8803f81c5bb5d5e1cdb73baf4519223a38eeef04
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-05 03:10:22 +0300

    [ags] cleanup

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -1,29 +1,10 @@
 {pkgs, inputs, ...}: {
   home.sessionVariables = {};
-  imports = [ inputs.ags.homeManagerModules.default ];
-  programs.ags = {
-    enable = true;
-    configDir = null;
-    extraPackages = [
-      inputs.ags.packages.${pkgs.system}.apps
-      inputs.ags.packages.${pkgs.system}.battery
-      inputs.ags.packages.${pkgs.system}.bluetooth
-      inputs.ags.packages.${pkgs.system}.hyprland
-      inputs.ags.packages.${pkgs.system}.io
-      inputs.ags.packages.${pkgs.system}.mpris
-      inputs.ags.packages.${pkgs.system}.network
-      inputs.ags.packages.${pkgs.system}.notifd
-      inputs.ags.packages.${pkgs.system}.powerprofiles
-      inputs.ags.packages.${pkgs.system}.tray
-      inputs.ags.packages.${pkgs.system}.wireplumber
-    ];
-  };
   home.packages = with pkgs; [
     clipse # yet another clipboard manager
     fuzzel # wayland launcher
     gowall # tool to convert a Wallpaper's color scheme / palette
     grim # to take screenshots
-    inputs.astal.packages.${system}.default # astal library support
     inputs.iwmenu.packages.${pkgs.system}.default # wifi menu
     satty # screenshot helper tool
     slurp # select region in wayland compositor
