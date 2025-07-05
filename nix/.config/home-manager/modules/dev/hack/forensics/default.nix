9b3243b7a5f8685330481e478e671cd0273d4e6a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-05 04:31:46 +0300

    [forensics] updates

--- nix/.config/home-manager/modules/dev/hack/forensics/default.nix
+++ nix/.config/home-manager/modules/dev/hack/forensics/default.nix
@@ -1,15 +1,20 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    binwalk # tool library for analyzing binary blobs and executable code
     capstone
     ddrescue
+    distorm3
     ext4magic
     extundelete
     ghidra-bin
+    outguess # universal steganographic tool that allows the insertion of hidden information into the redundant bits of data sources
     p0f
     pdf-parser
-    # master.binwalk # Tool library for analyzing binary blobs and executable code
-    distorm3
     sleuthkit
-    # volatility
+    steghide # open source steganography program
+    stegseek # tool to crack steganography
+    stegsolve # steganographic image analyzer, solver and data extractor for challanges
+    volatility3 # memory extraction framework
+    zsteg # detect stegano-hidden data in PNG & BMP
   ];
 }
