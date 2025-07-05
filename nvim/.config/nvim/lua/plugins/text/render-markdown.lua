0fc3db0314b55f450ef1d54511544ccbf45a0a07
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-05 11:54:41 +0300

    [nvim] render-markdown: fix

--- nvim/.config/nvim/lua/plugins/text/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/text/render-markdown.lua
@@ -2,21 +2,34 @@
 -- │ █▓▒░ MeanderingProgrammer/render-markdown.nvim                                    │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {'MeanderingProgrammer/render-markdown.nvim',
-  -- enabled=false,
-  name='render-markdown',
-  dependencies={'nvim-treesitter/nvim-treesitter'}, -- if you use the mini.nvim suite
-  ft={'markdown', 'quarto', 'md'}, opts={},
-  opts = {
-      theme = "dark",
-      headings = {
-          ["h1"] = { fg = "#FFD700", bold = true },
-          ["h2"] = { fg = "#FFA500", bold = true },
-          ["h3"] = { fg = "#FF8C00", bold = true },
+  dependencies={'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim'}, -- if you use the mini.nvim suite
+  -- ft={'markdown', 'quarto'},
+  config=function()
+    require'render-markdown'.setup{
+      completions={blink={enabled=true}},
+      render_modes=true,
+      win_options={
+        conceallevel={default=vim.o.conceallevel, rendered=3},
+        concealcursor={default=vim.o.concealcursor, rendered=''},
       },
-      bullets = { "•", "◦", "▪" },    -- свои маркеры
-      code = { background = "#1e1e2e", border = "rounded", },
-  },
-  config = function(_, opts)
-      require("render-markdown").setup(opts)
-  end,
+      indent = {
+        enabled = true,
+        character = " "  -- можно указать символ отступа (пробел или что угодно)
+      },
+      checkbox={
+        enabled=true,
+        render_modes=true, -- Additional modes to render checkboxes.
+        bullet=false, -- Render the bullet point before the checkbox.
+        right_pad=1, -- Padding to add to the right of checkboxes.
+        unchecked={
+          icon='✘ ', -- Replaces '[ ]' of 'task_list_marker_unchecked'.
+          highlight='MoreMsg', -- Highlight for the unchecked icon.
+        },
+        checked={
+          icon='✔ ', -- Replaces '[x]' of 'task_list_marker_checked'.
+          highlight='RenderMarkdownChecked', -- Highlight for the checked icon.
+        }
+    }
+  }
+  end
 }
