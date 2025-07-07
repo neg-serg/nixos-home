ba8ec0c219d77107a1a0f22da7df72be7d185cda
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-07 08:36:19 +0300

    [browsing] add tor-browser

--- nix/.config/home-manager/modules/user/web/browsing.nix
+++ nix/.config/home-manager/modules/user/web/browsing.nix
@@ -8,6 +8,6 @@
   home.packages = with pkgs; [
     yandex-browser.packages.x86_64-linux.yandex-browser-stable # google chrome-based yandex fork
     passff-host # host app for the WebExtension PassFF
-    # tor-browser # browse web via tor
+    tor-browser # browse web via tor
   ];
 }
