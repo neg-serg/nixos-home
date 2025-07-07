1152fedadf1abce4e6a387ba34d29775e2592a9e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-07 08:37:53 +0300

    [browsing] cleanup

--- nix/.config/home-manager/modules/user/web/browsing.nix
+++ nix/.config/home-manager/modules/user/web/browsing.nix
@@ -2,12 +2,9 @@
   imports = [
     ./firefox.nix
     # ./floorp.nix
-    # ./qutebrowser.nix
-    # ./nyxt.nix
   ];
   home.packages = with pkgs; [
     yandex-browser.packages.x86_64-linux.yandex-browser-stable # google chrome-based yandex fork
     passff-host # host app for the WebExtension PassFF
-    tor-browser # browse web via tor
   ];
 }
