0f0381a8501b2aa8d29ba7a6fa2ce892bba692df
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-07 05:11:53 +0300

    [wayland] add waypipe

--- nix/.config/home-manager/modules/user/gui/wayland.nix
+++ nix/.config/home-manager/modules/user/gui/wayland.nix
@@ -11,6 +11,7 @@
     swww # wallpaper daemon for wayland
     tofi # an extremely fast and simple dmenu / rofi replacement for wlroots-based Wayland compositors
     waybar # install temporary
+    waypipe # proxy for wayland similar to ssh -X
     wev # xev for wayland
     wf-recorder # tool to make screencasts
     wl-clipboard # copy-paste for wayland
