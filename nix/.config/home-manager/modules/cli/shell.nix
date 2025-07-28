6a7060b633ea625338b5e49834874c6ee6c92414
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-28 05:38:31 +0300

    [cli] oh-my-posh: initial commit

--- nix/.config/home-manager/modules/cli/shell.nix
+++ nix/.config/home-manager/modules/cli/shell.nix
@@ -3,6 +3,8 @@
   ...
 }: {
   programs.carapace.enable = true; # cross-shell completion
+  programs.oh-my-posh.enable = true;
+  programs.oh-my-posh.useTheme = "atomic";
   programs.nix-your-shell = {
     enable = true;
   };
