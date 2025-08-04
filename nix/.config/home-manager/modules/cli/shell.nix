6e35b40d5100d1eb26392caa146c58cd139e018d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-04 03:22:18 +0300

    [shell] remove carapace from home-manager

--- nix/.config/home-manager/modules/cli/shell.nix
+++ nix/.config/home-manager/modules/cli/shell.nix
@@ -2,7 +2,6 @@
   pkgs,
   ...
 }: {
-  programs.carapace.enable = true; # cross-shell completion
   programs.oh-my-posh.enable = true;
   programs.oh-my-posh.useTheme = "atomic";
   programs.nix-your-shell = {
