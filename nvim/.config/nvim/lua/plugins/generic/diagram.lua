d879c8d39febde9ecc3c5a44ed8594a8cb510e42
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-07 02:01:52 +0300

    [nvim] disable diagram temporary

--- nvim/.config/nvim/lua/plugins/generic/diagram.lua
+++ nvim/.config/nvim/lua/plugins/generic/diagram.lua
@@ -3,6 +3,7 @@
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {
   "3rd/diagram.nvim",
+  enabled = false,
   dependencies = { "3rd/image.nvim", },
   opts = {
     events = {
