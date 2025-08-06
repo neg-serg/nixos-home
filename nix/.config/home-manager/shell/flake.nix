2417e06998fe81fba0ccc19c71ed7a30975cd365
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-06 03:32:48 +0300

    [shell] update

--- nix/.config/home-manager/shell/flake.nix
+++ nix/.config/home-manager/shell/flake.nix
@@ -1,23 +1,29 @@
 {
   description = "A minimal flake with a devShell.";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} (
-      {lib, ...}: {
-        systems = lib.systems.flakeExposed;
-        perSystem = {pkgs, ...}: {
-          devShells.default = with pkgs;
-            mkShellNoCC {
-              packages = [
-                # your packages here
-              ];
-            };
+  outputs = inputs @ {flake-parts, nixpkgs, ...}:
+    flake-parts.lib.mkFlake { inherit inputs; } ({
+      lib, ... 
+    }: {
+      systems = lib.systems.flakeExposed;
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            hyperfine
+            kitty
+            rustc
+            wl-clipboard
+            xclip
+          ];
+
+          RUST_BACKTRACE = "1";
         };
-      }
-    );
+      };
+    });
 }
