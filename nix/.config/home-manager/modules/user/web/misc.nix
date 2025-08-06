531a129380d16253ee186560d2c619b726b01db9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-06 15:12:35 +0300

    [web] megacmd broke my bash

--- nix/.config/home-manager/modules/user/web/misc.nix
+++ nix/.config/home-manager/modules/user/web/misc.nix
@@ -4,7 +4,6 @@
 }: {
   home.packages = with pkgs; [
     gallery-dl # download image galleries/collections
-    megacmd # cli for MEGA
     monolith # download all webpage stuff as one file
     pipe-viewer # lightweight youtube client
     prettyping # fancy ping
