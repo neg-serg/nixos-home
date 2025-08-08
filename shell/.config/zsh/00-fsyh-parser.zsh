134a7bf1ddce8797ab7427aa27b3bf3534db4438
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-08 22:30:01 +0300

    [zsh] fix parser

--- shell/.config/zsh/00-fsyh-parser.zsh
+++ shell/.config/zsh/00-fsyh-parser.zsh
@@ -1,10 +1,59 @@
-setopt rematchpcre
-typeset -A FILE_EXTENSION_STYLES
+setopt rematchpcre  # Enable PCRE regex matching for =~ operator
+typeset -gA FILE_EXTENSION_STYLES  # Global associative array for file extension styles
 section=""
-while read line; do
-  if [[ "$line" =~ '^\[(.+)\]' ]]; then
-    section=${match[1]}
-  elif [[ "$section" == "file-extensions" && "$line" =~ '^([^ =]+)[ \t]*=[ \t]*(.+)$' ]]; then
-    FILE_EXTENSION_STYLES[${match[1]}]=${match[2]}
+
+# Process each line in the INI file
+while IFS= read -r line; do
+  # Clean up the line:
+  line="${line%%\;*}"     # Remove everything after ;
+  line="${line%%\#*}"     # Remove everything after #
+  line="${line#"${line%%[![:space:]]*}"}"  # Trim leading whitespace
+  line="${line%"${line##*[![:space:]]}"}"  # Trim trailing whitespace
+  [[ -z "$line" ]] && continue  # Skip empty lines
+
+  # Check for section headers [section-name]
+  if [[ "$line" =~ '^\[(.*)\]$' ]]; then
+    section="${match[1]}"
+  
+  # Process file-extension styles
+  elif [[ "$section" == "file-extensions" && "$line" =~ '^([a-zA-Z0-9_+-]+)[[:space:]]*=[[:space:]]*(.*)$' ]]; then
+    key="${match[1]}"
+    value="${match[2]}"
+    value="${value//\"/}"  # Remove double quotes
+
+    # Transform style format for f-sy-h compatibility
+    if [[ "$value" == bg:* ]]; then
+      # Handle background styles (bg:color,fg,attributes)
+      rest=${value#bg:}  # Remove 'bg:' prefix
+      parts=(${(s:,:)rest})  # Split by commas
+      bg_color=${parts[1]}  # First part is background color
+      shift parts  # Remove bg color from parts array
+      
+      new_value="bg=$bg_color"  # Start with background
+      
+      # Check if next part is foreground color (numeric)
+      if [[ -n "$parts[1]" && "$parts[1]" =~ '^[0-9]+$' ]]; then
+        new_value+=",fg=$parts[1]"  # Add foreground
+        shift parts  # Remove fg color
+      fi
+      
+      # Add any remaining attributes (bold, underline, etc)
+      [[ ${#parts} -gt 0 ]] && new_value+=",${(j:,:)parts}"
+      value=$new_value
+      
+    else
+      # Handle regular styles (fg=color,attributes)
+      value="fg=$value"  # Add fg= prefix
+    fi
+
+    FILE_EXTENSION_STYLES[$key]="$value"
   fi
 done < ${ZDOTDIR}/neg.ini
+
+# Apply styles to fast-syntax-highlighting
+FAST_THEME_NAME="neg"
+typeset -gA FAST_HIGHLIGHT_STYLES
+for ext style in "${(@kv)FILE_EXTENSION_STYLES}"; do
+  [[ -n $ext && -n $style ]] || continue
+  FAST_HIGHLIGHT_STYLES[${FAST_THEME_NAME}file-extensions-${ext}]="$style"
+done
