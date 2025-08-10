35ddc7e8ea2263c51f89a2477383f656e2c4990b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-10 19:07:06 +0300

    [nvim] remove overseer

--- nvim/.config/nvim/lua/plugins/dev/overseer.lua
+++
@@ -1,6 +0,0 @@
--- ┌───────────────────────────────────────────────────────────────────────────────────┐
--- │ █▓▒░ stevearc/overseer.nvim                                                       │
--- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'stevearc/overseer.nvim', -- provide async build, modern dispatch-vim alternative
-    opts = {},
-}
