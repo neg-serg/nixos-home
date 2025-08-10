03daf7aee4572994933037c2c80bf7e99a18dc24
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-10 03:47:30 +0300

    [nvim] cleanup

--- nvim/.config/nvim/lua/plugins/generic/nvim-alt-substitute.lua
+++
@@ -1,6 +0,0 @@
--- ┌───────────────────────────────────────────────────────────────────────────────────┐
--- │ █▓▒░ chrisgrieser/nvim-alt-substitute                                             │
--- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'chrisgrieser/nvim-alt-substitute', -- alternative substitute
-    config=function() require'alt-substitute'.setup() end,
-    event={'CmdlineEnter'}}  -- lazy-loading with `cmd =` does not work well with incremental preview
