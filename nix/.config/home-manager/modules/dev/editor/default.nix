416836808a8ffcba24e5562650fd116d9172b140
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-24 20:40:14 +0300

    [dev] zed is zed editor actually

--- nix/.config/home-manager/modules/dev/editor/default.nix
+++ nix/.config/home-manager/modules/dev/editor/default.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs; [
     code-cursor-fhs # AI-powered code editor built on vscode
     lmstudio # LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models
-    zed # to try new editor with the native llm support
+    zed-editor # to try new editor with the native llm support
   ];
   imports = [
     ./neovim
