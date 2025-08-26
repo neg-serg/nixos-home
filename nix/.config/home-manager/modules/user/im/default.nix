aae090450d02fffed8900b9520374994938b5008
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-26 22:28:05 +0300

    [im] install tdl telegram cli uploader/downloader

--- nix/.config/home-manager/modules/user/im/default.nix
+++ nix/.config/home-manager/modules/user/im/default.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    tdl # telegram cli downloader/uploader
     telegram-desktop # famous cloud-based im
     vesktop # alternative discord client
   ];
