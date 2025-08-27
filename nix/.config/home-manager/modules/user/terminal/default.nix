674ddbb6d8655cd14f9bdc47c8cf8acd7f50d8b3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-27 04:20:49 +0300

    [terminal] better sorting

--- nix/.config/home-manager/modules/user/terminal/default.nix
+++ nix/.config/home-manager/modules/user/terminal/default.nix
@@ -3,8 +3,8 @@
   ...
 }: {
   home.packages = with pkgs; [
-    asciinema # record terminal
     asciinema-agg # asciinema files to gif
+    asciinema # record terminal
     chafa # terminal graphics
     kitty # fastest terminal emulator so far
     kitty-img # print images inline in kitty
