0415130a0f646b15a07af01b5836a47cbf872ce7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-28 21:05:05 +0300

    [nvim] fix toggleterm binding

--- nvim/.config/nvim/lua/plugins/generic/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/generic/toggleterm.lua
@@ -15,7 +15,7 @@ return {'akinsho/toggleterm.nvim', -- better way to toggle term
         })
         local Terminal=require('toggleterm.terminal').Terminal
         navigator=Terminal:new({cmd='zsh', env={NEOVIM_TERMINAL=1}})
-        map('n', 'e3', '<Cmd>lua navigator:toggle()<CR>', {noremap=true, silent=true})
+        map('n', 'ei', '<Cmd>lua navigator:toggle()<CR>', {noremap=true, silent=true})
     end,
-    keys={'e3'},
+    keys={'ei'},
 }
