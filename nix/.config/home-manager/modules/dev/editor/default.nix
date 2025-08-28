0ec319a9567dba06b0de0d094f6d71925a8badb3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 00:44:09 +0300

    [lapce] add

--- nix/.config/home-manager/modules/dev/editor/default.nix
+++ nix/.config/home-manager/modules/dev/editor/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     code-cursor-fhs # AI-powered code editor built on vscode
+    lapce # lightning-fast and Powerful Code Editor written in Rust
     lmstudio # LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models
   ];
   programs.claude-code.enable = true;
