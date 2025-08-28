99a37c2f6c8c8d13b019aaf87edaa1b088a027b8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-28 22:19:33 +0300

    [nvim] otter: faster startup

--- nvim/.config/nvim/lua/plugins/dev/otter.lua
+++ nvim/.config/nvim/lua/plugins/dev/otter.lua
@@ -1,7 +1,9 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ jmbuhr/otter.nvim                                                            │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'jmbuhr/otter.nvim', -- provides lsp features and a code completion source for code embedded in other documents
-  dependencies = {'nvim-treesitter/nvim-treesitter'},
+return {
+  "jmbuhr/otter.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ft = { "markdown", "org", "norg", "rmd", "quarto" },
   opts = {},
 }
