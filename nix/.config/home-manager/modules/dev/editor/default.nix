11c8307263b5bc1086b068fb837e652332434f5e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 01:31:16 +0300

    [editor] try windsurf

--- nix/.config/home-manager/modules/dev/editor/default.nix
+++ nix/.config/home-manager/modules/dev/editor/default.nix
@@ -3,6 +3,7 @@
     code-cursor-fhs # AI-powered code editor built on vscode
     lapce # lightning-fast and Powerful Code Editor written in Rust
     lmstudio # LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models
+    windsurf # Agentic IDE powered by AI Flow paradigm
   ];
   programs.claude-code.enable = true;
   imports = [
