795f2ab2743da435f8976f494bef556cdc038871
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-28 22:18:18 +0300

    [nvim] harpoon faster startup

--- nvim/.config/nvim/lua/plugins/navigation/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/navigation/harpoon.lua
@@ -2,25 +2,20 @@
 -- │ █▓▒░ ThePrimeagen/harpoon                                                         │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config=function()
-        local harpoon = require("harpoon")
-        -- REQUIRED
-        harpoon:setup()
-        -- REQUIRED
-        
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-        
-        -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-    end
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  main = "harpoon",
+  opts = {},
+  keys = {
+    { "<leader>a", function() require("harpoon"):list():add() end, desc = "Harpoon add" },
+    { "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon menu" },
+    { "<C-h>", function() require("harpoon"):list():select(1) end, desc = "Harpoon 1" },
+    { "<C-t>", function() require("harpoon"):list():select(2) end, desc = "Harpoon 2" },
+    { "<C-n>", function() require("harpoon"):list():select(3) end, desc = "Harpoon 3" },
+    { "<C-s>", function() require("harpoon"):list():select(4) end, desc = "Harpoon 4" },
+    { "<C-S-P>", function() require("harpoon"):list():prev() end, desc = "Harpoon prev" },
+    { "<C-S-N>", function() require("harpoon"):list():next() end, desc = "Harpoon next" },
+  },
+  event = "VeryLazy",
 }
