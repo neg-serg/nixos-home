15ab1fdbd8a8514aac41d6b158a19a76691cb404
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 19:57:13 +0300

    [ocenaudio] enable again

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -3,7 +3,7 @@
     bespokesynth # nice modular synth
     dexed # nice yamaha dx7-like fm synth
     noisetorch # virtual microphone to suppress the noise
-    # ocenaudio # good audio editor
+    ocenaudio # good audio editor
     reaper # А вот в рипере!
     renoise # modern tracker
     rnnoise # neural network noise reduction
