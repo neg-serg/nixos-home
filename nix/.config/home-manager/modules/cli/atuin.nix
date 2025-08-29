2b37d8784324d5c41cd8e4f18418dfa52600c238
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 04:46:35 +0300

    [cli] atuin: better settings

--- nix/.config/home-manager/modules/cli/atuin.nix
+++ nix/.config/home-manager/modules/cli/atuin.nix
@@ -4,16 +4,19 @@
     # I *despise* this hijacking of the up key, even though I use Ctrl-p
     flags = ["--disable-up-arrow"];
     settings = {
+      daemon.enabled = false;
       enter_accept = false; # I like being able to edit my commands
-      inline_height = 15;
-      invert = true;
+      inline_height = 25;
+      invert = false;
       keys.scroll_exits = false;
       prefers_reduced_motion = true;
+      preview.max_preview_height = 0;
+      preview.strategy = "fixed";
       search_mode = "skim"; # Get closer to fzf's fuzzy search
       show_help = false;
-      show_preview = true; # Show long command lines at the bottom
+      show_preview = false; # Show long command lines at the bottom
       show_tabs = false;
-      style = "compact"; # I don't care for the fancy display
+      smart_sort = true;
       update_check = false; # The package is managed by Nix
     };
   };
