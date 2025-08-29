be9d5949ff15afe11925a78d8cb3576b0cd7713f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 18:54:37 +0300

    [radicle] come back

--- nix/.config/home-manager/modules/dev/pkgs/default.nix
+++ nix/.config/home-manager/modules/dev/pkgs/default.nix
@@ -6,8 +6,8 @@
     flawfinder # examine c, cpp code for security flaws
     mypy # optional static-typing for python
     nodejs_24 # for npm support
-    # radicle-explorer # web frontend for radicle
-    # radicle-node # radicle server
+    radicle-explorer # web frontend for radicle
+    radicle-node # radicle server
     scc # parallel cloc
     shfmt # shell formatting
     stylua # lua styler
