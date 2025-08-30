d14752c60481c86e4bcebce75aacedab2687f84a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-30 20:35:09 +0300

    [flake] try better caching

--- nix/.config/home-manager/flake.nix
+++ nix/.config/home-manager/flake.nix
@@ -7,11 +7,30 @@
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://cache.garnix.io"
+      # Additional popular caches
+      "https://numtide.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+      "https://hercules-ci.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://nix-gaming.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      # nix-community
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # Hyprland
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      # Garnix
       "cache.garnix.io-1:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      # numtide
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      # nixpkgs-wayland
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      # hercules-ci
+      "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
+      # cuda-maintainers
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      # nix-gaming
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
   inputs = {
