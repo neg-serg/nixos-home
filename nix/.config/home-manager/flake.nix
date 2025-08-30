2988d186f74fbc0cdb783ab5631d0b88691e9528
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-30 23:53:55 +0300

    [flake] pin everything to nixpkgs

--- nix/.config/home-manager/flake.nix
+++ nix/.config/home-manager/flake.nix
@@ -34,23 +34,23 @@
     ];
   };
   inputs = {
-    bzmenu = { url = "github:e-tho/bzmenu"; };
-    chaotic = { url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; };
-    crane = { url = "github:ipetkov/crane"; };
+    bzmenu = { url = "github:e-tho/bzmenu"; inputs.nixpkgs.follows = "nixpkgs"; };
+    chaotic = { url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; inputs.nixpkgs.follows = "nixpkgs"; };
+    crane = { url = "github:ipetkov/crane"; inputs.nixpkgs.follows = "nixpkgs"; };
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     # Pin hy3 to a commit compatible with Hyprland v0.50.0 (GitHub archive available)
     hy3 = { url = "github:outfoxxed/hy3?rev=d61a2eb9b9f22c6e46edad3e8f5fbd3578961b11"; inputs.hyprland.follows = "hyprland"; };
     # Pin Hyprland to a stable release to reduce API churn with hy3
-    hyprland = { url = "github:hyprwm/Hyprland?ref=v0.50.0"; };
+    hyprland = { url = "github:hyprwm/Hyprland?ref=v0.50.0"; inputs.nixpkgs.follows = "nixpkgs"; };
     iosevka-neg = { url = "git+ssh://git@github.com/neg-serg/iosevka-neg"; inputs.nixpkgs.follows = "nixpkgs"; };
-    iwmenu = { url = "github:e-tho/iwmenu"; };
+    iwmenu = { url = "github:e-tho/iwmenu"; inputs.nixpkgs.follows = "nixpkgs"; };
     nixpkgs = { url = "github:nixos/nixpkgs"; };
     nvfetcher = { url = "github:berberman/nvfetcher"; inputs.nixpkgs.follows = "nixpkgs"; };
     quickshell = { url = "git+https://git.outfoxxed.me/outfoxxed/quickshell"; inputs.nixpkgs.follows = "nixpkgs"; };
     rsmetrx = { url = "github:neg-serg/rsmetrx"; inputs.nixpkgs.follows = "nixpkgs"; };
-    sops-nix = { url = "github:Mic92/sops-nix"; };
+    sops-nix = { url = "github:Mic92/sops-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
     stylix = { url = "github:danth/stylix"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nur = { url = "github:nix-community/NUR"; };
+    nur = { url = "github:nix-community/NUR"; inputs.nixpkgs.follows = "nixpkgs"; };
     yandex-browser = { url = "github:miuirussia/yandex-browser.nix"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
