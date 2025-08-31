5e414dc13bd2aa51d77c93c75ccbef948cd5face
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 15:39:48 +0300

    [creation] remove renoise

--- nix/.config/home-manager/modules/media/audio/creation.nix
+++ nix/.config/home-manager/modules/media/audio/creation.nix
@@ -5,7 +5,6 @@
     noisetorch # virtual microphone to suppress the noise
     ocenaudio # good audio editor
     reaper # А вот в рипере!
-    renoise # modern tracker
     rnnoise # neural network noise reduction
     stochas # nice free sequencer
     vcv-rack # powerful soft modular synth
