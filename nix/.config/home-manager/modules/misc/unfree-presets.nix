28c6f2d864d907e41ea898b77a25fe94787e768d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 01:44:25 +0300

    [user/fun] revert to retroarchFull; allow required libretro cores
    Switch back to retroarchFull and extend unfree allowlist with needed cores: mame2000, mame2003, mame2003-plus, mame2010, mame2015, opera, picodrive, snes9x, snes9x2002, snes9x2005, snes9x2005-plus, snes9x2010.

--- nix/.config/home-manager/modules/misc/unfree-presets.nix
+++ nix/.config/home-manager/modules/misc/unfree-presets.nix
@@ -15,6 +15,18 @@
     "libretro-fbneo" # libretro arcade core (FBNeo)
     "libretro-fmsx" # libretro core (MSX)
     "libretro-genesis-plus-gx" # libretro core (Sega Genesis/Master System)
+    "libretro-mame2000" # libretro core (MAME 2000)
+    "libretro-mame2003" # libretro core (MAME 2003)
+    "libretro-mame2003-plus" # libretro core (MAME 2003 Plus)
+    "libretro-mame2010" # libretro core (MAME 2010)
+    "libretro-mame2015" # libretro core (MAME 2015)
+    "libretro-opera" # libretro core (3DO / Opera)
+    "libretro-picodrive" # libretro core (Sega PicoDrive)
+    "libretro-snes9x" # libretro core (SNES)
+    "libretro-snes9x2002" # libretro core (SNES 2002)
+    "libretro-snes9x2005" # libretro core (SNES 2005)
+    "libretro-snes9x2005-plus" # libretro core (SNES 2005 Plus)
+    "libretro-snes9x2010" # libretro core (SNES 2010)
     "yandex-browser-stable" # Chromium-based Yandex browser
     "lmstudio" # desktop app for local/open LLMs
     "code-cursor-fhs" # Cursor packaged via FHS env
--- nix/.config/home-manager/modules/user/fun/emulators.nix
+++ nix/.config/home-manager/modules/user/fun/emulators.nix
@@ -2,6 +2,6 @@
   home.packages = with pkgs; [
     pcem # emulator for ibm pc and clones
     pcsx2 # ps2 emulator
-    retroarch # multiplatform emulator frontend (free cores only)
+    retroarchFull # multiplatform emulator frontend with full cores set
   ];
 }
