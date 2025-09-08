a1a8b9296761db17abe8fd418ceac63d36234681
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 01:12:10 +0300

    [nixpkgs/unfree] add presets file (desktop, headless); options: features.allowUnfree.{preset,extra,allowed}; derive allowed from preset+extra

---
+++ nix/.config/home-manager/modules/misc/unfree-presets.nix
@@ -0,0 +1,12 @@
+{
+  # Desktop-oriented unfree packages
+  desktop = [
+    "yandex-browser-stable"
+    "lmstudio"
+    "code-cursor-fhs"
+  ];
+
+  # Headless/server preset: no unfree packages allowed
+  headless = [ ];
+}
+
