7ca68decc17b6a12d5cf3f41afc5547a3a8e8fe7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 08:22:40 +0300

    [nvim] matchup: drop default g:matchup_text_obj_enabled=1

--- nvim/.config/nvim/lua/plugins/edit/vim-matchup.lua
+++ nvim/.config/nvim/lua/plugins/edit/vim-matchup.lua
@@ -5,6 +5,6 @@ return {'andymass/vim-matchup', -- generic matcher
     config=function()
         vim.g.matchup_matchparen_enabled=0
         vim.g.matchup_motion_enabled=0
-        vim.g.matchup_text_obj_enabled=1
+        -- text objects are enabled by default; no need to set
     end,
     event={'BufRead','BufNewFile'}}
