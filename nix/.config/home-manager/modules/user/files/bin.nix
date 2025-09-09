4783c35188ad6e73bd5174ab90c54531065e2c38
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 12:45:04 +0300

    [user/files] bin.nix: use lib.neg.mkDotfilesSymlink

--- nix/.config/home-manager/modules/user/files/bin.nix
+++ nix/.config/home-manager/modules/user/files/bin.nix
@@ -2,13 +2,6 @@
   lib,
   config,
   ...
-}: let
-  l = config.lib.file.mkOutOfStoreSymlink;
-  dots = "${config.home.homeDirectory}/.dotfiles";
-  mkSymlink = path: recursive: {
-    source = l "${dots}/${path}";
-    inherit recursive;
-  };
-in {
-  home.file."bin" = mkSymlink "bin" false;
+}: {
+  home.file."bin" = config.lib.neg.mkDotfilesSymlink "bin" false;
 }
