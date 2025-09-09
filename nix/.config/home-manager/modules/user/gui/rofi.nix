b8972a2fe2a73d72f98b155fca941a49b80dac26
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 11:00:46 +0300

    [user/gui/rofi] activation: remove stale ~/.config/rofi symlink before linking (prevent conflicts)

--- nix/.config/home-manager/modules/user/gui/rofi.nix
+++ nix/.config/home-manager/modules/user/gui/rofi.nix
@@ -10,6 +10,16 @@ let
   repoRofiConf = "${config.home.homeDirectory}/.dotfiles/nix/.config/home-manager/modules/user/gui/rofi/conf";
 in
   mkIf config.features.gui.enable {
+    # Remove stale ~/.config/rofi symlink from older generations before linking
+    home.activation.fixRofiConfigDir =
+      lib.hm.dag.entryBefore [ "linkGeneration" ] ''
+        set -eu
+        RDIR="${config.xdg.configHome}/rofi"
+        if [ -L "$RDIR" ]; then
+          rm -f "$RDIR"
+        fi
+      '';
+
     home.packages = with pkgs; [
       rofi-pass-wayland # pass interface for rofi-wayland
       (rofi-wayland.override {
