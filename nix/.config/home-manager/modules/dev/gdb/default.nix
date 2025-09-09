f5d7cbe4427778af702d9d457e653ecd18f79d92
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 08:48:01 +0300

    [dev/gdb] gdbinit: embed file in module; read via text

--- nix/.config/home-manager/modules/dev/gdb/default.nix
+++ nix/.config/home-manager/modules/dev/gdb/default.nix
@@ -3,6 +3,6 @@
   # Install gdb and manage its config under XDG
   home.packages = [ pkgs.gdb ];
 
-  # Write the gdbinit contents directly (no path/source symlink)
-  xdg.configFile."gdb/gdbinit".text = builtins.readFile ../../../../../../gdb/.config/gdb/gdbinit;
+  # Write the gdbinit contents directly from a file embedded in this module
+  xdg.configFile."gdb/gdbinit".text = builtins.readFile ./gdbinit;
 }
