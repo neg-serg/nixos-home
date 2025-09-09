ed3e96a2aa9cce7ece05f99926ee8dfccdb3c474
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 11:42:15 +0300

    [dotfiles] cleanup

--- nix/.config/home-manager/modules/user/dotfiles/default.nix
+++ nix/.config/home-manager/modules/user/dotfiles/default.nix
@@ -10,14 +10,9 @@
     inherit recursive;
   };
 in {
-  xdg = {
-    # moved to modules/misc/fun-art.nix (guarded by features.fun.enable)
-  };
-
   home.file = {
     "bin" = mkSymlink "bin" false;
     ".ugrep" = mkSymlink "shell/.ugrep" true;
-    ".zshenv" = mkSymlink "shell/.zshenv" true;
     "${config.xdg.configHome}/nixpkgs/config.nix".text = let
       # Render current allowlist into a Nix list literal in the written config
       allowedList = lib.concatStringsSep " " (map (s: "\"${s}\"") config.features.allowUnfree.allowed);
