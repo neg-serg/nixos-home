3ed0e0d80fdd21dc2b5e5979d54daa2fb6a41cc9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-09 06:58:08 +0300

    [user/web] default-browser-lib: centralize default browser; fix defaults.nix nesting
    - Compute default browser in default-browser-lib and import it
    - Set lib.neg.web.defaultBrowser via mkIf; avoid nested 'config' misuse
    - Guard sessionVariables and xdg.mimeApps with features.web.enable

--- nix/.config/home-manager/modules/user/web/defaults.nix
+++ nix/.config/home-manager/modules/user/web/defaults.nix
@@ -8,40 +8,7 @@
 }:
 with lib; let
   cfg = config.features.web;
-
-  # Map the selected default browser to common fields used elsewhere
-  browser =
-    if (cfg.default or "floorp") == "yandex" && yandexBrowser != null then {
-      name = "yandex";
-      pkg = yandexBrowser.yandex-browser-stable;
-      bin = "${yandexBrowser.yandex-browser-stable}/bin/yandex-browser-stable";
-      desktop = "yandex-browser.desktop";
-      newTabArg = "--new-tab";
-    } else if (cfg.default or "floorp") == "firefox" then {
-      name = "firefox";
-      pkg = pkgs.firefox;
-      bin = "${pkgs.firefox}/bin/firefox";
-      desktop = "firefox.desktop";
-      newTabArg = "-new-tab";
-    } else if (cfg.default or "floorp") == "librewolf" then {
-      name = "librewolf";
-      pkg = pkgs.librewolf;
-      bin = "${pkgs.librewolf}/bin/librewolf";
-      desktop = "librewolf.desktop";
-      newTabArg = "-new-tab";
-    } else if (cfg.default or "floorp") == "nyxt" then {
-      name = "nyxt";
-      pkg = if nyxt4 != null then nyxt4 else pkgs.nyxt;
-      bin = "${(if nyxt4 != null then nyxt4 else pkgs.nyxt)}/bin/nyxt";
-      desktop = "nyxt.desktop";
-      newTabArg = "";
-    } else {
-      name = "floorp";
-      pkg = pkgs.floorp;
-      bin = "${pkgs.floorp}/bin/floorp";
-      desktop = "floorp.desktop";
-      newTabArg = "-new-tab";
-    };
+  browser = import ./default-browser-lib.nix { inherit lib pkgs config yandexBrowser nyxt4; };
 in {
   # Choose the default browser for system-wide handlers and $BROWSER
   options.features.web.default = mkOption {
@@ -50,28 +17,24 @@ in {
     description = "Default browser used for XDG handlers, $BROWSER, and integrations.";
   };
 
-  config = mkIf cfg.enable {
-    # Expose derived default browser under lib.neg for reuse
-    config.lib.neg.web = {
-      defaultBrowser = browser;
-    };
+  # Expose derived default browser under lib.neg for reuse
+  config.lib.neg.web = mkIf cfg.enable { defaultBrowser = browser; };
 
-    # Provide common env defaults (can be overridden elsewhere if needed)
-    home.sessionVariables = {
-      BROWSER = browser.bin;
-      DEFAULT_BROWSER = browser.bin;
-    };
+  # Provide common env defaults (can be overridden elsewhere if needed)
+  config.home.sessionVariables = mkIf cfg.enable {
+    BROWSER = browser.bin;
+    DEFAULT_BROWSER = browser.bin;
+  };
 
-    # Provide minimal sane defaults for common browser handlers
-    xdg.mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "text/html" = browser.desktop;
-        "x-scheme-handler/http" = browser.desktop;
-        "x-scheme-handler/https" = browser.desktop;
-        "x-scheme-handler/about" = browser.desktop;
-        "x-scheme-handler/unknown" = browser.desktop;
-      };
+  # Provide minimal sane defaults for common browser handlers
+  config.xdg.mimeApps = mkIf cfg.enable {
+    enable = true;
+    defaultApplications = {
+      "text/html" = browser.desktop;
+      "x-scheme-handler/http" = browser.desktop;
+      "x-scheme-handler/https" = browser.desktop;
+      "x-scheme-handler/about" = browser.desktop;
+      "x-scheme-handler/unknown" = browser.desktop;
     };
   };
 }
