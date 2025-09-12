87b11daca1cfe2d54c4595e853011795859a1428
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-12 17:30:15 +0300

    [main/home-manager] expose repo under ~/.config/home-manager via xdg helper; drop guard

--- nix/.config/home-manager/modules/main/home-manager/default.nix
+++ nix/.config/home-manager/modules/main/home-manager/default.nix
@@ -2,16 +2,14 @@
   lib,
   config,
   ...
-}: {
-  programs = {
-    home-manager.enable = true; # Let Home Manager install and manage itself.
-  };
-
-  # Remove stale ~/.config/home-manager symlink from older generations before linking
-  home.activation.fixHomeManagerConfigDir =
-    config.lib.neg.mkRemoveIfSymlink "${config.xdg.configHome}/home-manager";
-
-  # Make the repo available at ~/.config/home-manager for convenience
-  xdg.configFile."home-manager" =
-    config.lib.neg.mkDotfilesSymlink "nix/.config/home-manager" true;
-}
+}: let
+  xdg = import ../../lib/xdg-helpers.nix { inherit lib; };
+in lib.mkMerge [
+  {
+    programs = {
+      home-manager.enable = true; # Let Home Manager install and manage itself.
+    };
+  }
+  # Make the repo available at ~/.config/home-manager via helper
+  (xdg.mkXdgSource "home-manager" (config.lib.neg.mkDotfilesSymlink "nix/.config/home-manager" true))
+]
