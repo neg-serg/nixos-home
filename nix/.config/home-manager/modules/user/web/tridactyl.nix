d76a91c2eae2ef4a86cc218693a3efdcf61268ac
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-12 17:30:05 +0300

    [web/tridactyl] switch to xdg.mkXdgSource; remove stale-symlink guard

--- nix/.config/home-manager/modules/user/web/tridactyl.nix
+++ nix/.config/home-manager/modules/user/web/tridactyl.nix
@@ -4,12 +4,7 @@
   ...
 }:
 with lib;
-  mkIf config.features.web.enable {
-    # Remove stale ~/.config/tridactyl symlink from older generations before linking
-    home.activation.fixTridactylConfigDir =
-      config.lib.neg.mkRemoveIfSymlink "${config.xdg.configHome}/tridactyl";
-
-    # Live-editable config: out-of-store symlink to repo copy
-    xdg.configFile."tridactyl" =
-      config.lib.neg.mkDotfilesSymlink "misc/.config/tridactyl" true;
-  }
+  mkIf config.features.web.enable (let xdg = import ../../lib/xdg-helpers.nix { inherit lib; }; in lib.mkMerge [
+    # Live-editable config via helper (guards parent dir and target)
+    (xdg.mkXdgSource "tridactyl" (config.lib.neg.mkDotfilesSymlink "misc/.config/tridactyl" true))
+  ])
