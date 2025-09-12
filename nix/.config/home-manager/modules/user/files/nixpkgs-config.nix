c31f445a2edc45ab00d950e3b5eee069581006c8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-12 17:29:43 +0300

    [user/files] write nixpkgs config via xdg.mkXdgText; remove manual guards

--- nix/.config/home-manager/modules/user/files/nixpkgs-config.nix
+++ nix/.config/home-manager/modules/user/files/nixpkgs-config.nix
@@ -2,17 +2,12 @@
   lib,
   config,
   ...
-}: {
-  # Guard: ensure ~/.config/nixpkgs is a real directory and avoid writing through a symlinked config.nix
-  home.activation.fixNixpkgsDir =
-    config.lib.neg.mkEnsureRealDir "${config.xdg.configHome}/nixpkgs";
-  home.activation.fixNixpkgsConfigSymlink =
-    config.lib.neg.mkRemoveIfSymlink "${config.xdg.configHome}/nixpkgs/config.nix";
-
-  home.file."${config.xdg.configHome}/nixpkgs/config.nix".text = let
-    # Render current allowlist into a Nix list literal in the written config
-    allowedList = lib.concatStringsSep " " (map (s: "\"${s}\"") config.features.allowUnfree.allowed);
-  in ''
+}: let
+  xdg = import ../../lib/xdg-helpers.nix { inherit lib; };
+  # Render current allowlist into a Nix list literal in the written config
+  allowedList = lib.concatStringsSep " " (map (s: "\"${s}\"") config.features.allowUnfree.allowed);
+in
+  xdg.mkXdgText "nixpkgs/config.nix" ''
     {
       # Only allow specific unfree packages by name (synced with Home Manager)
       allowUnfreePredicate = pkg: let
@@ -20,5 +15,4 @@
         allowed = [ ${allowedList} ];
       in builtins.elem name allowed;
     }
-  '';
-}
+  ''
