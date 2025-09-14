d536145a74b440b8b9ffceeb0b95ebaec2b22b2a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-14 12:12:34 +0300

    [torrent][transmission] Ensure resume/torrents/blocklists dirs exist after activation (preserve symlink, avoid missing-dir errors)

--- nix/.config/home-manager/modules/user/torrent/default.nix
+++ nix/.config/home-manager/modules/user/torrent/default.nix
@@ -7,6 +7,15 @@
 with {
   transmission = pkgs.transmission_4;
 }; {
+  # Ensure runtime subdirectories exist even if the config dir is a symlink
+  # to an external location. This avoids "resume: No such file or directory"
+  # on first start after activation.
+  home.activation.ensureTransmissionDirs =
+    config.lib.neg.mkEnsureDirsAfterWrite [
+      "${config.xdg.configHome}/transmission-daemon/resume"
+      "${config.xdg.configHome}/transmission-daemon/torrents"
+      "${config.xdg.configHome}/transmission-daemon/blocklists"
+    ];
   home.packages = with pkgs; config.lib.neg.pkgsList [
     bitmagnet # dht crawler
     pkgs.neg.bt_migrate # torrent migrator
