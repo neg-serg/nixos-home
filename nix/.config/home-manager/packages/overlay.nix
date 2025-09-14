97ab0c64562c62d75b5743389c1483e85aed9067
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-14 10:43:08 +0300

    [pkgs][overlay] Merge top-level overrides from tools/media/dev overlays (enable python3Packages.ncclient override)

--- nix/.config/home-manager/packages/overlay.nix
+++ nix/.config/home-manager/packages/overlay.nix
@@ -3,6 +3,9 @@ _final: prev: let
   tools = importOv ./overlays/tools.nix;
   media = importOv ./overlays/media.nix;
   dev = importOv ./overlays/dev.nix;
-in {
-  neg = (tools.neg or {}) // (media.neg or {}) // (dev.neg or {});
-}
+in
+  # Merge all top-level overrides from overlays (tools/media/dev), and also
+  # provide a combined pkgs.neg namespace aggregating their custom packages.
+  (tools // media // dev) // {
+    neg = (tools.neg or {}) // (media.neg or {}) // (dev.neg or {});
+  }
