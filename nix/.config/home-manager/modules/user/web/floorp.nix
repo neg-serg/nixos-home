46cd228c2c01de4e0f7b36bec76ee5a858945838
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-14 09:52:52 +0300

    [web][firefox][librewolf][floorp] Unify module headers and trim comments
    - Keep consistent param sets and import of mozilla-common-lib
    - Remove redundant comments; align profile fields ordering
    - Pass optional fa into floorp for parity

--- nix/.config/home-manager/modules/user/web/floorp.nix
+++ nix/.config/home-manager/modules/user/web/floorp.nix
@@ -2,33 +2,24 @@
   config,
   pkgs,
   lib,
+  fa ? null,
   ...
 }:
 lib.mkIf (config.features.web.enable && config.features.web.floorp.enable) (let
-  common = import ./mozilla-common-lib.nix {inherit lib pkgs config;};
+  common = import ./mozilla-common-lib.nix {inherit lib pkgs config fa;};
 in {
   programs.floorp = {
     enable = true;
     nativeMessagingHosts = common.nativeMessagingHosts;
-
     profiles.${common.profileId} = {
       isDefault = true;
-      # Declarative extensions (NUR where available)
       extensions = {packages = common.addons.common;};
-
-      # about:config prefs
       settings = common.settings;
-
-      # Optional toggles
       extraConfig = common.extraConfig;
-
       userChrome = common.userChrome;
     };
-
-    # Policies: force-install addons missing in NUR (AMO latest.xpi)
     policies = common.policies;
   };
-
   home.sessionVariables = {
     MOZ_DBUS_REMOTE = "1";
     MOZ_ENABLE_WAYLAND = "1";
