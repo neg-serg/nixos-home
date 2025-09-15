4534cf6815f8701dc432c114fa439a6200a25b09
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-15 23:53:24 +0300

    [web] Expose lib.neg.web.{defaultBrowser,browsers}; add robust env+MIME fallbacks; fix syntax

--- nix/.config/home-manager/modules/user/web/defaults.nix
+++ nix/.config/home-manager/modules/user/web/defaults.nix
@@ -8,7 +8,8 @@
 }:
 with lib; let
   cfg = config.features.web;
-  browser = import ./default-browser-lib.nix {inherit lib pkgs config yandexBrowser nyxt4;};
+  browsers = import ./browsers-table.nix { inherit lib pkgs yandexBrowser nyxt4; };
+  browser = let key = cfg.default or "floorp"; in lib.attrByPath [key] browsers browsers.floorp;
 in {
   # Choose the default browser for system-wide handlers and $BROWSER
   options.features.web.default = mkOption {
@@ -18,23 +19,29 @@ in {
   };
 
   # Expose derived default browser under lib.neg for reuse
-  config.lib.neg.web = mkIf cfg.enable {defaultBrowser = browser;};
+  config.lib.neg.web = mkIf cfg.enable {
+    defaultBrowser = browser;
+    browsers = browsers;
+  };
 
   # Provide common env defaults (can be overridden elsewhere if needed)
-  config.home.sessionVariables = mkIf cfg.enable {
-    BROWSER = browser.bin;
-    DEFAULT_BROWSER = browser.bin;
-  };
+  config.home.sessionVariables = mkIf cfg.enable (
+    let db = browser; in {
+      BROWSER = db.bin or "${pkgs.xdg-utils}/bin/xdg-open";
+      DEFAULT_BROWSER = db.bin or "${pkgs.xdg-utils}/bin/xdg-open";
+    }
+  );
 
   # Provide minimal sane defaults for common browser handlers
-  config.xdg.mimeApps = mkIf cfg.enable {
+  config.xdg.mimeApps = mkIf cfg.enable (
+    let db = browser; in {
     enable = true;
     defaultApplications = {
-      "text/html" = browser.desktop;
-      "x-scheme-handler/http" = browser.desktop;
-      "x-scheme-handler/https" = browser.desktop;
-      "x-scheme-handler/about" = browser.desktop;
-      "x-scheme-handler/unknown" = browser.desktop;
+      "text/html" = db.desktop or "floorp.desktop";
+      "x-scheme-handler/http" = db.desktop or "floorp.desktop";
+      "x-scheme-handler/https" = db.desktop or "floorp.desktop";
+      "x-scheme-handler/about" = db.desktop or "floorp.desktop";
+      "x-scheme-handler/unknown" = db.desktop or "floorp.desktop";
     };
-  };
+  });
 }
