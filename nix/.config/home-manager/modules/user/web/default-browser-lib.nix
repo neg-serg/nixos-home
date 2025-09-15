8a8f5f4b8fe16a3875533e02fa7c1aae4698b907
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-15 23:53:18 +0300

    [web] Use browsers table in default-browser helper; switch to key lookup with safe fallback

--- nix/.config/home-manager/modules/user/web/default-browser-lib.nix
+++ nix/.config/home-manager/modules/user/web/default-browser-lib.nix
@@ -8,50 +8,7 @@
 }:
 with lib; let
   cfg = config.features.web;
+  key = cfg.default or "floorp";
+  browsers = import ./browsers-table.nix { inherit lib pkgs yandexBrowser nyxt4; };
 in
-  if (cfg.default or "floorp") == "yandex" && yandexBrowser != null
-  then {
-    name = "yandex";
-    pkg = yandexBrowser.yandex-browser-stable;
-    bin = "${yandexBrowser.yandex-browser-stable}/bin/yandex-browser-stable";
-    desktop = "yandex-browser.desktop";
-    newTabArg = "--new-tab";
-  }
-  else if (cfg.default or "floorp") == "firefox"
-  then {
-    name = "firefox";
-    pkg = pkgs.firefox;
-    bin = "${pkgs.firefox}/bin/firefox";
-    desktop = "firefox.desktop";
-    newTabArg = "-new-tab";
-  }
-  else if (cfg.default or "floorp") == "librewolf"
-  then {
-    name = "librewolf";
-    pkg = pkgs.librewolf;
-    bin = "${pkgs.librewolf}/bin/librewolf";
-    desktop = "librewolf.desktop";
-    newTabArg = "-new-tab";
-  }
-  else if (cfg.default or "floorp") == "nyxt"
-  then {
-    name = "nyxt";
-    pkg =
-      if nyxt4 != null
-      then nyxt4
-      else pkgs.nyxt;
-    bin = "${(
-      if nyxt4 != null
-      then nyxt4
-      else pkgs.nyxt
-    )}/bin/nyxt";
-    desktop = "nyxt.desktop";
-    newTabArg = "";
-  }
-  else {
-    name = "floorp";
-    pkg = pkgs.floorp-bin;
-    bin = "${pkgs.floorp-bin}/bin/floorp";
-    desktop = "floorp.desktop";
-    newTabArg = "-new-tab";
-  }
+  lib.attrByPath [key] browsers browsers.floorp
