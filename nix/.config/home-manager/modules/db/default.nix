9fe3fe89139c34b8e72433e1a5d68bce860b9666
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-16 17:55:24 +0300

    [db] add postgresql package (client/server tools)

--- nix/.config/home-manager/modules/db/default.nix
+++ nix/.config/home-manager/modules/db/default.nix
@@ -5,5 +5,6 @@
 }: {
   home.packages = with pkgs; config.lib.neg.pkgsList [
     sqlite # self-contained, serverless, zero-configuration, transactional SQL database engine
+    postgresql # PostgreSQL server and client tools (psql, initdb, etc.)
   ];
 }
