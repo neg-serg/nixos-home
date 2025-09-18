c928a52488bbe0123ccd861ee326ca607a7a994e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-18 13:42:04 +0300

    [fix][xdg] Remove lingering xdg module arg usage (rustmission)
    - Restore local import to avoid moduleChecks recursion

--- nix/.config/home-manager/modules/misc/rustmission.nix
+++ nix/.config/home-manager/modules/misc/rustmission.nix
@@ -1,5 +1,6 @@
-{ lib, config, xdg, ... }:
-lib.mkMerge [
+{ lib, config, ... }:
+let xdg = import ../lib/xdg-helpers.nix { inherit lib; };
+in lib.mkMerge [
   # Live-editable out-of-store symlink via helper (guards parent dir and target)
   (xdg.mkXdgSource "rustmission" {
     source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/nix/.config/home-manager/modules/misc/rustmission/conf";
