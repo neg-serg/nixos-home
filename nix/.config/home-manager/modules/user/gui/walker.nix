80fc1eaa62d6defec229d32baef1bb9605afaec8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-18 12:54:46 +0300

    [gui/walker] Restore full conf linking; remove cyclic symlink; set neg theme opaque
    - Link entire walker conf directory via XDG (as before)
    - Replace problematic themes symlink with real files
    - Make neg.css background near-opaque

--- nix/.config/home-manager/modules/user/gui/walker.nix
+++ nix/.config/home-manager/modules/user/gui/walker.nix
@@ -9,6 +9,9 @@ with lib;
     {
       home.packages = config.lib.neg.pkgsList [pkgs.walker];
     }
-    # Live-editable config via helper (guards parent dir and target)
-    (xdg.mkXdgSource "walker" (config.lib.neg.mkDotfilesSymlink "nix/.config/home-manager/modules/user/gui/walker/conf" true))
+    # Link entire conf directory (as before): config.toml + themes/*
+    (xdg.mkXdgSource "walker" {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/nix/.config/home-manager/modules/user/gui/walker/conf";
+      recursive = true;
+    })
   ])
