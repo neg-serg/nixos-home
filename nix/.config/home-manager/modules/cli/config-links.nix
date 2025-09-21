3c74f55987f2af396a4624e62578fe39a6c7b491
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-21 21:34:04 +0300

    [warnings] add soft migration notes for tmux, zsh, and kitty (XDG paths)

--- nix/.config/home-manager/modules/cli/config-links.nix
+++ nix/.config/home-manager/modules/cli/config-links.nix
@@ -5,4 +5,12 @@ lib.mkMerge [
   (xdg.mkXdgSource "f-sy-h" { source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/shell/.config/f-sy-h"; recursive = true; })
   (xdg.mkXdgText "inputrc" (builtins.readFile ./inputrc))
   (xdg.mkXdgSource "zsh" { source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/shell/.config/zsh"; recursive = true; })
+  # Soft migration note for Zsh: configuration is managed via $ZDOTDIR ($XDG_CONFIG_HOME/zsh)
+  # If legacy ~/.zshrc or ~/.zshenv still exist, remove them or make them source
+  # "$ZDOTDIR/zshrc" to avoid precedence issues.
+  {
+    warnings = [
+      "Zsh is configured via $ZDOTDIR ($XDG_CONFIG_HOME/zsh). If ~/.zshrc or ~/.zshenv still exist, remove or source $ZDOTDIR/zshrc."
+    ];
+  }
 ]
