f131842683fe178eaae8f6680558b796ba1f65a2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-21 17:41:22 +0300

    [media/playerctld] Fix ExecStart quoting in systemd user unit
    Use plain string in Nix interpolation for lib.getExe' name; remove unnecessary escapes. No functional change beyond fixing quoting.

--- nix/.config/home-manager/modules/media/playerctld.nix
+++ nix/.config/home-manager/modules/media/playerctld.nix
@@ -6,10 +6,9 @@ mkIf (config.features.media.audio.apps.enable or false) {
       Unit = { Description = "Keep track of media player activity"; };
       Service = {
         Type = "oneshot";
-        ExecStart = "${lib.getExe' pkgs.playerctl \"playerctld\"} daemon";
+        ExecStart = "${lib.getExe' pkgs.playerctl "playerctld"} daemon";
       };
     }
     (config.lib.neg.systemdUser.mkUnitFromPresets { presets = ["defaultWanted"]; })
   ];
 }
-
