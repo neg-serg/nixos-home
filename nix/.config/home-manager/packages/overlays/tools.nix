242021509125c16c9d0033328673fedb7486208b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-22 09:33:26 +0300

    [packages/tools] Add neg-pretty-printer (library + ppinfo CLI) and install via HM; remove legacy ~/bin/pretty_printer.py

--- nix/.config/home-manager/packages/overlays/tools.nix
+++ nix/.config/home-manager/packages/overlays/tools.nix
@@ -9,6 +9,10 @@ in {
     cxxmatrix = call ../cxxmatrix {};
     comma = call ../comma {};
 
+    # Pretty-printer library + CLI (ppinfo)
+    pretty_printer = call ../pretty-printer {};
+    "pretty-printer" = pretty_printer;
+
     # Rofi plugins / desktop helpers
     rofi_games = call ../rofi-games {};
     "rofi-games" = rofi_games;
