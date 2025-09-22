a7493f50d45cdf1655dfadaef16f46fd3001e0ed
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-22 09:22:05 +0300

    [web/floorp] switch to lib.mkMerge for session vars; remove obsolete bin/hypr-win-list

--- nix/.config/home-manager/modules/user/web/floorp.nix
+++ nix/.config/home-manager/modules/user/web/floorp.nix
@@ -7,13 +7,16 @@
 }:
 lib.mkIf (config.features.web.enable && config.features.web.floorp.enable) (let
   common = import ./mozilla-common-lib.nix {inherit lib pkgs config faProvider;};
-in (common.mkBrowser {
-  name = "floorp";
-  package = pkgs.floorp-bin;
-  profileId = common.profileId;
-}) // {
-  home.sessionVariables = {
-    MOZ_DBUS_REMOTE = "1";
-    MOZ_ENABLE_WAYLAND = "1";
-  };
-})
+in lib.mkMerge [
+  (common.mkBrowser {
+    name = "floorp";
+    package = pkgs.floorp-bin;
+    profileId = common.profileId;
+  })
+  {
+    home.sessionVariables = {
+      MOZ_DBUS_REMOTE = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+    };
+  }
+])
