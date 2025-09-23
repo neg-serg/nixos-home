84a679a7b9cd72c5df92af9b393bcc32d6b846cd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-23 22:21:35 +0300

    [rofi] Unify prompts to '❯>' across scripts (clip, screenshot, read_documents, pl, swayimg-actions, hypr win list, handlr); remove unused var; HM switch OK

--- nix/.config/home-manager/modules/user/local-bin/scripts/swayimg-actions.sh
+++ nix/.config/home-manager/modules/user/local-bin/scripts/swayimg-actions.sh
@@ -14,7 +14,7 @@ mkdir -p ${XDG_DATA_HOME:-$HOME/.local/share}/swayimg
 z="${XDG_DATA_HOME:-$HOME/.local/share}/swayimg/data"
 last_file="${XDG_DATA_HOME:-$HOME/.local/share}/swayimg/last"
 trash="${HOME}/trash/1st-level/pic"
-rofi_cmd='rofi -dmenu -sort -matching fuzzy -no-plugins -no-only-match -theme sxiv -custom'
+rofi_cmd='rofi -dmenu -sort -matching fuzzy -no-plugins -no-only-match -theme sxiv -custom -p "❯>"'
 pics_dir_default="$HOME/Pictures"
 pics_dir="${XDG_PICTURES_DIR:-$pics_dir_default}"
 
@@ -143,7 +143,7 @@ choose_dest() {
   fi
 
   printf '%s\n' "$entries" \
-    | sh -c "$rofi_cmd -p \"⟬$prompt⟭ ❯>\"" \
+    | sh -c "$rofi_cmd" \
     | sed "s:^~:$HOME:"
 }
 
