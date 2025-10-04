0f2d88955ac4b56a3634fb36fa2f1e0d63cca43c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-05 02:00:14 +0300

    [local-bin] avoid tkinter deps

--- nix/.config/home-manager/packages/overlays/dev.nix
+++ nix/.config/home-manager/packages/overlays/dev.nix
@@ -2,6 +2,10 @@ _final: prev: {
   # Targeted fix via reusable helper: tigervnc needs autoreconf.
   tigervnc = _final.neg.functions.withAutoreconf prev.tigervnc;
 
+  python311Packages = prev.python311Packages.overrideScope (self: super: {
+    matplotlib = super.matplotlib.override { enableTk = false; };
+  });
+
   # Reserved for development/toolchain overlays
   neg = {};
 }
