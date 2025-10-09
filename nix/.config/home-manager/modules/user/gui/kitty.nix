ae031512ab3e450692e70362806ed1257951894e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-09 08:55:27 +0300

    [gui/kitty] bundle search/extrakto kittens and panel helper

--- nix/.config/home-manager/modules/user/gui/kitty.nix
+++ nix/.config/home-manager/modules/user/gui/kitty.nix
@@ -1,5 +1,15 @@
-{ lib, config, xdg, ... }:
+{ lib, config, pkgs, xdg, ... }:
 lib.mkIf (config.features.gui.enable or false) (lib.mkMerge [
+  {
+    home.packages = config.lib.neg.pkgsList [
+      pkgs.gum
+      pkgs.peaclock
+      pkgs.cava
+      pkgs.brightnessctl
+      pkgs.wirelesstools
+    ];
+  }
+  (config.lib.neg.mkLocalBin "kitty-panel" (builtins.readFile ./kitty/panel))
   # Live-editable config via helper (guards parent dir and target)
   (xdg.mkXdgSource "kitty" {
     source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/nix/.config/home-manager/modules/user/gui/kitty/conf";
