339d8651508ab57d5f90b255ddcdc8f0dfb6950b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-09 08:54:22 +0300

    [packages] add awrit terminal browser

--- nix/.config/home-manager/modules/user/web/misc.nix
+++ nix/.config/home-manager/modules/user/web/misc.nix
@@ -6,6 +6,7 @@
 }:
 lib.mkIf (config.features.web.enable && config.features.web.tools.enable) {
   home.packages = config.lib.neg.pkgsList [
+    pkgs.neg.awrit # render web pages inside Kitty terminal
     pkgs.gallery-dl # download image galleries/collections
     pkgs.monolith # download all webpage stuff as one file
     pkgs.pipe-viewer # lightweight youtube client
