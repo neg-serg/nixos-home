6229cd82b7e63e83bae4101181527828c4d30908
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-17 01:19:01 +0300

    [secrets/keyring] Disable GNOME Keyring autostarts

---
+++ nix/.config/home-manager/modules/secrets/keyring.nix
@@ -0,0 +1,25 @@
+{ lib, xdg, ... }:
+with lib;
+let
+  disable = name:
+    xdg.mkXdgText "autostart/${name}" ''
+      [Desktop Entry]
+      Type=Application
+      Name=${name} (disabled)
+      Hidden=true
+    '';
+in
+{
+  # Disable GNOME Keyring autostart entries to avoid conflicts with
+  # gpg-agent and ssh-agent managed by Home Manager.
+  # This prevents systemd's XDG autostart generator from creating
+  # app-gnome-keyring-*-autostart user services that fail under Hyprland.
+  config = mkMerge [
+    (disable "gnome-keyring-ssh.desktop")
+    (disable "gnome-keyring-secrets.desktop")
+    (disable "gnome-keyring-pkcs11.desktop")
+    # Some distros ship a legacy daemon entry; disable just in case.
+    (disable "gnome-keyring-daemon.desktop")
+  ];
+}
+
