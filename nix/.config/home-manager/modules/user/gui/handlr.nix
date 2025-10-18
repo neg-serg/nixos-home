824cb16a6eddd6e6b2de10b3cf1101fd40e06b8a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-19 01:56:19 +0300

    [web/handlr] remove duplicate package install; keep XDG config only

--- nix/.config/home-manager/modules/user/gui/handlr.nix
+++ nix/.config/home-manager/modules/user/gui/handlr.nix
@@ -1,12 +1,6 @@
 { pkgs, lib, config, xdg, ... }:
-lib.mkIf (config.features.gui.enable or false) (lib.mkMerge [
-  {
-    home.packages = config.lib.neg.pkgsList [
-      pkgs.handlr # xdg-open alternative with fine-grained rules
-    ];
-  }
+lib.mkIf (config.features.gui.enable or false)
   (xdg.mkXdgConfigToml "handlr/handlr.toml" {
     enable_selector = false;
     selector = "rofi -dmenu -p 'Open With: ❯>'";
   })
-])
