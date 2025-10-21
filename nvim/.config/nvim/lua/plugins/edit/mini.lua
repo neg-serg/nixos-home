cf9f266e8912435b6e56560d75864213ff04aa2c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-21 16:44:32 +0300

    [nvim/edit] replace nvim-surround with mini.surround (preserve keymaps)

--- nvim/.config/nvim/lua/plugins/edit/mini.lua
+++ nvim/.config/nvim/lua/plugins/edit/mini.lua
@@ -27,5 +27,35 @@ return {
       sj.setup()
       Map('n', '<leader>a', function() sj.toggle() end, { desc = 'Split/Join toggle' })
     end
+
+    -- Surround (replaces kylechui/nvim-surround) with your preferred keymaps.
+    local ok_sur, surround = pcall(require, 'mini.surround')
+    if ok_sur then
+      surround.setup({
+        mappings = {
+          add = 'cs',       -- add surrounding (Normal/Visual)
+          delete = 'ds',    -- delete surrounding
+          replace = 'ys',   -- replace surrounding
+          find = '',        -- disable extras unless needed
+          find_left = '',
+          highlight = '',
+          suffix_last = 'l',
+          suffix_next = 'n',
+        },
+        respect_selection_type = true,
+      })
+      -- Visual add: keep 'S' (like previous plugin)
+      Map('x', 'S', function() require('mini.surround').add('visual') end, { desc = 'Surround (visual)' })
+      -- Visual line add: 'gS' – promote selection to linewise, then add
+      Map('x', 'gS', function()
+        vim.cmd('normal! gvV')
+        require('mini.surround').add('visual')
+      end, { desc = 'Surround (visual line)' })
+      -- Current line add (cSS equivalent): map to linewise add on current line
+      vim.keymap.set('n', 'cSS', 'cs_', { remap = true, desc = 'Surround current line' })
+      -- Convenience: keep your csw -> csiw shortcut
+      vim.keymap.set('n', 'csw', 'csiw', { remap = true })
+      vim.keymap.set('n', 'csW', 'csiW', { remap = true })
+    end
   end,
 }
