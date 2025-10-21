6a5a70c47c097590844d19afff23657da7ba543f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-21 16:40:54 +0300

    [nvim/text] remove vim-diagon

--- nvim/.config/nvim/lua/plugins/text/diagon.lua
+++
@@ -1,4 +0,0 @@
--- ┌───────────────────────────────────────────────────────────────────────────────────┐
--- │ █▓▒░ willchao612/vim-diagon                                                       │
--- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'willchao612/vim-diagon', cmd='Diagon'} -- creates diagrams from text. dependencies diagon from snap.
