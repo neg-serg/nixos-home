21fcb35313a769da5d2e81b4e525bc2ec08bb685
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-27 17:31:31 +0300

    [gui/hypr] permissions: avoid hy3 plugin path to prevent hyprland closure build

--- nix/.config/home-manager/modules/user/gui/hyprland/permissions.nix
+++ nix/.config/home-manager/modules/user/gui/hyprland/permissions.nix
@@ -1,15 +1,10 @@
-{ lib, config, pkgs, hy3, xdg, ... }:
-with lib; let
-  hy3Plugin = hy3.packages.${pkgs.system}.hy3;
-in mkIf config.features.gui.enable (
+{ lib, config, pkgs, xdg, ... }:
+with lib; mkIf config.features.gui.enable (
   xdg.mkXdgText "hypr/permissions.conf" ''
     ecosystem {
       enforce_permissions = 1
     }
-    permission = ${hy3Plugin}/lib/libhy3.so, plugin, allow
     permission = ${lib.getExe pkgs.grim}, screencopy, allow
     permission = ${lib.getExe pkgs.hyprlock}, screencopy, allow
-    plugin = ${hy3Plugin}/lib/libhy3.so
   ''
 )
-
