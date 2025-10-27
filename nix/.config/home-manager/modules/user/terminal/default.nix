84885fc5209b5b75f5711feb4153d5197b877917
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-27 21:37:46 +0300

    [cli/terminal] enable programs.asciinema

--- nix/.config/home-manager/modules/user/terminal/default.nix
+++ nix/.config/home-manager/modules/user/terminal/default.nix
@@ -3,11 +3,11 @@
   config,
   ...
 }: {
-  # Prefer Home Manager module for asciinema (installs pkg and manages XDG config)
   programs.asciinema.enable = true;
 
   home.packages = config.lib.neg.pkgsList [
     pkgs.asciinema-agg # asciinema files to gif
+    pkgs.asciinema # record terminal
     pkgs.chafa # terminal graphics
     pkgs.kitty # fastest terminal emulator so far
     pkgs.kitty-img # print images inline in kitty
