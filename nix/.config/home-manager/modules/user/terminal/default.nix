e9170641b837d9616a7d10fe48632a934c854916
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-27 21:37:55 +0300

    [cli/terminal] drop direct asciinema package

--- nix/.config/home-manager/modules/user/terminal/default.nix
+++ nix/.config/home-manager/modules/user/terminal/default.nix
@@ -7,7 +7,6 @@
 
   home.packages = config.lib.neg.pkgsList [
     pkgs.asciinema-agg # asciinema files to gif
-    pkgs.asciinema # record terminal
     pkgs.chafa # terminal graphics
     pkgs.kitty # fastest terminal emulator so far
     pkgs.kitty-img # print images inline in kitty
