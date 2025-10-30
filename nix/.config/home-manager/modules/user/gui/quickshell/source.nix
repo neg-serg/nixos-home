7b1af9c95a934dc9df8ff9689c7f0dc84729200d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-31 01:53:23 +0300

    [gui/quickshell] source config from modules path

--- nix/.config/home-manager/modules/user/gui/quickshell/source.nix
+++ nix/.config/home-manager/modules/user/gui/quickshell/source.nix
@@ -7,7 +7,9 @@
 with lib;
   mkIf (config.features.gui.enable && (config.features.gui.qt.enable or false) && (! (config.features.devSpeed.enable or false))) (
     xdg.mkXdgSource "quickshell" {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/quickshell/.config/quickshell";
+      # Keep Quickshell config with other modules under this repo
+      # Use an out-of-store symlink so edits apply immediately without a switch
+      source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/nix/.config/home-manager/modules/user/gui/quickshell/conf";
       recursive = true;
     }
   )
