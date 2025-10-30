4766f34af9c25e92432986317339195ecb2bf147
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-30 23:53:44 +0300

    [cli/bat] silence Dockerfile+bash context warning by overriding syntax; add minimal JSON stub; rebuild cache clean

--- nix/.config/home-manager/modules/cli/bat.nix
+++ nix/.config/home-manager/modules/cli/bat.nix
@@ -1,4 +1,31 @@
-{lib, ...}: {
+{ lib, xdg, ... }:
+let
+  dockerfileBashOverride = xdg.mkXdgText "bat/syntaxes/Dockerfile (with bash).sublime-syntax" ''
+    %YAML 1.2
+    ---
+    name: Dockerfile (with bash)
+    scope: source.dockerfile.bash
+    contexts:
+      main:
+        - include: scope:source.dockerfile
+  '';
+  jsonStub = xdg.mkXdgText "bat/syntaxes/JSON.sublime-syntax" ''
+    %YAML 1.2
+    ---
+    name: JSON
+    scope: source.json
+    contexts:
+      main:
+        - include: arrays
+      arrays:
+        - match: '\G\['
+          push: arrays
+        - match: ']' 
+          pop: true
+        - match: '.'
+          scope: constant.character
+  '';
+in {
   # Reduce activation noise: keep bat disabled by default (can be overridden)
   programs.bat.enable = lib.mkDefault false;
   programs.bat.config = {
@@ -8,3 +35,5 @@
     decorations = "never";
   };
 }
+// dockerfileBashOverride
+// jsonStub
