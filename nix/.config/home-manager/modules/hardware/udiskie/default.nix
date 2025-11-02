60909acd8dfe6d83cbad495d5ad567b1384a7c8b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-02 07:54:21 +0300

    [hardware/udiskie][systemd] Use graphical preset, Wayland env and restart policy

--- nix/.config/home-manager/modules/hardware/udiskie/default.nix
+++ nix/.config/home-manager/modules/hardware/udiskie/default.nix
@@ -1,8 +1,20 @@
-_: {
+{ lib, config, ... }: {
   services = {
     udiskie = {
       enable = true;
       tray = "never";
     };
   };
+
+  # Align the unit with our Wayland session and presets, similar to Flameshot.
+  systemd.user.services.udiskie = lib.mkMerge [
+    {
+      Service = {
+        Environment = [ "QT_QPA_PLATFORM=wayland" "XDG_SESSION_TYPE=wayland" ];
+        Restart = "on-failure";
+        RestartSec = 2;
+      };
+    }
+    (config.lib.neg.systemdUser.mkUnitFromPresets { presets = ["graphical"]; })
+  ];
 }
