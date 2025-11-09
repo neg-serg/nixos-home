e390a08a4dede09aa7f89ff4807bb07e242955ac
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-09 12:09:28 +0300

    [lib] soften local-bin defaults

--- nix/.config/home-manager/packages/lib/local-bin.nix
+++ nix/.config/home-manager/packages/lib/local-bin.nix
@@ -3,8 +3,9 @@
   # activation steps. If a conflicting directory exists at the target path,
   # Home Manager will report it; such cases are rare for ~/.local/bin entries.
   home.file.".local/bin/${name}" = {
-    executable = true;
-    force = true;
+    # Mark executable and allow replacing an existing file; lower priority to allow overrides
+    executable = lib.mkDefault true;
+    force = lib.mkDefault true;
     inherit text;
   };
 }
