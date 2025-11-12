fae45e05e979f13ee8fa620d0f19836ce4a9298e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-12 07:36:45 +0300

    [flake][hm] add missing files (dev/editor/emacs, mpd/cantata, pkgs.cantata) to fix flake eval

---
+++ nix/.config/home-manager/packages/cantata/default.nix
@@ -0,0 +1,138 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  qtbase,
+  qtsvg,
+  qttools,
+  qtwayland,
+  wrapQtAppsHook,
+  perl,
+
+  # Cantata doesn't build with cdparanoia enabled right now.
+  withCdda ? false,
+  cdparanoia,
+  withCddb ? false,
+  libcddb,
+  withLame ? false,
+  lame,
+  withMusicbrainz ? false,
+  libmusicbrainz5,
+
+  withTaglib ? true,
+  taglib,
+  taglib_extras,
+  withHttpStream ? true,
+  qtmultimedia,
+  gst_all_1,
+  withReplaygain ? true,
+  ffmpeg,
+  speex,
+  mpg123,
+  withMtp ? true,
+  libmtp,
+  withOnlineServices ? true,
+  withDevices ? true,
+  udisks2,
+  withDynamic ? true,
+  withHttpServer ? true,
+  withLibVlc ? false,
+  libvlc,
+  withStreams ? true,
+}: let
+  # Inter-dependencies.
+  _ = assert withCddb -> withCdda && withTaglib;
+  _ = assert withCdda -> withCddb && withMusicbrainz;
+  _ = assert withLame -> withCdda && withTaglib;
+  _ = assert withMtp -> withTaglib;
+  _ = assert withMusicbrainz -> withCdda && withTaglib;
+  _ = assert withOnlineServices -> withTaglib;
+  _ = assert withReplaygain -> withTaglib;
+  _ = assert withLibVlc -> withHttpStream;
+
+  fstat = enabled: flag: "-DENABLE_${flag}=${if enabled then "ON" else "OFF"}";
+  withUdisks = withTaglib && withDevices;
+
+  gst = with gst_all_1; [
+    gstreamer
+    gst-libav
+    gst-plugins-base
+    gst-plugins-good
+    gst-plugins-bad
+  ];
+
+  options = [
+    {names = ["CDDB"]; enable = withCddb; pkgs = [libcddb];}
+    {names = ["CDPARANOIA"]; enable = withCdda; pkgs = [cdparanoia];}
+    {names = ["DEVICES_SUPPORT"]; enable = withDevices; pkgs = [];}
+    {names = ["DYNAMIC"]; enable = withDynamic; pkgs = [];}
+    {names = ["FFMPEG" "MPG123" "SPEEXDSP"]; enable = withReplaygain; pkgs = [ffmpeg speex mpg123];}
+    {names = ["HTTPS_SUPPORT"]; enable = true; pkgs = [];}
+    {names = ["HTTP_SERVER"]; enable = withHttpServer; pkgs = [];}
+    {names = ["HTTP_STREAM_PLAYBACK"]; enable = withHttpStream; pkgs = [qtmultimedia];}
+    {names = ["LAME"]; enable = withLame; pkgs = [lame];}
+    {names = ["LIBVLC"]; enable = withLibVlc; pkgs = [libvlc];}
+    {names = ["MTP"]; enable = withMtp; pkgs = [libmtp];}
+    {names = ["MUSICBRAINZ"]; enable = withMusicbrainz; pkgs = [libmusicbrainz5];}
+    {names = ["ONLINE_SERVICES"]; enable = withOnlineServices; pkgs = [];}
+    {names = ["STREAMS"]; enable = withStreams; pkgs = [];}
+    {names = ["TAGLIB" "TAGLIB_EXTRAS"]; enable = withTaglib; pkgs = [taglib taglib_extras];}
+    {names = ["UDISKS2"]; enable = withUdisks; pkgs = [udisks2];}
+  ];
+in
+  stdenv.mkDerivation rec {
+    pname = "cantata";
+    version = "3.3.1";
+
+    src = fetchFromGitHub {
+      owner = "nullobsi";
+      repo = "cantata";
+      rev = "a19efdf9649c50320f8592f07d82734c352ace9c";
+      sha256 = "TVqgTYpHyU1OM9XddJ915GM1XQQrhH9V7yhSxQOaXRs=";
+    };
+
+    patches = [
+      ./dont-check-for-perl-in-PATH.diff
+      ./cantata-projectid.diff
+    ];
+
+    postPatch = ''
+      patchShebangs playlists
+    '';
+
+    buildInputs =
+      [
+        qtbase
+        qtsvg
+        qtwayland
+        (perl.withPackages (ppkgs: with ppkgs; [URI]))
+      ]
+      ++ lib.flatten (builtins.catAttrs "pkgs" (builtins.filter (opt: opt.enable) options));
+
+    nativeBuildInputs = [
+      cmake
+      pkg-config
+      qttools
+      wrapQtAppsHook
+    ];
+
+    cmakeFlags = lib.flatten (map (opt: map (name: fstat opt.enable name) opt.names) options);
+
+    qtWrapperArgs = lib.optionals (withHttpStream && !withLibVlc) [
+      "--prefix"
+      "GST_PLUGIN_PATH"
+      ":"
+      "${lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" gst}"
+    ];
+
+    meta = with lib; {
+      description = "Graphical client for MPD";
+      mainProgram = "cantata";
+      homepage = "https://github.com/cdrummond/cantata";
+      license = licenses.gpl3Only;
+      maintainers = with maintainers; [peterhoeg];
+      platforms = platforms.linux;
+    };
+  }
