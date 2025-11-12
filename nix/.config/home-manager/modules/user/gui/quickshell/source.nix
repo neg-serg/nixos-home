42ddb2b5abc72707a91a8522a50ef1b09266d4e6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-12 04:15:12 +0300

    [gui/quickshell] always link config

--- nix/.config/home-manager/modules/user/gui/quickshell/source.nix
+++ nix/.config/home-manager/modules/user/gui/quickshell/source.nix
@@ -1,15 +1,12 @@
 {
   lib,
   config,
-  xdg,
   ...
 }:
 with lib;
-  mkIf (config.features.gui.enable && (config.features.gui.qt.enable or false) && (config.features.gui.quickshell.enable or false) && (! (config.features.devSpeed.enable or false))) (
-    xdg.mkXdgSource "quickshell" {
-      # Link the maintained Quickshell config tree from repository root
-      # Out-of-store symlink: edits apply immediately without rebuild
-      source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/quickshell/.config/quickshell";
-      recursive = true;
-    }
-  )
+mkIf (config.features.gui.enable && (config.features.gui.qt.enable or false) && (config.features.gui.quickshell.enable or false)) {
+  home.file.".config/quickshell" = {
+    recursive = true;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.neg.dotfilesRoot}/quickshell/.config/quickshell";
+  };
+}
